1900e3048b82182adff610ce0153a8aafecf9f2c
Dylan Leard <dylan@button.is>
2021-11-24 14:42:30 -0800

    test: use jest-mock-random for snapshots

--- app/tests/unit/lib/theme/service-development-toolkit-form.test.tsx
+++ app/tests/unit/lib/theme/service-development-toolkit-form.test.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import {mockRandom} from 'jest-mock-random';
 import { render } from "@testing-library/react";
 import Form from "lib/theme/service-development-toolkit-form";
 import { JSONSchema7 } from "json-schema";
 
 describe("The Form", () => {
+  beforeEach(() => {
+    // Mock Math.random() to be deterministic.
+    // This is needed by react-jsonschema-form's RadioWidget and by json-schema-faker
+    mockRandom([0.1, 0.2, 0.3, 0.4, 0.5]);
+  });
   it("Renders a basic test schema with a submit button", () => {
     const testSchema: JSONSchema7 = {
       type: "object",
