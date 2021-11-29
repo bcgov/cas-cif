94b590fdc79f3f8cdcbb2fd27d730e4ffe829d85
Alex Zorkin <47334977+AlexZorkin@users.noreply.github.com>
2021-11-29 09:34:48 -0800

    Update app/.postgraphilerc.js
    Co-authored-by: Pierre Bastianelli <pbastia@gmail.com>

--- app/.postgraphilerc.js
+++ app/.postgraphilerc.js
@@ -11,7 +11,7 @@ module.exports = {
       uploadFieldDefinitions: [
         {
           match: ({ schema, table, column, tags }) =>
-            table === "attachment" && column == "file",
+            table === "attachment" && column === "file",
         },
       ],
     },
