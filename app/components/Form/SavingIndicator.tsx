7ee171e7d68ba05dafd71c95292c51df07994333
Matthieu Foucault <matthieu@button.is>
2021-12-13 12:26:35 -0800

    feat: use debounced mutation in create-project and add saving indicator

---
+++ app/components/Form/SavingIndicator.tsx
@@ -0,0 +1,50 @@
+import { faSync, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface Props {
+  isSaved: boolean;
+  lastEdited: Date;
+}
+
+const SavingIndicator: React.FC<Props> = ({ isSaved, lastEdited }) => {
+  return (
+    <div>
+      <span className="last-edited">
+        Last edited:{" "}
+        {lastEdited.toLocaleString("en-CA", {
+          timeZone: "America/Vancouver",
+          dateStyle: "short",
+        })}
+      </span>
+      <span>
+        <FontAwesomeIcon icon={isSaved ? faCheck : faSync} />
+        <span className="is-saved">
+          {isSaved ? "Changes saved." : "Saving changes..."}
+        </span>
+      </span>
+      <style jsx>
+        {`
+          div {
+            display: inline-flex;
+            justify-content: flex-end;
+            align-items: baseline;
+            color: #939393;
+            border: solid 1px #939393;
+            border-radius: 4px;
+            padding: 0.5rem;
+          }
+
+          .last-edited {
+            margin-right: 1rem;
+          }
+
+          .is-saved {
+            margin-left: 0.5rem;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default SavingIndicator;
