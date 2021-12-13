041f290eaa3431e9499e01092eca12d8cb52a8df
Dylan Leard <dylan@button.is>
2021-12-13 11:53:42 -0800

    test: add widget test and remove unused Identity test

---
+++ app/tests/unit/lib/theme/widgets/SearchDropdown.test.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import SearchDropdownWidget from "lib/theme/widgets/SearchDropdownWidget";
+
+import { render } from "@testing-library/react";
+
+describe("The SearchDropdown Widget", () => {
+  it("Matches the snapshot with no default value set", () => {
+    const props: any = {
+      id: "1",
+      placeholder: "test placeholder",
+      onChange: jest.fn(),
+      label: "Search",
+      value: undefined,
+      required: false,
+      schema: {
+        anyOf: [
+          {value: 1, enum: [1], type: "number", title: "Option 1"},
+          {value: 2, enum: [2], type: "number", title: "Option 2"}
+        ]
+      }
+    };
+
+    const componentUnderTest = render(<SearchDropdownWidget {...props} />);
+
+    expect(componentUnderTest.container).toMatchSnapshot();
+  });
+
+  it("Matches the snapshot with a default value set", () => {
+    const props: any = {
+      id: "1",
+      placeholder: "test placeholder",
+      onChange: jest.fn(),
+      label: "Search",
+      value: 1,
+      required: false,
+      schema: {
+        anyOf: [
+          {value: 1, enum: [1], type: "number", title: "Option 1"},
+          {value: 2, enum: [2], type: "number", title: "Option 2"}
+        ]
+      }
+    };
+
+    const componentUnderTest = render(<SearchDropdownWidget {...props} />);
+
+    expect(componentUnderTest.container).toMatchSnapshot();
+  });
+});
