701117f9b2dc24bb9ec870a252ae1e5698c147ec
Matthieu Foucault <matthieu@button.is>
2021-12-13 12:25:36 -0800

    feat: add useDebouncedMutation hook

---
+++ app/mutations/useDebouncedMutation.ts
@@ -0,0 +1,74 @@
+import { CacheConfigWithDebounce } from "lib/relay/debounceMutationMiddleware";
+import { useMutation, UseMutationConfig } from "react-relay";
+import { commitMutation as baseCommitMutation } from "relay-runtime";
+import {
+  Disposable,
+  GraphQLTaggedNode,
+  IEnvironment,
+  MutationConfig,
+  MutationParameters,
+} from "relay-runtime";
+
+const debouncedMutationMap = new Map<string, Disposable>();
+
+export interface DebouncedMutationConfig<TMutation extends MutationParameters>
+  extends MutationConfig<TMutation> {
+  debounceKey: string;
+  cacheConfig: CacheConfigWithDebounce;
+}
+
+export interface UseDebouncedMutationConfig<
+  TMutation extends MutationParameters
+> extends UseMutationConfig<TMutation> {
+  debounceKey: string;
+}
+
+export default function useDebouncedMutation<
+  TMutation extends MutationParameters
+>(mutation: GraphQLTaggedNode) {
+  const commitMutationFn = (
+    environment: IEnvironment,
+    config: DebouncedMutationConfig<TMutation>
+  ) => {
+    const { debounceKey } = config;
+
+    if (!debounceKey) {
+      throw new Error(
+        "debounceKey is required when using useDebouncedMutation"
+      );
+    }
+
+    // Debounced mutations should be commited immediately to perform the optimisticUpdate
+    // The actual request will be debounced in the network layer
+    // Here we either dispose of a debounced mutation, or remove it from the map when it errors/completes
+    const previousMutation = debouncedMutationMap.get(debounceKey);
+    if (previousMutation) {
+      previousMutation.dispose();
+    }
+
+    const commitConfig: DebouncedMutationConfig<TMutation> = {
+      ...config,
+      cacheConfig: {
+        ...config.cacheConfig,
+        debounceKey,
+      },
+      onError: (error) => {
+        debouncedMutationMap.delete(debounceKey);
+        config.onError?.(error);
+      },
+      onCompleted: (response, errors) => {
+        debouncedMutationMap.delete(debounceKey);
+        config.onCompleted?.(response, errors);
+      },
+    };
+
+    const disposable = baseCommitMutation(environment, commitConfig);
+    debouncedMutationMap.set(debounceKey, disposable);
+    return disposable;
+  };
+
+  return useMutation(mutation, commitMutationFn) as [
+    (config: UseDebouncedMutationConfig<TMutation>) => Disposable,
+    boolean
+  ];
+}
