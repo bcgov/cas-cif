79f8d26d97dcfc4cbabafac002efd6ce1fb4a39c
Dylan Leard <dylan@button.is>
2021-12-14 13:23:35 -0800

    refactor: update dev data & add sqitch dependency

--- schema/data/dev/002_cif_operator.sql
+++ schema/data/dev/002_cif_operator.sql
@@ -4,11 +4,11 @@ insert into cif.operator (id, legal_name, trade_name, bc_registry_id)
 overriding system value
 values
 
-  (1, 'first operator legal name', 'first operator trade name', '1234abcd'),
+  (1, 'first operator legal name', 'first operator trade name', 'AB1234567'),
 
-  (2, 'second operator legal name', 'second operator trade name', '5555ffff'),
+  (2, 'second operator legal name', 'second operator trade name', 'CD2345678'),
 
-  (3, 'third operator legal name', 'third operator trade name', '9999zzzz')
+  (3, 'third operator legal name', 'third operator trade name', 'EF3456789')
 
 on conflict (id) do update set
 legal_name = excluded.legal_name,
