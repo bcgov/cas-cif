6f9f69b926d104967fad582b37d6ac46bad2955b
Matthieu Foucault <matthieu@button.is>
2022-01-10 10:55:13 -0800

    chore: update FK in cif_project dev data

--- schema/data/dev/003_cif_project.sql
+++ schema/data/dev/003_cif_project.sql
@@ -1,6 +1,6 @@
 begin;
 
-insert into cif.project(operator_id, funding_stream_id, rfp_number, summary, project_name) values
+insert into cif.project(operator_id, funding_stream_rfp_id, rfp_number, summary, project_name) values
 (1, 1, '2019-RFP-1-000-ABCD', 'summary', 'project 1'),
 (2, 1, '2019-RFP-0-000-EFGH', 'summary', 'project 2'),
 (3, 1, '2019-RFP-0-000-ZXCV', 'summary', 'project 3'),
