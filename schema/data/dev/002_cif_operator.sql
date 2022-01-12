7a110d8f93972c618d3d2f1236d04bb212e151f5
Matthieu Foucault <matthieu@button.is>
2022-01-11 16:51:15 -0800

    fix: don't override operator id when inserting dev data

--- schema/data/dev/002_cif_operator.sql
+++ schema/data/dev/002_cif_operator.sql
@@ -1,16 +1,9 @@
 begin;
 
-insert into cif.operator (id, legal_name, trade_name, bc_registry_id)
-overriding system value
+insert into cif.operator (legal_name, trade_name, bc_registry_id)
 values
-
-  (1, 'first operator legal name', 'first operator trade name', 'AB1234567'), -- pragma: allowlist secret
-  (2, 'second operator legal name', 'second operator lorem ipsum dolor sit amet limited', 'BC1234567'), -- pragma: allowlist secret
-  (3, 'third operator legal name', 'third operator trade name', 'EF3456789') -- pragma: allowlist secret
-
-on conflict (id) do update set
-legal_name = excluded.legal_name,
-trade_name = excluded.trade_name,
-bc_registry_id = excluded.bc_registry_id;
+  ('first operator legal name', 'first operator trade name', 'AB1234567'),
+  ('second operator legal name', 'second operator lorem ipsum dolor sit amet limited', 'BC1234567'),
+  ('third operator legal name', 'third operator trade name', 'EF3456789');
 
 commit;
