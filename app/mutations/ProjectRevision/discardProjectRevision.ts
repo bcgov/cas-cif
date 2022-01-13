895b043fc06dadf005d35357582578dfd3af9f37
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-01-12 16:21:59 -0800

    feat: generic discard mutation

--- app/mutations/ProjectRevision/discardProjectRevision.ts
+++ app/mutations/ProjectRevision/discardProjectRevision.ts
@@ -1,41 +1,9 @@
-import { graphql, useMutation, UseMutationConfig } from "react-relay";
-import { Disposable, MutationParameters } from "relay-runtime";
+import { graphql } from "react-relay";
 
-const mutation = graphql`
+export const mutation = graphql`
   mutation discardProjectRevisionMutation($input: UpdateProjectRevisionInput!) {
     updateProjectRevision(input: $input) {
       __typename
     }
   }
 `;
-
-function useDiscardProjectMutation<TMutation extends MutationParameters>(): [
-  (
-    projectRevisionId: string,
-    config: Partial<UseMutationConfig<TMutation>>
-  ) => Disposable,
-  boolean
-] {
-  const [updateMutation, isInFlight] = useMutation(mutation);
-
-  const discardProjectMutation = (
-    projectRevisionId: string,
-    config: Partial<UseMutationConfig<TMutation>>
-  ) => {
-    return updateMutation({
-      variables: {
-        input: {
-          id: projectRevisionId,
-          projectRevisionPatch: {
-            deletedAt: new Date().toISOString(),
-          },
-        },
-      },
-      ...config,
-    });
-  };
-
-  return [discardProjectMutation, isInFlight];
-}
-
-export default useDiscardProjectMutation;
