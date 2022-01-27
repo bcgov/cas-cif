8b0613f7c9c65b7b921fb398038f18d7d193fa05
Matthieu Foucault <matthieu@button.is>
2022-01-27 09:51:25 -0800

    test: truncate new tables

--- schema/data/e2e/dbReset.sql
+++ schema/data/e2e/dbReset.sql
@@ -2,7 +2,7 @@ begin;
 
 -- cif.cif_user should not be truncated or else all tables should be (since they all have a created_by column)
 truncate table
-cif.form_change, cif.project_revision, cif.project, cif.project_manager, cif.operator
+cif.form_change, cif.project_revision, cif.project, cif.project_contact, cif.contact, cif.project_manager, cif.operator
 restart identity;
 
 
