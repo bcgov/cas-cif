b16549ac3147003350c87494c28a477cc89c30de
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-01-27 16:48:44 -0800

    feat: new mutation to add a secondary contact form_change to a revision

---
+++ schema/deploy/mutations/add_contact_to_revision.sql
@@ -0,0 +1,33 @@
+-- Deploy cif:mutations/add_contact_to_revision to pg
+
+begin;
+
+create or replace function cif.add_contact_to_revision(revision_id int, contact_index int)
+returns cif.form_change
+as $add_contact_form_change$
+  insert into cif.form_change(
+    new_form_data,
+    operation,
+    form_data_schema_name,
+    form_data_table_name,
+    form_data_record_id,
+    project_revision_id,
+    change_status,
+    change_reason
+  ) values (
+    format(
+      '{ "projectId": %s, "contact_index": %s }',
+      (select form_data_record_id from cif.form_change where form_data_schema_name='cif' and form_data_table_name='project' and project_revision_id=$1),
+      $2
+    )::jsonb,
+    'INSERT',
+    'cif',
+    'project_contact',
+    nextval(pg_get_serial_sequence('cif.project_contact', 'id')),
+    $1,
+    'pending',
+    'Creating new project: secondary project_contact record'
+  ) returning *;
+$add_contact_form_change$ language sql volatile;
+
+commit;
---
+++ schema/test/unit/mutations/add_contact_to_revision_test.sql
@@ -0,0 +1,13 @@
+
+begin;
+
+select * from no_plan();
+
+
+select is(1,2,'no!');
+
+
+select finish();
+
+
+rollback;
