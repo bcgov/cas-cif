ff666c3d66eca781613219e9bd17e4c415f9879d
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-02-02 14:45:59 -0800

    feat: allowing user to set the id on the form

--- app/components/Form/FormComponentProps.tsx
+++ app/components/Form/FormComponentProps.tsx
@@ -1,5 +1,6 @@
 import type { AjvError, ObjectFieldTemplateProps } from "@rjsf/core";
 export default interface FormComponentProps {
+  id?: string;
   formData: any;
   onChange: (changeObject: object) => void;
   onFormErrors?: (errorsArray: AjvError[]) => void;
