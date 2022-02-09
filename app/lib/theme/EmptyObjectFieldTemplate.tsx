0e0d7390a84b27426faa48f5f14ea624142be81a
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-02-09 11:20:10 -0800

    docs: why an empty object field template

--- app/lib/theme/EmptyObjectFieldTemplate.tsx
+++ app/lib/theme/EmptyObjectFieldTemplate.tsx
@@ -1,5 +1,13 @@
 import { ObjectFieldTemplateProps } from "@rjsf/core";
 
+/**
+ *
+ * The ObjectFieldTemplate is used by RJSF to render the "Object" type in a schema.
+ * For really small forms, it can be useful to override the default tempalte with
+ * an empty template that has only renders the individual form fields.
+ *
+ */
+
 const EmptyObjectFieldTemplate: React.FC<ObjectFieldTemplateProps> = (
   props
 ) => {
