9678ac4f44aa8a68fbaa49ddf7989bdec6aa9c7f
Dylan Leard <dylan@button.is>
2022-02-14 14:29:41 -0800

    chore: update operator dev data

--- schema/data/dev/002_cif_operator.sql
+++ schema/data/dev/002_cif_operator.sql
@@ -1,9 +1,9 @@
 begin;
 
-insert into cif.operator (legal_name, trade_name, bc_registry_id, operator_code)
+insert into cif.operator (swrs_organisation_id, legal_name, trade_name, bc_registry_id, operator_code)
 values
-  ('first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD'),
-  ('second operator legal name', 'second operator lorem ipsum dolor sit amet limited', 'BC1234567', 'EFGH'),
-  ('third operator legal name', 'third operator trade name', 'EF3456789', 'IJKL');
+  (1000, 'first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD'),
+  (1001, 'second operator legal name', 'second operator lorem ipsum dolor sit amet limited', 'BC1234567', 'EFGH'),
+  (1002, 'third operator legal name', 'third operator trade name', 'EF3456789', 'IJKL');
 
 commit;
