de52b1e56df239a868a899b002d97041f0827c30
Matthieu Foucault <matthieu@button.is>
2022-02-16 14:15:15 -0800

    feat: project_form_change computed column supports multiple project revs

--- schema/deploy/computed_columns/project_revision_project_form_change.sql
+++ schema/deploy/computed_columns/project_revision_project_form_change.sql
@@ -3,21 +3,36 @@
 
 begin;
 
-create or replace function cif.project_revision_project_form_change(project_revision cif.project_revision)
+create or replace function cif.project_revision_project_form_change(cif.project_revision)
 returns cif.form_change
 as
 $computed_column$
 
-  select *
-    from cif.form_change
-    where project_revision_id = project_revision.id
-      and form_data_schema_name='cif'
-      and form_data_table_name='project';
+  with project_form_change_history as (
+    select *
+      from cif.form_change
+      where project_revision_id = $1.id
+        and form_data_schema_name='cif'
+        and form_data_table_name='project'
+    union
+    select *
+      from cif.form_change
+      where form_data_schema_name='cif'
+        and form_data_table_name='project'
+        and change_status = 'committed'
+        and form_data_record_id is not null
+        and form_data_record_id = $1.project_id
+      order by updated_at desc, id desc
+  )
+  select * from project_form_change_history limit 1;
 
 $computed_column$ language sql stable;
 
 grant execute on function cif.project_revision_project_form_change to cif_internal, cif_external, cif_admin;
 
-comment on function cif.project_revision_project_form_change is 'Computed column for graphql to retrieve the change related to the project record, within a project revision';
+comment on function cif.project_revision_project_form_change is
+$comment$
+  Computed column for graphql to retrieve the change related to the project record, within a project revision.
+  If the change does not exist for the project revision, retrieve the change matching the current project record.$comment$;
 
 commit;
--- schema/test/unit/computed_columns/project_revision_project_form_change_test.sql
+++ schema/test/unit/computed_columns/project_revision_project_form_change_test.sql
@@ -2,7 +2,7 @@
 
 begin;
 
-select plan(2);
+select plan(4);
 
 insert into cif.project_revision(id, change_status)
   overriding system value
@@ -34,6 +34,55 @@ select is(
   ),
   null,
   'Returns null if there is no form change for the project table'
+);
+
+
+-- Setting up the test data for the following scenario:
+-- For our project (id = 1), we have multiple revisions:
+-- revision 3: committed, with a form change for the project table (id = 4)
+-- revision 4: committed, without a form change for the project table
+-- revision 5: pending, without a form change for the project table
+alter table cif.form_change disable trigger commit_form_change;
+insert into cif.operator(id, legal_name)
+  overriding system value
+  values (1, 'test operator');
+insert into cif.project(id, operator_id, funding_stream_rfp_id, project_status_id, rfp_number, summary, project_name)
+  overriding system value
+  values (1, 1, 1, 1, '000', 'summary', 'project 1');
+
+insert into cif.project_revision(id, change_status, project_id)
+  overriding system value
+  values (3, 'pending', 1), (4, 'pending', 1), (5, 'pending', 1);
+
+insert into cif.form_change(
+  id, operation, form_data_schema_name, form_data_table_name, form_data_record_id, project_revision_id, change_reason, json_schema_name)
+  overriding system value
+  values
+    (4, 'create', 'cif', 'project', 1, 3, 'test reason', 'project');
+
+update cif.project_revision set change_status = 'committed' where id = 3 or id = 4;
+
+select is(
+  (
+    select id from cif.project_revision_project_form_change(
+      (select row(project_revision.*)::cif.project_revision
+      from cif.project_revision where id=4)
+    )
+  ),
+  4,
+  'returns the latest form change record for the project table when there is no form change in the revision and the current project_revision is pending'
+);
+
+
+select is(
+  (
+    select id from cif.project_revision_project_form_change(
+      (select row(project_revision.*)::cif.project_revision
+      from cif.project_revision where id=5)
+    )
+  ),
+  4,
+  'returns the latest form change record for the project table when there is no form change in the revision and the current project_revision is committed'
 );
 
 select finish();
