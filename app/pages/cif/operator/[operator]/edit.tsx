474bff73fc293f3e0e2826f5358734c0b857b787
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-02-15 17:05:50 -0800

    refactor: edit operator page uses relay and session

--- app/pages/cif/operator/[operator]/edit.tsx
+++ app/pages/cif/operator/[operator]/edit.tsx
@@ -1,3 +1,30 @@
-const editPage = () => <div>EDIT</div>;
+import withRelayOptions from "lib/relay/withRelayOptions";
+import { graphql } from "react-relay";
+import { RelayProps, withRelay } from "relay-nextjs";
+import { usePreloadedQuery } from "react-relay/hooks";
+import { editOperatorQuery } from "__generated__/editOperatorQuery.graphql";
+import DefaultLayout from "components/Layout/DefaultLayout";
 
-export default editPage;
+const operatorQuery = graphql`
+  query editOperatorQuery($operator: ID!) {
+    session {
+      ...DefaultLayout_session
+    }
+    operator(id: $operator) {
+      legalName
+    }
+  }
+`;
+
+export function EditOperator({
+  preloadedQuery,
+}: RelayProps<{}, editOperatorQuery>) {
+  const { session, operator } = usePreloadedQuery(
+    operatorQuery,
+    preloadedQuery
+  );
+
+  return <DefaultLayout session={session}>{operator.legalName}</DefaultLayout>;
+}
+
+export default withRelay(EditOperator, operatorQuery, withRelayOptions);
