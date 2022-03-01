1d727db3e76bb70152bc5bbf1037f9a38ea8b244
Dylan Leard <dylan@button.is>
2022-03-01 14:57:36 -0800

    feat: display rjsf errors in a consistent, styled way

--- app/components/Form/FormBase.tsx
+++ app/components/Form/FormBase.tsx
@@ -1,23 +1,48 @@
 import Form from "lib/theme/FormWithTheme";
 import { forwardRef } from "react";
-import { FormProps } from "@rjsf/core";
+import { FormProps, AjvError } from "@rjsf/core";
+import { customTransformErrors } from "lib/theme/customTransformErrors";
+import { customFormats, customFormatsErrorMessages } from "data/jsonSchemaForm/customFormats";
 
 const FormBase: React.ForwardRefRenderFunction<any, FormProps<any>> = (
   props,
   ref
 ) => {
+  console.log(props);
+  console.log(ref)
+
+  const transformErrors = (errors: AjvError[]) => {
+    return customTransformErrors(errors, customFormatsErrorMessages);
+  };
   return (
     <>
       <Form
         {...props}
         // @ts-ignore
         ref={ref}
+        customFormats={customFormats}
+        transformErrors={transformErrors}
         noHtml5Validate
         omitExtraData
+        showErrorList={false}
       ></Form>
       <style jsx>{`
         :global(label) {
           font-weight: bold;
+        }
+        .errors {
+          margin: 1em 0 0 0;
+          padding: 20px;
+          background: #ce5c5c;
+          color: white;
+          font-size: 20px;
+        }
+        :global(ul.error-detail) {
+          padding: 0;
+          list-style: none;
+        }
+        :global(li.text-danger) {
+          color: #cd2026 !important;
         }
       `}</style>
     </>
--- app/lib/theme/FieldTemplate.tsx
+++ app/lib/theme/FieldTemplate.tsx
@@ -1,14 +1,33 @@
-const FieldTemplate = ({ children, errors, help }) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+
+const FieldTemplate = ({ children, errors, help, rawErrors }) => {
   return (
     <>
       <div>
         {children}
-        {errors}
+        {rawErrors ?
+          <div className={"error-div"}>
+            <FontAwesomeIcon id="error-icon" icon={faExclamationTriangle} />{errors}
+          </div>
+          :
+            <div className={"error-div"}>
+              <div>&nbsp;</div>
+            </div>
+        }
         {help}
       </div>
       <style jsx>{`
         div:not(:last-child) {
           margin-bottom: 1em;
+        }
+        :global(#error-icon) {
+          color: #cd2026;
+        }
+        :global(.error-div) {
+          height: 5%;
+          display: flex;
+          padding-top: 10px;
         }
       `}</style>
     </>
