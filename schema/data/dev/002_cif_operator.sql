a43539982e567778a9a9beea94e594493f1692c1
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-02-28 16:52:19 -0800

    refactor: operator dev data is inserted with form_change records

--- schema/data/dev/002_cif_operator.sql
+++ schema/data/dev/002_cif_operator.sql
@@ -1,9 +1,39 @@
 begin;
 
-insert into cif.operator (swrs_organisation_id, legal_name, trade_name, bc_registry_id, operator_code)
+insert into cif.form_change(
+  new_form_data,
+  operation,
+  form_data_schema_name,
+  form_data_table_name,
+  change_status,
+  change_reason,
+  json_schema_name
+)
 values
-  (1000, 'first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD'),
-  (1001, 'second operator legal name', 'second operator lorem ipsum dolor sit amet limited', 'BC1234567', 'EFGH'),
-  (1002, 'third operator legal name', 'third operator trade name', 'EF3456789', 'IJKL');
+('{
+  "swrsOrganisationId": 1000,
+  "legalName": "first operator legal name",
+  "tradeName": "first operator trade name",
+  "swrsLegalName": "first operator legal name",
+  "swrsTradeName": "first operator trade name",
+  "bcRegistryId": "AB1234567",
+  "operatorCode": "ABCD" }', 'create', 'cif', 'operator', 'committed', 'Created by dev data seed script', 'operator'),
+('{
+  "swrsOrganisationId": 1001,
+  "legalName": "second operator legal name",
+  "tradeName": "second operator lorem ipsum dolor sit amet limited",
+  "swrsLegalName": "second operator legal name",
+  "swrsTradeName": "second operator lorem ipsum dolor sit amet limited",
+  "bcRegistryId": "BC1234567",
+  "operatorCode": "EFGH" }', 'create', 'cif', 'operator', 'committed', 'Created by dev data seed script', 'operator'),
+('{
+  "swrsOrganisationId": 1002,
+  "legalName": "third operator legal name",
+  "tradeName": "third operator trade name",
+  "swrsLegalName": "third operator SWRS legal name",
+  "swrsTradeName": "third operator SWRS trade name",
+  "bcRegistryId": "EF3456789",
+  "operatorCode": "IJKL" }', 'create', 'cif', 'operator', 'committed', 'Created by dev data seed script', 'operator');
+
 
 commit;
