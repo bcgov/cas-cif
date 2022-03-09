e6264e77e954e5b7808f3e1368a01927f3abe74e
Brianna Cerkiewicz <brianna@button.is>
2022-03-09 13:36:48 -0800

    test: create test for 500 page redirect

---
+++ app/cypress/integration/cif/500.spec.js
@@ -0,0 +1,17 @@
+describe("The 500 page", () => {
+  beforeEach(() => {
+    cy.sqlFixture("e2e/dbReset");
+    cy.sqlFixture("dev/001_cif_user");
+  });
+
+  it("navigates to the correct page when the refresh link is clicked", () => {
+    cy.visit("/");
+    cy.visit("/500", { failOnStatusCode: false });
+    cy.get("body").happoScreenshot({
+      component: "500 page",
+    });
+    cy.get("a[href='javascript:;']").click();
+    cy.url().should("eq", Cypress.config().baseUrl + "/");
+    cy.contains("Welcome to the cif web application.").should("be.visible");
+  });
+});
