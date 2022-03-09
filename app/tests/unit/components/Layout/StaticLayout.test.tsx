38231e29b20b0ae1bafad32748dff4657ee4823b
Brianna Cerkiewicz <brianna@button.is>
2022-03-09 13:23:56 -0800

    test: create test for StaticLayout component

---
+++ app/tests/unit/components/Layout/StaticLayout.test.tsx
@@ -0,0 +1,23 @@
+import StaticLayout from "components/Layout/StaticLayout";
+import { render, screen } from "@testing-library/react";
+
+const renderStaticLayout = () => {
+  return render(<StaticLayout />);
+};
+
+describe("The StaticLayout component", () => {
+  it("should not render login button", () => {
+    renderStaticLayout();
+    expect(screen.queryByText("Login")).toBeNull();
+    expect(screen.queryByText("Projects")).toBeNull();
+  });
+
+  it("should not render the subheader links", () => {
+    renderStaticLayout();
+    expect(screen.getByText("CleanBC Industry Fund")).toBeVisible();
+    expect(screen.queryByText("Dashboard")).toBeNull();
+    expect(screen.queryByText("Projects")).toBeNull();
+    expect(screen.queryByText("Operators")).toBeNull();
+    expect(screen.queryByText("Contacts")).toBeNull();
+  });
+});
