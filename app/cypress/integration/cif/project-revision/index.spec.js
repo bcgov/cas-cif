ef0c098782576e6d558530457bd80267735c84cf
Brianna Cerkiewicz <brianna@button.is>
2022-03-15 14:44:50 -0700

    test: add mocked time and dates to existing cypress tests

--- app/cypress/integration/cif/project-revision/index.spec.js
+++ app/cypress/integration/cif/project-revision/index.spec.js
@@ -6,6 +6,7 @@ describe("the new project page", () => {
     cy.sqlFixture("dev/001_cif_user");
     cy.sqlFixture("dev/002_cif_operator");
     cy.sqlFixture("dev/004_cif_contact");
+    cy.useMockedTime(new Date("June 10, 2020 09:00:00"));
   });
 
   it("renders the project overview form", () => {
@@ -105,6 +106,7 @@ describe("the new project page", () => {
     cy.url().should("include", "/cif/projects");
     cy.findByText("View").click();
     cy.url().should("include", "/cif/project/");
+    cy.useMockedTime(new Date("June 10, 2020 09:00:01"));
     cy.findByText("Edit").click();
     cy.url().should("include", "/cif/project-revision/");
 
