d9ffd1f0157d1ff2a483802e90a2cc4d15fd51c4
Dylan Leard <dylan@button.is>
2022-03-17 16:52:22 -0700

    refactor: update deleteManagerFromRevision mutation to be dynamic

--- app/mutations/Manager/deleteManagerFromRevision.ts
+++ app/mutations/Manager/deleteManagerFromRevision.ts
@@ -1,8 +1,11 @@
 import { useMutation, graphql, Disposable, Environment } from "react-relay";
 import { MutationConfig } from "relay-runtime";
 import { deleteManagerFromRevisionMutation } from "__generated__/deleteManagerFromRevisionMutation.graphql";
+import { deleteManagerFromRevisionWithArchiveMutation } from "__generated__/deleteManagerFromRevisionWithArchiveMutation.graphql";
 
-export const mutation = graphql`
+let mutation;
+
+export const deleteMutation = graphql`
   mutation deleteManagerFromRevisionMutation(
     $input: DeleteFormChangeInput!
     $projectRevision: ID!
@@ -32,16 +35,54 @@ export const mutation = graphql`
   }
 `;
 
+export const archiveMutation = graphql`
+  mutation deleteManagerFromRevisionWithArchiveMutation(
+    $input: UpdateFormChangeInput!
+    $projectRevision: ID!
+  ) {
+    updateFormChange(input: $input) {
+      formChange {
+        id
+        newFormData
+      }
+      query {
+        projectRevision(id: $projectRevision) {
+          managerFormChanges: projectManagerFormChangesByLabel {
+            edges {
+              node {
+                projectManagerLabel {
+                  id
+                  rowId
+                  label
+                }
+                formChange {
+                  id
+                  newFormData
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const useDeleteManagerFromRevisionMutation = (
+  operation: any,
   commitMutationFn?: (
     environment: Environment,
-    config: MutationConfig<deleteManagerFromRevisionMutation>
+    config: MutationConfig<
+      | deleteManagerFromRevisionMutation
+      | deleteManagerFromRevisionWithArchiveMutation
+    >
   ) => Disposable
 ) => {
-  return useMutation<deleteManagerFromRevisionMutation>(
-    mutation,
-    commitMutationFn
-  );
+  mutation = operation === "CREATE" ? deleteMutation : archiveMutation;
+  return useMutation<
+    | deleteManagerFromRevisionMutation
+    | deleteManagerFromRevisionWithArchiveMutation
+  >(mutation, commitMutationFn);
 };
 
 export default useDeleteManagerFromRevisionMutation;
