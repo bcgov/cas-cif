3e01ffdd3b5c233fe1edc0518a9326419ca7bb87
Brianna Cerkiewicz <brianna@button.is>
2022-03-22 13:56:50 -0700

    feat: create GlobalAlert component

---
+++ app/components/Layout/GlobalAlert.tsx
@@ -0,0 +1,15 @@
+import Alert from "@button-inc/bcgov-theme/Alert";
+
+interface Props {
+  error: string;
+}
+
+const GlobalAlert = (props: Props) => {
+  return (
+    <Alert variant="danger" closable>
+      {props.error}
+    </Alert>
+  );
+};
+
+export default GlobalAlert;
