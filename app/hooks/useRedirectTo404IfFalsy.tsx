fd269f30c0e83452ba24ebc6aafefea779a5afcf
Brianna Cerkiewicz <77306764+BCerki@users.noreply.github.com>
2022-03-23 08:13:04 -0700

    chore: add comment about how to use useRedirectTo404IfFalsy hook
    Co-authored-by: Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>

--- app/hooks/useRedirectTo404IfFalsy.tsx
+++ app/hooks/useRedirectTo404IfFalsy.tsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-
+/** 
+ *  Hook that returns true if the router will redirect, false otherwise.
+ *  - Example usage:
+ *    const isRedirecting = useRedirectTo404IfFalsy(object);
+ *    if(isRedirecting) return null;
+ */
 export default function useRedirectTo404IfFalsy(record) {
   const router = useRouter();
   useEffect(() => {
