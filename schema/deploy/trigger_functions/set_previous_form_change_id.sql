94da26a0aa46c3b6ff73d39ccf4ff5ef192e790a
Dylan Leard <dylan@button.is>
2022-03-23 10:38:44 -0700

    refactor: make trigger apply id across revisions

--- schema/deploy/trigger_functions/set_previous_form_change_id.sql
+++ schema/deploy/trigger_functions/set_previous_form_change_id.sql
@@ -2,13 +2,20 @@
 -- requires: tables/form_change
 
 begin;
-create or replace function cif_private.set_previous_form_change_id()
-  returns trigger as $$
-begin
-  new.previous_form_change_id = (select id from cif.form_change where project_revision_id = new.project_revision_id and change_status='committed' order by id desc limit 1);
-  return new;
-end;
-$$ language plpgsql volatile;
+  create or replace function cif_private.set_previous_form_change_id()
+    returns trigger as $$
+  begin
+    new.previous_form_change_id = (
+      select id from cif.form_change
+        where form_data_schema_name = new.form_data_schema_name
+        and form_data_table_name = new.form_data_table_name
+        and form_data_record_id = new.form_data_record_id
+        and change_status='committed'
+      order by updated_at desc, id desc limit 1
+    );
+    return new;
+  end;
+  $$ language plpgsql volatile;
 
 grant execute on function cif_private.set_previous_form_change_id to cif_internal,cif_external,cif_admin;
 
