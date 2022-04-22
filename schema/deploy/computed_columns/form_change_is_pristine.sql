ebdb6ee339d2dde205bf944c868af1a17d275bf7
Gurjeet Matharu <gurjeet@matharuco.com>
2022-04-22 13:56:22 -0500

    test: adding is_prestine unit test and fixing deploy

--- schema/deploy/computed_columns/form_change_is_pristine.sql
+++ schema/deploy/computed_columns/form_change_is_pristine.sql
@@ -4,12 +4,11 @@ begin;
 
 create or replace function cif.form_change_is_pristine(fc cif.form_change) returns boolean as
 $computed_column$
-begin
-  return exists (
-    select 1 from cif.form_change where cif.form_change.id = cif.form_change.previous_form_change_id
-  );
-end
-$computed_column$ language plpgsql stable;
+
+select new_form_data = ( select new_form_data from cif.form_change pfc where fc.previous_form_change_id = pfc.id)
+from cif.form_change where id=fc.id;
+
+$computed_column$ language sql stable;
 
 
 commit;
---
+++ schema/test/unit/computed_columns/form_change_is_pristine_test.sql
@@ -0,0 +1,89 @@
+begin;
+
+select plan(3);
+
+/** TEST SETUP **/
+truncate cif.project restart identity cascade;
+
+insert into cif.cif_user(id, uuid)
+  overriding system value
+  values (1, '11111111-1111-1111-1111-111111111111');
+
+insert into cif.operator (id, legal_name, trade_name, bc_registry_id, operator_code)
+overriding system value
+values
+  (1, 'first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD');
+
+insert into cif.project(id, operator_id, funding_stream_rfp_id, project_status_id, proposal_reference, summary, project_name)
+overriding system value
+values
+  (1, 1, 1, 1, '000', 'summary', 'project 1');
+
+insert into cif.project_revision(id, change_status, change_reason, project_id)
+overriding system value
+values
+  (1, 'committed', 'reason for change', 1),
+  (2, 'committed', 'reason for change', 1),
+  (3, 'committed', 'reason for change', 1);
+
+alter table cif.form_change disable trigger _set_previous_form_change_id;
+
+insert into cif.form_change(new_form_data, operation, form_data_schema_name, form_data_table_name, json_schema_name, previous_form_change_id, project_revision_id)
+values (
+  '{"testField": "test value"}',
+  'create',
+  'cif',
+  'test_table',
+  'schema',
+  null,
+  1
+);
+
+insert into cif.form_change(new_form_data, operation, form_data_schema_name, form_data_table_name, json_schema_name, previous_form_change_id, project_revision_id)
+values (
+  '{"testField": "test value"}',
+  'create',
+  'cif',
+  'test_table',
+  'schema',
+  1,
+  2
+);
+
+insert into cif.form_change(new_form_data, operation, form_data_schema_name, form_data_table_name, json_schema_name, previous_form_change_id, project_revision_id)
+values (
+  '{"xyz": "changed value"}',
+  'create',
+  'cif',
+  'test_table',
+  'schema',
+  2,
+  2
+);
+
+select is(
+  (
+    select cif.form_change_is_pristine((select row(form_change.*)::cif.form_change from cif.form_change where id=1))
+  ),
+  null,
+  'Returns null, as form does not have a previous form id'
+);
+
+select is(
+  (
+    select cif.form_change_is_pristine((select row(form_change.*)::cif.form_change from cif.form_change where id=2))
+  ),
+  true,
+  'Returns True, as previous form id has no changes'
+);
+
+select is(
+  (
+    select cif.form_change_is_pristine((select row(form_change.*)::cif.form_change from cif.form_change where id=3))
+  ),
+  false,
+  'Returns false, as there are changes to the new form data'
+);
+
+select finish();
+rollback;
