558a6eae4a756a5f4052539a8f270c24804b1402
Brianna Cerkiewicz <brianna@button.is>
2022-04-25 14:47:10 -0700

    refactor: use test helpers in operators test

--- app/tests/unit/pages/operators.test.tsx
+++ app/tests/unit/pages/operators.test.tsx
@@ -1,23 +1,14 @@
-import React from "react";
-import { Operators, OperatorsQuery } from "../../../pages/cif/operators";
-import { render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import {
-  createMockEnvironment,
-  MockPayloadGenerator,
-  RelayMockEnvironment,
-} from "relay-test-utils";
-import { RelayEnvironmentProvider, loadQuery } from "react-relay";
-import {
-  operatorsQuery,
-  operatorsQuery$variables,
-} from "__generated__/operatorsQuery.graphql";
-import { MockResolvers } from "relay-test-utils/lib/RelayMockPayloadGenerator";
-import { DEFAULT_PAGE_SIZE } from "components/Table/Pagination";
-import { useRouter } from "next/router";
-import { mocked } from "jest-mock";
+import { act, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { ErrorContext } from "contexts/ErrorContext";
+import { DEFAULT_PAGE_SIZE } from "components/Table/Pagination";
+import { mocked } from "jest-mock";
+import { useRouter } from "next/router";
+import PageTestingHelper from "tests/helpers/pageTestingHelper";
+import compiledOperatorsQuery, {
+  operatorsQuery,
+} from "__generated__/operatorsQuery.graphql";
+import { Operators } from "../../../pages/cif/operators";
 jest.mock("next/router");
 
 mocked(useRouter).mockReturnValue({
@@ -25,9 +16,6 @@ mocked(useRouter).mockReturnValue({
   query: {},
   push: jest.fn(),
 } as any);
-
-let environment: RelayMockEnvironment;
-let initialQueryRef;
 
 const defaultMockResolver = {
   Query() {
@@ -45,10 +33,11 @@ const defaultMockResolver = {
   },
 };
 
-const loadOperatorsQuery = (
-  mockResolver: MockResolvers = defaultMockResolver
-) => {
-  const variables: operatorsQuery$variables = {
+const pageTestingHelper = new PageTestingHelper<operatorsQuery>({
+  pageComponent: Operators,
+  compiledQuery: compiledOperatorsQuery,
+  defaultQueryResolver: defaultMockResolver,
+  defaultQueryVariables: {
     legalName: null,
     tradeName: null,
     bcRegistryId: null,
@@ -56,66 +45,38 @@ const loadOperatorsQuery = (
     offset: null,
     pageSize: DEFAULT_PAGE_SIZE,
     orderBy: null,
-  };
-
-  environment.mock.queueOperationResolver((operation) => {
-    return MockPayloadGenerator.generate(operation, mockResolver);
-  });
-
-  environment.mock.queuePendingOperation(OperatorsQuery, variables);
-  initialQueryRef = loadQuery<operatorsQuery>(
-    environment,
-    OperatorsQuery,
-    variables
-  );
-};
-let errorContext;
-const renderOperators = () =>
-  render(
-    <ErrorContext.Provider value={errorContext}>
-      <RelayEnvironmentProvider environment={environment}>
-        <Operators CSN preloadedQuery={initialQueryRef} />
-      </RelayEnvironmentProvider>
-    </ErrorContext.Provider>
-  );
+  },
+});
 
 describe("The operators page", () => {
   beforeEach(() => {
-    environment = createMockEnvironment();
-    errorContext = {
-      error: null,
-      setError: jest.fn().mockImplementation((error) =>
-        act(() => {
-          errorContext.error = error;
-        })
-      ),
-    };
+    pageTestingHelper.reinit();
   });
 
   it("renders the list of operators", () => {
-    loadOperatorsQuery();
-    renderOperators();
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
 
     expect(screen.getByText(/Operator 1/i)).toBeInTheDocument();
     expect(screen.getByText(/Operator 2/i)).toBeInTheDocument();
   });
 
   it("displays the Add a Operator Button", () => {
-    loadOperatorsQuery();
-    renderOperators();
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
 
     expect(screen.getByText(/Add an Operator/i)).toBeInTheDocument();
   });
 
   it("displays an error when the Edit button is clicked & createNewOperator mutation fails", () => {
-    loadOperatorsQuery();
-    renderOperators();
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
 
     userEvent.click(screen.getByText(/Add an Operator/i));
     act(() => {
-      environment.mock.rejectMostRecentOperation(new Error());
+      pageTestingHelper.environment.mock.rejectMostRecentOperation(new Error());
     });
-    expect(errorContext.setError).toHaveBeenCalledTimes(1);
+    expect(pageTestingHelper.errorContext.setError).toHaveBeenCalledTimes(1);
     expect(
       screen.getByText(
         "An error occurred while attempting to create the operator."
@@ -124,7 +85,7 @@ describe("The operators page", () => {
   });
 
   it("displays the Resume Operator Creation button if there is an existing form_change", () => {
-    loadOperatorsQuery({
+    pageTestingHelper.loadQuery({
       Query() {
         return {
           ...defaultMockResolver.Query(),
@@ -134,7 +95,7 @@ describe("The operators page", () => {
         };
       },
     });
-    renderOperators();
+    pageTestingHelper.renderPage();
 
     expect(screen.getByText(/Resume Operator Creation/i)).toBeInTheDocument();
   });
