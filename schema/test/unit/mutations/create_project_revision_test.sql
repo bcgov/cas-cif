2f708d30a4250e392b384fb01d9eccdcf5cb9d45
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-05-02 17:06:02 -0700

    test: truncate cif.project table related dependencies

--- schema/test/unit/mutations/create_project_revision_test.sql
+++ schema/test/unit/mutations/create_project_revision_test.sql
@@ -5,7 +5,7 @@ select no_plan();
 truncate table
 cif.project, cif.project_contact,
 cif.project_manager, cif.project_revision,
-cif.operator, cif.contact, cif.form_change, cif.attachment
+cif.operator, cif.contact, cif.form_change, cif.attachment, cif.reporting_requirement, cif.budget_item, cif.payment
 restart identity;
 
 insert into cif.cif_user(id, uuid, given_name, family_name)
