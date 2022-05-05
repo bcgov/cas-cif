014e516ea121409c6e6cc74695a7a56731f585b1
Brianna Cerkiewicz <brianna@button.is>
2022-05-05 15:07:21 -0600

    refactor: fixing computed column snake case

--- schema/test/unit/computed_columns/project_revision_project_quarterly_report_form_changes_test.sql
+++ schema/test/unit/computed_columns/project_revision_project_quarterly_report_form_changes_test.sql
@@ -8,20 +8,20 @@ insert into cif.project_revision(id, change_status)
 insert into cif.form_change(id, new_form_data, operation, form_data_schema_name, form_data_table_name, project_revision_id,json_schema_name)
   overriding system value
   values
-    (1, '{"report_type":"Not Quarterly","test_data": "record for not quarterly report"}','create', 'cif', 'reporting_requirement', 1, 'reporting_requirement'),
-    (2, '{"report_type":"Quarterly","test_data": "record for some_other_table"}','create', 'cif', 'some_other_table', 1, 'some_other_table'),
-    (3, '{"report_type":"Quarterly", "test_data": "record for quarterly report"}','create', 'cif', 'reporting_requirement', 1, 'reporting_requirement'),
-    (4, '{"report_type":"Quarterly", "test_data": "record for quarterly report"}','create', 'cof', 'reporting_requirement', 1, 'reporting_requirement'),
-    (5, '{"report_type":"Quarterly", "test_data": "record for quarterly report on other revision"}','create', 'cif', 'reporting_requirement', 2, 'reporting_requirement'),
-    (6, '{"report_type":"Not Quarterly", "test_data": "record for quarterly report on other revision"}','create', 'cif', 'reporting_requirement', 2, 'reporting_requirement'),
-    (7, '{"report_type":"Not Quarterly", "test_data": "record for quarterly report on other revision"}','create', 'cif', 'reporting_requirement', 3, 'reporting_requirement');
+    (1, '{"reportType":"Not Quarterly","testData": "record for not quarterly report"}','create', 'cif', 'reporting_requirement', 1, 'reporting_requirement'),
+    (2, '{"reportType":"Quarterly","testData": "record for some_other_table"}','create', 'cif', 'some_other_table', 1, 'some_other_table'),
+    (3, '{"reportType":"Quarterly", "testData": "record for quarterly report"}','create', 'cif', 'reporting_requirement', 1, 'reporting_requirement'),
+    (4, '{"reportType":"Quarterly", "testData": "record for quarterly report"}','create', 'cof', 'reporting_requirement', 1, 'reporting_requirement'),
+    (5, '{"reportType":"Quarterly", "testData": "record for quarterly report on other revision"}','create', 'cif', 'reporting_requirement', 2, 'reporting_requirement'),
+    (6, '{"reportType":"Not Quarterly", "testData": "record for quarterly report on other revision"}','create', 'cif', 'reporting_requirement', 2, 'reporting_requirement'),
+    (7, '{"reportType":"Not Quarterly", "testData": "record for quarterly report on other revision"}','create', 'cif', 'reporting_requirement', 3, 'reporting_requirement');
 
 select is(
   (
     with record as (
       select row(project_revision.*)::cif.project_revision
       from cif.project_revision where id=1
-    ) select new_form_data->>'test_data' from cif.project_revision_project_quarterly_report_form_changes((select * from record))
+    ) select new_form_data->>'testData' from cif.project_revision_project_quarterly_report_form_changes((select * from record))
   ),
   'record for quarterly report',
   'Only returns the form change record for the revision and the project quarterly report table'
@@ -32,7 +32,7 @@ select is(
     with record as (
       select row(project_revision.*)::cif.project_revision
       from cif.project_revision where id=3
-    ) select new_form_data->>'test_data' from cif.project_revision_project_quarterly_report_form_changes((select * from record))
+    ) select new_form_data->>'testData' from cif.project_revision_project_quarterly_report_form_changes((select * from record))
   ),
   null,
   'Returns null if there are no quarterly reports for that project revision'
