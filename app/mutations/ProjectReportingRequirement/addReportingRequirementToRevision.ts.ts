27e08df2e83073a4c2adcf4598a224aeb775734e
Brianna Cerkiewicz <brianna@button.is>
2022-05-05 12:59:07 -0600

    feat: make addReportingRequirementToRevision mutation

---
+++ app/mutations/ProjectReportingRequirement/addReportingRequirementToRevision.ts.ts
@@ -0,0 +1,40 @@
+import { graphql } from "react-relay";
+import useMutationWithErrorMessage from "mutations/useMutationWithErrorMessage";
+import { addReportingRequirementToRevisionMutation } from "__generated__/addReportingRequirementToRevisionMutation.graphql";
+
+export const mutation = graphql`
+  mutation addReportingRequirementToRevisionMutation(
+    $connections: [ID!]!
+    $projectRevisionId: Int!
+    $newFormData: JSON!
+  ) {
+    createFormChange(
+      input: {
+        formDataSchemaName: "cif"
+        formDataTableName: "reporting_requirement"
+        jsonSchemaName: "reporting_requirement"
+        operation: CREATE
+        projectRevisionId: $projectRevisionId
+        newFormData: $newFormData
+      }
+    ) {
+      formChangeEdge @appendEdge(connections: $connections) {
+        cursor
+        node {
+          id
+          newFormData
+          projectRevisionByProjectRevisionId {
+            ...ProjectQuarterlyReportsForm_projectRevision
+            ...TaskList_projectRevision
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const useAddReportingRequirementToRevision = () =>
+  useMutationWithErrorMessage<addReportingRequirementToRevisionMutation>(
+    mutation,
+    () => "An error occurred while attempting to create the project revision."
+  );
