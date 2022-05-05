158a0fea81daf8e8ea628e10d29986a90eb13da6
Brianna Cerkiewicz <brianna@button.is>
2022-05-05 10:30:14 -0600

    feat: create reporting requirement schema

--- app/data/jsonSchemaForm/projectReportingRequirementSchema.ts
+++ app/data/jsonSchemaForm/projectReportingRequirementSchema.ts
@@ -1,22 +1,14 @@
 const schema = {
   $schema: "http://json-schema.org/draft-07/schema",
   type: "object",
-  title: "Quarterly-Report",
-  required: ["quarterlyReportId", "dueDate"],
+  title: "Reporting Requirement",
+  required: ["dueDate"],
   properties: {
-    quarterlyReportId: {
-      type: "number",
-      title: "Quarterly Report",
-      default: undefined,
-      anyOf: undefined,
-    },
     dueDate: {
       type: "string",
-      title: "Due Date",
-      //brianna -- set quarterly dates here?
+      title: "Report Due Date",
       default: undefined,
     },
-    // brianna --is this received date? there's also completion date
     submittedDate: {
       type: "string",
       title: "Submitted/Received Date",
