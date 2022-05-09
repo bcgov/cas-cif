cd4ee16fb0e6320b752184d26261e0f83280898a
Brianna Cerkiewicz <brianna@button.is>
2022-05-09 16:30:38 -0600

    test: add quarterly reports to project revision db test

--- schema/test/unit/mutations/create_project_revision_test.sql
+++ schema/test/unit/mutations/create_project_revision_test.sql
@@ -20,6 +20,18 @@ values ('test operator');
 
 select cif.create_project();
 
+insert into cif.form_change(new_form_data,
+    operation,
+    form_data_schema_name,
+    form_data_table_name,
+    form_data_record_id,
+    project_revision_id,
+    change_status,
+    json_schema_name)
+    values ('{"status":"on_track","projectId":1, "reportType": "Quarterly"}','create','cif','reporting_requirement',null,1,'pending','reporting_requirement'),
+    ('{"projectId":1,"cifUserId":1,"projectManagerLabelId":1}','create','cif','project_manager',null,1,'pending','project_manager');
+
+
 update cif.form_change
 set new_form_data =
 '{
@@ -41,15 +53,6 @@ set new_form_data =
 }'::jsonb
 where form_data_table_name = 'project_contact';
 
-update cif.form_change
-set new_form_data =
-'{
-  "cifUserId": 1,
-  "projectId": 1,
-  "projectManagerLabelId": 1
-}'::jsonb
-where form_data_table_name = 'project_manager';
-
 update cif.project_revision set change_status = 'committed';
 
 select results_eq(
@@ -67,8 +70,14 @@ select results_eq(
   where form_data_table_name = 'project' and project_revision_id = 2
   $$,
   $$
-  select new_form_data from cif.form_change
-  where form_data_table_name = 'project' and project_revision_id = 1
+  select '{
+    "projectName": "name",
+    "summary": "lorem ipsum",
+    "fundingStreamRfpId": 1,
+    "projectStatusId": 1,
+    "proposalReference": "1235",
+    "operatorId": 1
+  }'::jsonb
   $$,
   'creating a new project revision should create a form_change record for the project'
 );
@@ -79,8 +88,11 @@ select results_eq(
   where form_data_table_name = 'project_manager' and project_revision_id = 2
   $$,
   $$
-  select new_form_data from cif.form_change
-  where form_data_table_name = 'project_manager' and project_revision_id = 1
+  select '{
+    "projectId":1,
+    "cifUserId":1,
+    "projectManagerLabelId":1
+    }'::jsonb
   $$,
   'creating a new project revision should create a form_change record for the project_manager'
 );
@@ -91,11 +103,30 @@ select results_eq(
   where form_data_table_name = 'project_contact' and project_revision_id = 2
   $$,
   $$
-  select new_form_data from cif.form_change
-  where form_data_table_name = 'project_contact' and project_revision_id = 1
+  select '{
+    "contactId": 1,
+    "contactIndex": 0,
+    "projectId": 1
+  }'::jsonb
   $$,
   'creating a new project revision should create a form_change record for the project_contact'
 );
+
+select results_eq(
+  $$
+  select new_form_data from cif.form_change
+  where form_data_table_name = 'reporting_requirement' and project_revision_id = 2
+  $$,
+  $$
+  select '{
+    "status":"on_track",
+    "projectId":1,
+    "reportType": "Quarterly"
+    }'::jsonb
+  $$,
+  'creating a new project revision should create a form_change record for the reporting_requirement'
+);
+
 
 select results_eq(
 $$
