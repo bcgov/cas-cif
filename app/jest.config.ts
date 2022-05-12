04135b2442a868f6a6a024ef47424ad20543e377
mikevespi <mike@button.is>
2022-05-12 08:13:16 -0700

    chore: handle jest error on css imports

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -73,7 +73,9 @@ const config = {
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/tests/cssFileMock.js",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
