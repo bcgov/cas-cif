e748a9ad1da3636bf8f5d86b85dd1f9d139b540f
Dylan Leard <dylan@button.is>
2022-05-16 14:59:24 -0700

    chore: create dynamic form_changes_for computed column

---
+++ schema/deploy/computed_columns/project_revision_form_changes_for.sql
@@ -0,0 +1,25 @@
+-- Deploy cif:computed_columns/project_revision_form_changes_for to pg
+-- requires: tables/form_change
+-- requires: tables/project_revision
+
+begin;
+
+create or replace function cif.project_revision_form_changes_for(cif.project_revision, form_data_table_name text, json_matcher jsonb default '{}')
+returns setof cif.form_change
+as
+$computed_column$
+
+  select *
+    from cif.form_change
+    where project_revision_id = $1.id
+      and form_data_schema_name='cif'
+      and form_data_table_name=$2
+      and new_form_data::jsonb @> $3::jsonb;
+
+$computed_column$ language sql stable;
+
+grant execute on function cif.project_revision_project_quarterly_report_form_changes to cif_internal, cif_external, cif_admin;
+
+comment on function cif.project_revision_project_quarterly_report_form_changes is 'Computed column to dynamically retrieve the form_change records relating to the project_revision from a given table';
+
+commit;
