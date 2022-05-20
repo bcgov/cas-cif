e08ff1af9f30a55bdbfe8863340f11f74cd34c0f
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-05-19 17:33:22 -0700

    test: updating quarterly report forms

--- app/tests/unit/components/Form/ProjectQuarterlyReportForm.test.tsx
+++ app/tests/unit/components/Form/ProjectQuarterlyReportForm.test.tsx
@@ -20,14 +20,22 @@ const testQuery = graphql`
 
 const defaultMockResolver = {
   ProjectRevision(context, generateID) {
+    const firstFormId = `mock-project-quarterly-report-form-${generateID()}`;
     return {
       id: `mock-proj-rev-${generateID()}`,
       rowId: 1234,
+      upcomingReportingRequirementFormChange: {
+        id: firstFormId,
+        reportingRequirement: {
+          reportDueDate: "2022-01-01",
+          reportingRequirementIndex: 1,
+        },
+      },
       projectQuarterlyReportFormChanges: {
         edges: [
           {
             node: {
-              id: `mock-project-quarterly-report-form-${generateID()}`,
+              id: firstFormId,
               newFormData: {
                 status: "on_track",
                 reportDueDate: "2022-01-01",
@@ -48,7 +56,7 @@ const defaultMockResolver = {
                 comments: "some comments",
                 projectId: 51,
                 reportType: "Quarterly",
-                quasubmittedDate: "2022-05-02",
+                submittedDate: "2022-05-02",
               },
               operation: "CREATE",
               changeStatus: "pending",
@@ -86,13 +94,16 @@ describe("The ProjectQuarterlyReportForm", () => {
     componentTestingHelper.reinit();
   });
 
-  it("Renders two quarterly reports with remove buttons", () => {
+  it("Renders two quarterly reports with remove buttons, and the report due indicator", () => {
     componentTestingHelper.loadQuery();
     componentTestingHelper.renderComponent();
 
     expect(screen.getAllByRole("textbox")).toHaveLength(2);
 
     expect(screen.getAllByText("Remove")).toHaveLength(2);
+    expect(screen.getAllByRole("group")[0]).toHaveTextContent(
+      /Overdue by \d+ day\(s\)/
+    );
   });
 
   it("Calls the addQuarterlyReportToRevision mutation when the Add button is clicked", () => {
