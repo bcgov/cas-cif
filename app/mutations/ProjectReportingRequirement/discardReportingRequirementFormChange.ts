1f980e66ee62e16e9ba79e085b74c586bd5c97f0
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-05-25 11:14:04 -0700

    test: testing the configurable delete hook

--- app/mutations/ProjectReportingRequirement/discardReportingRequirementFormChange.ts
+++ app/mutations/ProjectReportingRequirement/discardReportingRequirementFormChange.ts
@@ -27,78 +27,8 @@ const discardMutation = graphql`
 const useDeleteReportingRequirementFormChange = () =>
   useMutationWithErrorMessage<discardReportingRequirementFormChangeMutation>(
     discardMutation,
-    () => "An error occurred when deleting a reporting requirement."
+    () => "An error occurred when deleting."
   );
-
-// interface DiscardFormChangeOptions {
-//   formChange: {
-//     id: string;
-//     operation: "CREATE" | "UPDATE" | "ARCHIVE";
-//   };
-//   onCompleted?: (
-//     response:
-//       | updateReportingRequirementFormChangeMutation$data
-//       | discardReportingRequirementFormChangeMutation$data,
-//     errors: PayloadError[]
-//   ) => void;
-//   onError?: (error: Error) => void;
-// }
-
-// const useDiscardFormChange = (
-//   reportType: string,
-//   connectionId: string
-// ): [(options: DiscardFormChangeOptions) => void, boolean] => {
-//   // Disabling the rules of hooks below is okay, as long as they are not disabled
-//   // in the component that uses this hook.
-//   const [deleteFormChange, isDeleting] = useMutationWithErrorMessage(
-//     discardMutation,
-//     () => "An error occurred while deleting"
-//   );
-//   const [updateFormChange, isUpdating] =
-//     useUpdateReportingRequirementFormChange();
-
-//   const discardFormChange = (options: DiscardFormChangeOptions) => {
-//     const { formChange, onCompleted, onError } = options;
-//     if (formChange.operation === "CREATE") {
-//       const variables: discardReportingRequirementFormChangeMutation$variables =
-//         {
-//           input: {
-//             id: formChange.id,
-//           },
-//           reportType: reportType,
-//           connections: [connectionId],
-//         };
-
-//       deleteFormChange({ variables, onCompleted, onError });
-//     } else {
-//       updateFormChange({
-//         variables: {
-//           input: {
-//             id: formChange.id,
-//             formChangePatch: {
-//               operation: "ARCHIVE",
-//             },
-//           },
-//           reportType: reportType,
-//         },
-//         optimisticResponse: {
-//           updateFormChange: {
-//             formChange: {
-//               id: formChange.id,
-//               operation: "ARCHIVE",
-//               newFormData: {},
-//             },
-//           },
-//         },
-//         onCompleted,
-//         onError,
-//         debounceKey: formChange.id,
-//       });
-//     }
-//   };
-//   const isDiscarding = isDeleting || isUpdating;
-//   return [discardFormChange, isDiscarding];
-// };
 
 const useDiscardReportingRequirementFormChange = (
   reportType: string,
