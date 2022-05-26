a6878277737b6d0b31902af1488410a5ce89d058
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-05-26 16:56:45 -0700

    chore: add undo_form_changes sql function and tests

--- schema/deploy/mutations/undo_form_changes.sql
+++ schema/deploy/mutations/undo_form_changes.sql
@@ -12,19 +12,28 @@ declare
   return_value cif.project_revision;
 
 begin
+  -- Since project revision is the same for all form changes, we can just use the first one
+  select * from cif.project_revision where id = (select project_revision_id from cif.form_change where id = $1[1]) into return_value;
+
   foreach form_change_id in ARRAY $1 loop
     select * from cif.form_change where id = form_change_id into fc;
     if fc.previous_form_change_id is not null then
-      update cif.form_change set new_form_data = (select new_form_data from cif.form_change where id = fc.previous_form_change_id) where id = fc.id;
+      update cif.form_change set operation = 'update', new_form_data = (select new_form_data from cif.form_change where id = fc.previous_form_change_id)
+        where id = fc.id;
     else
-      update cif.form_change set new_form_data = '{}' where id = fc.id;
+    -- we need to treat project overview table differently as it needs an empty object as form data
+      if fc.form_data_table_name = 'project' then
+        update cif.form_change set new_form_data = '{}' where id = fc.id;
+      else
+        delete from cif.form_change where id = fc.id;
+      end if;
     end if;
   end loop;
-  -- Since project revision is the same for all form changes, we can just use the first one
-  select * from cif.project_revision where id = (select project_revision_id from cif.form_change where id = $1[1]) into return_value;
   return return_value;
 end
 
 $$ language plpgsql strict;
+
+grant execute on function cif.undo_form_changes to cif_internal, cif_external, cif_admin;
 
 commit;
