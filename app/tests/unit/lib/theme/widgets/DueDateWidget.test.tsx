f17d65aa8b958cb5d4a469097f13ddcc58e35739
Mike Vesprini <mike@button.is>
2022-05-26 15:07:26 -0700

    test: add test for different timezone

--- app/tests/unit/lib/theme/widgets/DueDateWidget.test.tsx
+++ app/tests/unit/lib/theme/widgets/DueDateWidget.test.tsx
@@ -1,6 +1,6 @@
 import DueDateWidget from "lib/theme/widgets/DueDateWidget";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { DateTime } from "luxon";
+import { DateTime, Settings } from "luxon";
 import getTimestamptzFromDate from "lib/helpers/getTimestamptzFromDate";
 import userEvent from "@testing-library/user-event";
 
@@ -28,7 +28,7 @@ describe("The DueDateWidget", () => {
       required: true,
     };
     render(<DueDateWidget id="widget-id" {...props} />);
-    expect(screen.getByText(`Jan. 01, 2020`)).toBeInTheDocument();
+    expect(screen.getByText(/jan[.]? 01, 2020/i)).toBeInTheDocument();
     fireEvent.click(screen.getByLabelText(/report due date/i));
     userEvent.selectOptions(screen.getAllByRole("combobox")[0], "11");
     userEvent.selectOptions(screen.getAllByRole("combobox")[1], "2050");
@@ -63,7 +63,7 @@ describe("The DueDateWidget", () => {
       id: "test-id",
       onChange: jest.fn(),
       label: "Report Due Date",
-      value: "1999-01-01T23:59:59.999-08:00",
+      value: "1999-01-01T23:59:59.999-07:00",
       required: true,
     };
     render(<DueDateWidget {...props} />);
@@ -71,5 +71,30 @@ describe("The DueDateWidget", () => {
     expect(screen.getByLabelText(/report due date/i)).toHaveTextContent(
       /Jan[.]? 01, 1999/
     );
+  });
+
+  it("saves the date in PST when user is in another timezone", () => {
+    Settings.defaultZone = "America/Edmonton";
+    const expectedDate = "2050-12-15T23:59:59.999-08:00";
+    const handleOnChange = jest.fn();
+    const props: any = {
+      id: "test-id",
+      onChange: handleOnChange,
+      label: "Report Due Date",
+      value: "2020-01-01T23:59:59.999-07:00",
+      required: true,
+    };
+    render(<DueDateWidget id="widget-id" {...props} />);
+    expect(screen.getByText(/jan[.]? 01, 2020/i)).toBeInTheDocument();
+    fireEvent.click(screen.getByLabelText(/report due date/i));
+    userEvent.selectOptions(screen.getAllByRole("combobox")[0], "11");
+    userEvent.selectOptions(screen.getAllByRole("combobox")[1], "2050");
+    userEvent.click(
+      screen.getByRole("option", {
+        name: /choose thursday, december 15th, 2050/i,
+      })
+    );
+    expect(handleOnChange).toHaveBeenCalledWith(expectedDate);
+    Settings.defaultZone = "America/Vancouver";
   });
 });
