69433d75aff5889c218b8b5936e4125ec4483f25
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-05-26 17:01:52 -0700

    chore: add mutation to undo form changes

---
+++ app/mutations/FormChange/undoFormChanges.ts
@@ -0,0 +1,25 @@
+import useMutationWithErrorMessage from "mutations/useMutationWithErrorMessage";
+import { undoFormChangesMutation } from "undoFormChangesMutation.graphql";
+import { graphql } from "react-relay";
+
+const mutation = graphql`
+  mutation undoFormChangesMutation($input: UndoFormChangesInput!) {
+    undoFormChanges(input: $input) {
+      projectRevision {
+        ...ProjectForm_projectRevision
+        ...ProjectManagerFormGroup_revision
+        ...ProjectContactForm_projectRevision
+        ...ProjectQuarterlyReportForm_projectRevision
+      }
+    }
+  }
+`;
+
+const useUndoFormChanges = () => {
+  return useMutationWithErrorMessage<undoFormChangesMutation>(
+    mutation,
+    () => "An error occurred when undoing the form changes."
+  );
+};
+
+export { mutation, useUndoFormChanges };
