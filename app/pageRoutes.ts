3924c6751006052bb08487d13df1d5bbd90a7df7
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-05-31 15:51:14 -0700

    refactor: removing unused pages

--- app/pageRoutes.ts
+++ app/pageRoutes.ts
@@ -80,27 +80,6 @@ export const getProjectViewPageRoute = (projectId: string) => ({
   },
 });
 
-export const getAttachmentsPageRoute = (projectId: string) => ({
-  pathname: "/cif/project/[project]/attachments",
-  query: {
-    project: projectId,
-  },
-});
-
-export const getAttachmentUploadPageRoute = (projectId: string) => ({
-  pathname: `/cif/project/[project]/upload-attachment`,
-  query: {
-    project: projectId,
-  },
-});
-
-export const getAttachmentViewPageRoute = (attachmentId: string) => ({
-  pathname: `/cif/attachments/[attachment]`,
-  query: {
-    attachment: attachmentId,
-  },
-});
-
 export const getAttachmentDownloadRoute = (attachmentId: string) => ({
   pathname: `/download/${attachmentId}`,
 });
