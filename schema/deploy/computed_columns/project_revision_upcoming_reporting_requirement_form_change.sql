d60e9833a3b1213e3d2d0713852cbbf7c3cf827d
Brianna Cerkiewicz <brianna@button.is>
2022-06-20 14:06:02 -0700

    Revert "chore: spec work on computed column"
    This reverts commit 24bacaf9da283d1edd8515757ec61ab95f311632.

--- schema/deploy/computed_columns/project_revision_upcoming_reporting_requirement_form_change.sql
+++ schema/deploy/computed_columns/project_revision_upcoming_reporting_requirement_form_change.sql
@@ -3,18 +3,17 @@
 begin;
 
 
-create or replace function cif.project_revision_upcoming_reporting_requirement_form_change(project_revision cif.project_revision, report_type text[])
+create or replace function cif.project_revision_upcoming_reporting_requirement_form_change(project_revision cif.project_revision, report_type text default null)
 returns cif.form_change
 as
 $computed_column$
 
--- Update the project_revision_upcoming_reporting_requirement_form_change computed column to accept an array of values: this can be done by using the string_to_array function of postgres, and check for new_form_data->>'reportType'=any(...array...)
 
   select * from cif.form_change
   where project_revision_id = $1.id
     and form_data_schema_name='cif'
     and form_data_table_name='reporting_requirement'
-    and ($2 is null or new_form_data->>'reportType'=any(...report_type...))
+    and ($2 is null or new_form_data->>'reportType' ilike ('%' || $2))
     and new_form_data->>'submittedDate' is null
   order by (new_form_data->>'reportDueDate')::timestamptz asc
   fetch first row only
