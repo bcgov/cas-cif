95986a8e5cb70c2aa2375de4ca6162fc6806ee4e
Matthieu Foucault <matthieu@button.is>
2022-06-22 14:59:32 -0700

    refactor: delete `allProjectManagerFormChangesByLabel` computed column

--- schema/revert/computed_columns/project_revision_all_project_manager_form_changes_by_label.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:computed_columns/project_revision_all_project_manager_form_changes_by_label from pg
-
-begin;
-
-drop function cif.project_revision_all_project_manager_form_changes_by_label;
-
-commit;
--- schema/verify/computed_columns/project_revision_all_project_manager_form_changes_by_label.sql
+++
@@ -1,7 +0,0 @@
--- Verify cif:computed_columns/project_revision_all_project_manager_form_changes_by_label on pg
-
-begin;
-
-select pg_get_functiondef('cif.project_revision_all_project_manager_form_changes_by_label(cif.project_revision)'::regprocedure);
-
-rollback;
