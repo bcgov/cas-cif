1a1eb84ef39c42239ae949416a0b1433f6e18b03
Dylan Leard <dylan@button.is>
2022-07-04 16:23:33 -0700

    chore: add comment to getConsolidatedMilestoneFormData function

--- app/components/Form/Functions/projectMilestoneFormFunctions.tsx
+++ app/components/Form/Functions/projectMilestoneFormFunctions.tsx
@@ -7,6 +7,18 @@ import { milestoneReportingRequirementUiSchema } from "data/jsonSchemaForm/proje
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+/**
+  This function ingests all the sets of form_change edges that are required to render a milestone report. It matches
+  milestone & payment form_change records to a reporting_requirement form_change record by the reportingRequirementId field &
+  consolidates those records into a consolidated object. This function then returns a set of those consolidated objects.
+
+  @param {Object[]} reportingRequirementEdges - The set of edges from form_changes for the reporting_requirement table
+  @param {Object[]} milestoneEdges - The set of edges from form_changes for the milestone_report table
+  @param {Object[]} paymentEdges - The set of edges from form_changes for the payment table
+  @returns {Object[]} consolidatedFormDataArray - The set of consolidated form_change data objects. Each Object contains matched
+  form_changes for the reporting_requirement, milestone_report and payment tables.
+*/
+
 export const getConsolidatedMilestoneFormData = (
   reportingRequirementEdges,
   milestoneEdges,
