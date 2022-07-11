9d493713b8513c1e1f00acc39d511a87bba1578a
Dylan Leard <dylan@button.is>
2022-07-11 14:27:35 -0700

    chore: add example flag

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,6 +4,7 @@ import { graphql, usePreloadedQuery } from "react-relay/hooks";
 import { pagesQuery } from "__generated__/pagesQuery.graphql";
 import defaultRelayOptions from "lib/relay/withRelayOptions";
 import { getUserGroupLandingRoute } from "lib/userGroups";
+import { useFeature } from "@growthbook/growthbook-react";
 
 export const IndexQuery = graphql`
   query pagesQuery {
@@ -17,11 +18,13 @@ export const IndexQuery = graphql`
 
 function Index({ preloadedQuery }: RelayProps<{}, pagesQuery>) {
   const { query } = usePreloadedQuery(IndexQuery, preloadedQuery);
+  const showFlaggedItem = useFeature("flagged-item").on;
 
   return (
     <DefaultLayout session={query.session}>
       <div>
         <p>Welcome to the cif web application.</p>
+        {showFlaggedItem && <p>Hello there. I am a flag!</p>}
       </div>
     </DefaultLayout>
   );
