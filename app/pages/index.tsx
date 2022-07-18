de266fffb965637037daa5068dfa43a0ab6d6ce4
Dylan Leard <dylan@button.is>
2022-07-18 15:12:04 -0700

    chore: remove example flag

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,7 +4,6 @@ import { graphql, usePreloadedQuery } from "react-relay/hooks";
 import { pagesQuery } from "__generated__/pagesQuery.graphql";
 import defaultRelayOptions from "lib/relay/withRelayOptions";
 import { getUserGroupLandingRoute } from "lib/userGroups";
-import { useFeature } from "@growthbook/growthbook-react";
 
 export const IndexQuery = graphql`
   query pagesQuery {
@@ -18,13 +17,11 @@ export const IndexQuery = graphql`
 
 function Index({ preloadedQuery }: RelayProps<{}, pagesQuery>) {
   const { query } = usePreloadedQuery(IndexQuery, preloadedQuery);
-  const showFlaggedItem = useFeature("flagged-item").on;
 
   return (
     <DefaultLayout session={query.session}>
       <div>
         <p>Welcome to the cif web application.</p>
-        {showFlaggedItem && <p>Hello there. I am a flag!</p>}
       </div>
     </DefaultLayout>
   );
