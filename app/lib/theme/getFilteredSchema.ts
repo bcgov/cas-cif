c08877b19a07f6e3c256b9efbf9888cbc989b6ed
Dylan Leard <dylan@button.is>
2022-07-21 14:44:32 -0700

    chore: rename confusing parameter in getFilteredSchema

--- app/lib/theme/getFilteredSchema.ts
+++ app/lib/theme/getFilteredSchema.ts
@@ -2,17 +2,17 @@ import type { JSONSchema7 } from "json-schema";
 
 // Filter out fields from the formSchema that have not changed from the previous revision so the summary ignores these fields
 // This is mainly used when the form has multiple fields within it and we want to check each field data with the previous revision
-export const getFilteredSchema = (formSchema: JSONSchema7, formData) => {
+export const getFilteredSchema = (formSchema: JSONSchema7, formChange) => {
   const filteredSchema = JSON.parse(JSON.stringify(formSchema));
   const newDataObject = {};
 
   for (const key of Object.keys(filteredSchema.properties)) {
     if (
-      formData?.newFormData?.[key] ===
-      formData?.formChangeByPreviousFormChangeId?.newFormData?.[key]
+      formChange?.newFormData?.[key] ===
+      formChange?.formChangeByPreviousFormChangeId?.newFormData?.[key]
     )
       delete filteredSchema.properties[key];
-    else newDataObject[key] = formData.newFormData?.[key];
+    else newDataObject[key] = formChange.newFormData?.[key];
   }
 
   return { formSchema: filteredSchema, formData: newDataObject };
