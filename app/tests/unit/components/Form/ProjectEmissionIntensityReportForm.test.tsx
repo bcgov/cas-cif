4023bfb49668e98e0bbb619192e953efb8cdb331
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-07-20 17:25:42 -0700

    test: test skeleton for teimp forms

---
+++ app/tests/unit/components/Form/ProjectEmissionIntensityReportForm.test.tsx
@@ -0,0 +1,11 @@
+describe("The emission intensity report form component", () => {
+  it("Renders a create button that calls the addEmissionIntensityReportToRevision mutation, when there is no report on that project revision", () => {});
+  it("Uses useMutationWithErrorMessage and returns expected message when the user clicks the Add button and there's a mutation error", () => {});
+  it("Validates all milestone forms when the submit button is clicked");
+  it("stages the form changes when the `submit` button is clicked");
+  it("reverts the form_change status to 'pending' when editing");
+  it(
+    "calls the undoFormChangesMutation when the user clicks the Undo Changes button"
+  );
+  it("calls the updateformchange mutation when the user types in data");
+});
---
+++ app/tests/unit/components/Form/ProjectEmissionIntensityReportFormSummary.test.tsx
@@ -0,0 +1,7 @@
+describe("The Project Emission Intensity Report Form Summary Component", () => {
+  it(
+    "displays a 'not added' message when there was no TEIMP information added"
+  );
+  it("displays a 'not updated' when there were no updates to the form");
+  it("only displays the fields that were updated on the summary");
+});
