06ca8fbb1026dc9a39ceba1cef27b37ad75b4fbc
Brianna Cerkiewicz <brianna@button.is>
2022-08-03 15:30:15 -0700

    test: form_change_as_emission_intensity_report computed column

--- schema/test/unit/computed_columns/emission_intensity_report_calculated_ei_performance_test.sql
+++ schema/test/unit/computed_columns/emission_intensity_report_calculated_ei_performance_test.sql
@@ -1,6 +1,21 @@
+
+
 begin;
 
 select plan(5);
+
+insert into cif.operator (id, legal_name, trade_name, bc_registry_id, operator_code)
+overriding system value
+values (1, 'first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD');
+
+insert into cif.project(id, operator_id, funding_stream_rfp_id, project_status_id, proposal_reference, summary, project_name)
+overriding system value
+values (1, 1, 1, 1, '000', 'summary', 'project 1');
+
+insert into cif.reporting_requirement(id,report_due_date, comments, project_id, report_type, reporting_requirement_index)
+overriding system value
+values (1,'2020-01-01', 'comment_1', 1, 'General Milestone',1);
+
 
 insert into cif.emission_intensity_report(id, reporting_requirement_id, baseline_emission_intensity, target_emission_intensity, post_project_emission_intensity) overriding system value
   values (1, 1,1,2,3), (2, 1, null,2,3),(3,1,1,null,3),(4,1,1,2,null);
@@ -21,6 +36,7 @@ select is(
   null,
   'Returns null when baseline_emission_intensity is null but all other values exist'
 );
+
 select is(
   (
     select cif.emission_intensity_report_calculated_ei_performance((select row(emission_intensity_report.*)::cif.emission_intensity_report from cif.emission_intensity_report where id=3))
@@ -28,6 +44,7 @@ select is(
   null,
   'Returns null when target_emission_intensity is null but all other values exist'
 );
+
 select is(
   (
     select cif.emission_intensity_report_calculated_ei_performance((select row(emission_intensity_report.*)::cif.emission_intensity_report from cif.emission_intensity_report where id=4))
---
+++ schema/test/unit/computed_columns/form_change_as_emission_intensity_report_test.sql
@@ -0,0 +1,87 @@
+
+
+
+begin;
+
+select plan(3);
+
+/** SETUP **/
+truncate table cif.change_status restart identity cascade;
+
+insert into cif.change_status (status, triggers_commit, active)
+values
+  ('pending', false, true);
+
+insert into cif.project_revision(id, change_status)
+  overriding system value
+  values (1, 'pending');
+
+insert into cif.form_change(id, operation, form_data_schema_name, form_data_table_name, form_data_record_id, project_revision_id, json_schema_name, new_form_data)
+  overriding system value
+  values
+    (
+      1,
+      'create',
+      'cif',
+      'emission_intensity_report',
+      1,
+      1,
+      'emission_intensity_report',
+      '{
+          "baselineEmissionIntensity":"50"
+
+        }'
+    ),
+    (2, 'create', 'cif', 'operator', 1, 1, 'operator', '{"legalName": "I am an operator"}');
+/** SETUP END **/
+
+
+select is(
+  (
+    with record as (
+    select row(form_change.*)::cif.form_change
+    from cif.form_change where id=1
+    ) select pg_typeof(cif.form_change_as_emission_intensity_report((select * from record)))::text
+  ),
+  (
+    'cif.emission_intensity_report'::text
+  ),
+  'Returns a record of type emission_intensity_report'
+);
+
+select results_eq(
+  $$
+    with record as (
+    select row(form_change.*)::cif.form_change
+    from cif.form_change where id=1
+    )
+    select
+      id,
+      baseline_emission_intensity
+    from cif.form_change_as_emission_intensity_report((select * from record))
+  $$,
+  $$
+    values (
+      -1,
+      50.00
+      )
+  $$,
+  'Returns a record populated with the data from the form_change''s emission_intensity_report field, and id of -1'
+);
+
+
+select is(
+  (
+    with record as (
+    select row(form_change.*)::cif.form_change
+    from cif.form_change where id=2
+    ) select id from cif.form_change_as_emission_intensity_report((select * from record))
+  ),
+  null,
+  'Returns null when passed a form_change record with a form_data_table_name that is not emission_intensity_report'
+);
+
+
+select finish();
+
+rollback;
