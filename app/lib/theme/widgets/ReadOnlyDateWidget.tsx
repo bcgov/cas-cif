75b41a3b5f6b3c243f69fce27704349390189328
Mike Vesprini <mike@button.is>
2022-08-09 16:59:15 -0700

    feat: add content suffix to read only date widget

--- app/lib/theme/widgets/ReadOnlyDateWidget.tsx
+++ app/lib/theme/widgets/ReadOnlyDateWidget.tsx
@@ -1,14 +1,18 @@
 import { WidgetProps } from "@rjsf/core";
 import { getLocaleFormattedDate } from "../getLocaleFormattedDate";
 
-const ReadOnlyDateWidget: React.FC<WidgetProps> = ({ value }) => {
+const ReadOnlyDateWidget: React.FC<WidgetProps> = ({ value, options }) => {
   return (
     <>
       <dd>{value ? getLocaleFormattedDate(value) : <em>Not added</em>}</dd>
+      <dd className="contentSuffix">{value && options.contentSuffix}</dd>
       <style jsx>{`
         dd {
           line-height: 1.2;
           margin: 0;
+        }
+        .contentSuffix {
+          margin-left: 1em;
         }
       `}</style>
     </>
