f4b5a51f0adf26c5b2b3850f815634a26a0b0396
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-08-11 09:27:28 -0700

    chore: adding missing comments on new table fields

--- schema/deploy/tables/emission_intensity_report_001.sql
+++ schema/deploy/tables/emission_intensity_report_001.sql
@@ -7,4 +7,8 @@ alter table cif.emission_intensity_report
   add column adjusted_holdback_payment_amount numeric,
   add column date_sent_to_csnr timestamptz;
 
+comment on column cif.emission_intensity_report.adjusted_holdback_payment_amount is 'An override field to manually adjust the calculated TEIMP holdback payment amount';
+comment on column cif.emission_intensity_report.date_sent_to_csnr is 'The date the payment was issued';
+
+
 commit;
