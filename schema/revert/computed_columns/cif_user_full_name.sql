bd13e1555f7769279fd520bd409d9829789bf2a7
Mike Vesprini <mike@button.is>
2022-08-16 16:31:46 -0700

    chore: change output of cif_user_full_name to first last

--- schema/revert/computed_columns/cif_user_full_name.sql
+++ schema/revert/computed_columns/cif_user_full_name.sql
@@ -1,7 +1,24 @@
--- Revert cif:computed_columns/full_name from pg
+-- Deploy cif:computed_columns/full_name to pg
+-- requires: tables/cif_user
 
 BEGIN;
 
-drop function cif.cif_user_full_name(cif.cif_user);
+create or replace function cif.cif_user_full_name(u cif.cif_user)
+returns varchar(1000)
+as
+$computed_column$
+  select concat(
+    u.family_name,
+    (case
+      when u.family_name is not null and u.given_name is not null
+      then ', '
+      else null
+    end),
+    u.given_name);
+$computed_column$ language sql immutable;
+
+grant execute on function cif.cif_user_full_name to cif_internal, cif_admin;
+
+comment on function cif.cif_user_full_name is 'Displays a cif_user as: family_name, given_name';
 
 COMMIT;
