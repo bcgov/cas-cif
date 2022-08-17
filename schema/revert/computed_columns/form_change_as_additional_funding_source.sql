99009d3c58e68d04e8c635d7e9827ce0e3df7780
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-08-17 12:47:59 -0700

    chore: remove redundant computed column

--- schema/revert/computed_columns/form_change_as_additional_funding_source.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:computed_columns/form_change_as_additional_funding_source from pg
-
-begin;
-
-drop function cif.form_change_as_additional_funding_source(cif.form_change);
-
-commit;
--- schema/verify/computed_columns/form_change_as_additional_funding_source.sql
+++
@@ -1,7 +0,0 @@
--- Verify cif:computed_columns/form_change_as_additional_funding_source on pg
-
-begin;
-
-select pg_get_functiondef('cif.form_change_as_additional_funding_source(cif.form_change)'::regprocedure);
-
-rollback;
