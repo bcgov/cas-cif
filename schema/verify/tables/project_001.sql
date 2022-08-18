8dae23c419ad0f3d27499438846b7fdbd2d233e6
Brianna Cerkiewicz <brianna@button.is>
2022-08-18 10:44:49 -0700

    chore: update project_001 verify (missing a column)

--- schema/verify/tables/project_001.sql
+++ schema/verify/tables/project_001.sql
@@ -6,4 +6,8 @@ select column_name
 from information_schema.columns
 where table_schema='cif' and table_name='project' and column_name='score';
 
+select column_name
+from information_schema.columns
+where table_schema='cif' and table_name='project' and column_name='project_type';
+
 rollback;
