8c93732d34cfd50b512c1bdf1b3270c4372ffd0e
Gurjeet Matharu <gurjeet@matharuco.com>
2022-08-18 17:43:43 -0700

    refactor: wip on tasklist config

--- app/components/TaskList/index.tsx
+++ app/components/TaskList/index.tsx
@@ -121,6 +121,256 @@ const TaskList: React.FC<Props> = ({ projectRevision, mode }) => {
     summary: useShowGrowthbookFeature("teimp") ? 8 : 7,
   };
 
+  const projectOverviewSection = (step: string) => {
+    return (
+      <TaskListSection
+        defaultExpandedState={
+          currentStep === "0" ||
+          projectOverviewStatus === ATTENTION_REQUIRED_STATUS
+        }
+        listItemNumber={step}
+        listItemName="Project Overview"
+      >
+        <TaskListItem
+          stepName={step}
+          linkUrl={getProjectRevisionFormPageRoute(id, 0)}
+          formTitle="Project overview"
+          formStatus={projectOverviewStatus}
+          currentStep={currentStep}
+          mode={mode}
+        />
+      </TaskListSection>
+    );
+  };
+
+  const projectDetailsSection = (step: string) => {
+    return (
+      <TaskListSection
+        defaultExpandedState={
+          currentStep === "1" ||
+          currentStep === "2" ||
+          projectManagersStatus === ATTENTION_REQUIRED_STATUS ||
+          projectContactsStatus === ATTENTION_REQUIRED_STATUS
+        }
+        listItemNumber={step}
+        listItemName="Project Details"
+        listItemMode={mode === "update" ? "" : "(optional)"}
+      >
+        <TaskListItem
+          stepName={step}
+          linkUrl={getProjectRevisionFormPageRoute(id, 1)}
+          formTitle="Project managers"
+          formStatus={projectManagersStatus}
+          currentStep={currentStep}
+          mode={mode}
+        />
+        <TaskListItem
+          stepName={step}
+          linkUrl={getProjectRevisionFormPageRoute(id, 2)}
+          formTitle="Project contacts"
+          formStatus={projectContactsStatus}
+          currentStep={currentStep}
+          mode={mode}
+        />
+      </TaskListSection>
+    );
+  };
+
+  const projectBudgetSection = (step: string) => {
+    return (
+      <TaskListSection
+        defaultExpandedState={
+          currentStep === "3" ||
+          fundingAgreementStatus === ATTENTION_REQUIRED_STATUS
+        }
+        listItemNumber={step}
+        listItemName="Budgets, Expenses & Payments"
+      >
+        <TaskListItem
+          stepName={step}
+          linkUrl={getProjectRevisionFormPageRoute(id, 3)}
+          formTitle="budgets"
+          formStatus={fundingAgreementStatus}
+          currentStep={currentStep}
+          mode={mode}
+        />
+      </TaskListSection>
+    );
+  };
+
+  const projectMilestoneSection = (step: string) => {
+    return (
+      <TaskListSection
+        defaultExpandedState={
+          currentStep === "4" ||
+          milestoneReportStatuses.edges.some(
+            ({ node }) =>
+              node.formCompletionStatus === ATTENTION_REQUIRED_STATUS
+          )
+        }
+        listItemNumber={step}
+        listItemName="Milestone Reports"
+      >
+        {milestoneReportStatuses.edges.length === 0 ? (
+          <TaskListItem
+            stepName={step}
+            linkUrl={getProjectRevisionFormPageRoute(id, 4)}
+            formTitle="Milestone reports"
+            formStatus={null} // No status as there are no milestones
+            currentStep={currentStep}
+            mode={mode}
+          />
+        ) : (
+          milestoneReportStatuses.edges.map(({ node }, index) => (
+            <TaskListItem
+              key={node.milestoneIndex}
+              stepName={step}
+              linkUrl={getProjectRevisionFormPageRoute(
+                id,
+                4,
+                `Milestone${index + 1}`
+              )}
+              formTitle={`Milestone ${index + 1}`}
+              formStatus={node.formCompletionStatus}
+              milestoneDueDate={displayMilestoneDueDateStatus(
+                node.reportDueDate,
+                node.submittedDate
+              )}
+              currentStep={currentStep}
+              mode={mode}
+              hasAnchor={true}
+            />
+          ))
+        )}
+      </TaskListSection>
+    );
+  };
+
+  const projectQuarterlyReportsSection = (step: string) => {
+    return (
+      <TaskListSection
+        defaultExpandedState={
+          currentStep === "5" ||
+          quarterlyReportsStatus === ATTENTION_REQUIRED_STATUS
+        }
+        listItemNumber={step}
+        listItemName="Quarterly Reports"
+      >
+        <TaskListItem
+          stepName={step}
+          linkUrl={getProjectRevisionFormPageRoute(id, 5)}
+          formTitle="Quarterly reports"
+          formStatus={quarterlyReportsStatus}
+          currentStep={currentStep}
+          mode={mode}
+        />
+      </TaskListSection>
+    );
+  };
+  // {useShowGrowthbookFeature("teimp") && (
+  const projectEmissionsIntensityReportSection = (step: string) => {
+    return (
+      <TaskListSection
+        defaultExpandedState={
+          currentStep === "6" ||
+          quarterlyReportsStatus === ATTENTION_REQUIRED_STATUS
+        }
+        listItemNumber={step}
+        listItemName="Emissions Intensity Report"
+      >
+        <TaskListItem
+          stepName={step}
+          linkUrl={getProjectRevisionFormPageRoute(id, 6)}
+          formTitle="Emissions Intensity Report"
+          formStatus={quarterlyReportsStatus}
+          currentStep={currentStep}
+          mode={mode}
+        />
+      </TaskListSection>
+    );
+  };
+
+  const projectAnnualReportSection = (step: string) => {
+    return (
+      <TaskListSection
+        defaultExpandedState={
+          currentStep === String(sectionIndex.annual) ||
+          annualReportsStatus === ATTENTION_REQUIRED_STATUS
+        }
+        listItemNumber={step}
+        listItemName="Annual Reports"
+      >
+        <TaskListItem
+          stepName={String(sectionIndex.annual)}
+          linkUrl={getProjectRevisionFormPageRoute(id, sectionIndex.annual)}
+          formTitle="Annual reports"
+          formStatus={annualReportsStatus}
+          currentStep={currentStep}
+          mode={mode}
+        />
+      </TaskListSection>
+    );
+  };
+
+  const projectSummarySection = () => {
+    return (
+      <TaskListSection
+        defaultExpandedState={currentStep === "summary"}
+        listItemNumber={String(sectionIndex.summary)}
+        listItemName="Submit changes"
+      >
+        <TaskListItem
+          stepName="summary"
+          linkUrl={getProjectRevisionPageRoute(id)}
+          formTitle="Review and submit information"
+          formStatus={null}
+          currentStep={currentStep}
+          mode={mode}
+        />
+      </TaskListSection>
+    );
+  };
+
+  const projectAttachmentSection = () => {
+    return (
+      <AttachmentsTaskListSection
+        icon={faPaperclip}
+        title="Attachments"
+        linkUrl={getProjectRevisionAttachmentsPageRoute(id)}
+      />
+    );
+  };
+
+  let shouldRenderAttachments = () => {
+    if (mode === "view") return true;
+  };
+
+  const config = [
+    { section: projectOverviewSection, render: true },
+    { section: projectDetailsSection, render: true },
+    { section: projectBudgetSection, render: true },
+    { section: projectMilestoneSection, render: true },
+    { section: projectEmissionsIntensityReportSection, render: true },
+    { section: projectQuarterlyReportsSection, render: true },
+    {
+      section: projectAnnualReportSection,
+      render: useShowGrowthbookFeature("teimp"),
+    },
+    {
+      section: projectSummarySection,
+      render: useShowGrowthbookFeature("teimp"),
+    },
+    {
+      section: projectAttachmentSection,
+      render: shouldRenderAttachments(),
+    },
+  ];
+
+  var newConfig = config.reduce(function (acc, curr) {
+    if (curr.render === true) acc.push(curr.section);
+    return acc;
+  }, []);
+
   return (
     <div className="container">
       <h2>
@@ -131,205 +381,9 @@ const TaskList: React.FC<Props> = ({ projectRevision, mode }) => {
           : "Add a Project"}
       </h2>
       <ol>
-        {/* Project Overview Section */}
-        <TaskListSection
-          defaultExpandedState={
-            currentStep === "0" ||
-            projectOverviewStatus === ATTENTION_REQUIRED_STATUS
-          }
-          listItemNumber="1"
-          listItemName="Project Overview"
-        >
-          <TaskListItem
-            stepName="0"
-            linkUrl={getProjectRevisionFormPageRoute(id, 0)}
-            formTitle="Project overview"
-            formStatus={projectOverviewStatus}
-            currentStep={currentStep}
-            mode={mode}
-          />
-        </TaskListSection>
-
-        {/* Project Details Section */}
-        <TaskListSection
-          defaultExpandedState={
-            currentStep === "1" ||
-            currentStep === "2" ||
-            projectManagersStatus === ATTENTION_REQUIRED_STATUS ||
-            projectContactsStatus === ATTENTION_REQUIRED_STATUS
-          }
-          listItemNumber="2"
-          listItemName="Project Details"
-          listItemMode={mode === "update" ? "" : "(optional)"}
-        >
-          <TaskListItem
-            stepName="1"
-            linkUrl={getProjectRevisionFormPageRoute(id, 1)}
-            formTitle="Project managers"
-            formStatus={projectManagersStatus}
-            currentStep={currentStep}
-            mode={mode}
-          />
-          <TaskListItem
-            stepName="2"
-            linkUrl={getProjectRevisionFormPageRoute(id, 2)}
-            formTitle="Project contacts"
-            formStatus={projectContactsStatus}
-            currentStep={currentStep}
-            mode={mode}
-          />
-        </TaskListSection>
-
-        {/* Budget Details Section */}
-        <TaskListSection
-          defaultExpandedState={
-            currentStep === "3" ||
-            fundingAgreementStatus === ATTENTION_REQUIRED_STATUS
-          }
-          listItemNumber="3"
-          listItemName="Budgets, Expenses & Payments"
-        >
-          <TaskListItem
-            stepName="3"
-            linkUrl={getProjectRevisionFormPageRoute(id, 3)}
-            formTitle="budgets"
-            formStatus={fundingAgreementStatus}
-            currentStep={currentStep}
-            mode={mode}
-          />
-        </TaskListSection>
-
-        {/* Milestone Reports Section */}
-        <TaskListSection
-          defaultExpandedState={
-            currentStep === "4" ||
-            milestoneReportStatuses.edges.some(
-              ({ node }) =>
-                node.formCompletionStatus === ATTENTION_REQUIRED_STATUS
-            )
-          }
-          listItemNumber="4"
-          listItemName="Milestone Reports"
-        >
-          {milestoneReportStatuses.edges.length === 0 ? (
-            <TaskListItem
-              stepName="4"
-              linkUrl={getProjectRevisionFormPageRoute(id, 4)}
-              formTitle="Milestone reports"
-              formStatus={null} // No status as there are no milestones
-              currentStep={currentStep}
-              mode={mode}
-            />
-          ) : (
-            milestoneReportStatuses.edges.map(({ node }, index) => (
-              <TaskListItem
-                key={node.milestoneIndex}
-                stepName="4"
-                linkUrl={getProjectRevisionFormPageRoute(
-                  id,
-                  4,
-                  `Milestone${index + 1}`
-                )}
-                formTitle={`Milestone ${index + 1}`}
-                formStatus={node.formCompletionStatus}
-                milestoneDueDate={displayMilestoneDueDateStatus(
-                  node.reportDueDate,
-                  node.submittedDate
-                )}
-                currentStep={currentStep}
-                mode={mode}
-                hasAnchor={true}
-              />
-            ))
-          )}
-        </TaskListSection>
-
-        {/* Quarterly Reports Section */}
-        <TaskListSection
-          defaultExpandedState={
-            currentStep === "5" ||
-            quarterlyReportsStatus === ATTENTION_REQUIRED_STATUS
-          }
-          listItemNumber="5"
-          listItemName="Quarterly Reports"
-        >
-          <TaskListItem
-            stepName="5"
-            linkUrl={getProjectRevisionFormPageRoute(id, 5)}
-            formTitle="Quarterly reports"
-            formStatus={quarterlyReportsStatus}
-            currentStep={currentStep}
-            mode={mode}
-          />
-        </TaskListSection>
-
-        {/* Emissions Intensity Report */}
-        {/* Growthbook - teimp */}
-        {useShowGrowthbookFeature("teimp") && (
-          <TaskListSection
-            defaultExpandedState={
-              currentStep === "6" ||
-              quarterlyReportsStatus === ATTENTION_REQUIRED_STATUS
-            }
-            listItemNumber="6"
-            listItemName="Emissions Intensity Report"
-          >
-            <TaskListItem
-              stepName="6"
-              linkUrl={getProjectRevisionFormPageRoute(id, 6)}
-              formTitle="Emissions Intensity Report"
-              formStatus={quarterlyReportsStatus}
-              currentStep={currentStep}
-              mode={mode}
-            />
-          </TaskListSection>
-        )}
-
-        {/* Annual Reports Section */}
-        <TaskListSection
-          defaultExpandedState={
-            currentStep === String(sectionIndex.annual) ||
-            annualReportsStatus === ATTENTION_REQUIRED_STATUS
-          }
-          listItemNumber={String(sectionIndex.annual)}
-          listItemName="Annual Reports"
-        >
-          <TaskListItem
-            stepName={String(sectionIndex.annual)}
-            linkUrl={getProjectRevisionFormPageRoute(id, sectionIndex.annual)}
-            formTitle="Annual reports"
-            formStatus={annualReportsStatus}
-            currentStep={currentStep}
-            mode={mode}
-          />
-        </TaskListSection>
-
-        {/* Project Summary Section */}
-        {mode !== "view" && (
-          <TaskListSection
-            defaultExpandedState={currentStep === "summary"}
-            listItemNumber={String(sectionIndex.summary)}
-            listItemName="Submit changes"
-          >
-            <TaskListItem
-              stepName="summary"
-              linkUrl={getProjectRevisionPageRoute(id)}
-              formTitle="Review and submit information"
-              formStatus={null}
-              currentStep={currentStep}
-              mode={mode}
-            />
-          </TaskListSection>
-        )}
-
-        {/* Attachments Section */}
-        {mode === "view" && (
-          <AttachmentsTaskListSection
-            icon={faPaperclip}
-            title="Attachments"
-            linkUrl={getProjectRevisionAttachmentsPageRoute(id)}
-          />
-        )}
+        {newConfig.map((section, index) => {
+          return section(index + 1);
+        })}
       </ol>
       <style jsx>{`
         ol {
