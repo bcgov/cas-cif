c97be655373102e2530f0ceb15cbd9ed1f09a1b9
Brianna Cerkiewicz <77306764+BCerki@users.noreply.github.com>
2022-08-19 16:23:31 -0400

    chore: apply suggestions from code review
    Co-authored-by: Dylan Leard <dylan@button.is>

--- schema/deploy/data_migration/data_migration_add_general_revision.sql
+++ schema/deploy/data_migration/data_migration_add_general_revision.sql
@@ -1,25 +1,17 @@
 -- Deploy cif:data_migration/data_migration_add_general_revision to pg
 
-BEGIN;
 begin;
 
 create or replace function cif.add_general_revision()
   returns void as $$
-declare
-  temp_row record;
-begin
-    alter table cif.project_revision disable trigger _100_committed_changes_are_immutable;
 
-    for temp_row in
-      select id from cif.project_revision
-     loop
-      update cif.project_revision
-      set revision_type='General Revision';
-    end loop;
+    alter table cif.project_revision disable trigger _100_committed_changes_are_immutable, disable trigger _100_timestamps;
 
-    alter table cif.project_revision enable trigger _100_committed_changes_are_immutable;
-end;
-$$ language plpgsql volatile;
+update cif.project_revision set revision_type = 'General Revision'
+
+    alter table cif.project_revision enable trigger _100_committed_changes_are_immutable, enable trigger _100_timestamps;
+
+$$ language sql volatile;
 
 -- Data migration: add 'General Revision' revision type to all existing project revisions
 select cif.add_general_revision();
--- schema/deploy/tables/project_revision_001.sql
+++ schema/deploy/tables/project_revision_001.sql
@@ -2,7 +2,7 @@
 
 begin;
 
-alter table cif.project_revision add column revision_type varchar(1000) references cif.revision_type(type), add column comments varchar(10000);
+alter table cif.project_revision add column revision_type varchar(1000) not null references cif.revision_type(type), add column comments varchar(10000);
 
 comment on column cif.project_revision.revision_type is 'The type of the project revision (e.g. General Revision)';
 comment on column cif.project_revision.comments is 'Comments on the project revision';
