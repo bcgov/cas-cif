a26f5cd0c424f1e8a0c41065cfc88e1b870b0ab8
Brianna Cerkiewicz <brianna@button.is>
2022-08-22 07:32:14 -0700

    chore: add number widget to themes

--- app/lib/theme/FormWithTheme.tsx
+++ app/lib/theme/FormWithTheme.tsx
@@ -8,14 +8,12 @@ import SearchDropdownWidget from "lib/theme/widgets/SearchDropdownWidget";
 import DisplayOnlyWidget from "lib/theme/widgets/DisplayOnlyWidget";
 import SelectWidget from "lib/theme/widgets/SelectWidget";
 import SelectParentWidget from "lib/theme/widgets/SelectParentWidget";
-import MoneyWidget from "lib/theme/widgets/MoneyWidget";
 import PhoneNumberWidget from "lib/theme/widgets/PhoneNumberWidget";
 import DueDateWidget from "lib/theme/widgets/DueDateWidget";
 import DateWidget from "./widgets/DateWidget";
-import DecimalWidget from "./widgets/DecimalWidget";
 import { AdjustableCalculatedValueWidget } from "./widgets/AdjustableCalculatedValueWidget";
 import ReadOnlyCalculatedValueWidget from "./widgets/ReadOnlyCalculatedValueWidget";
-import PercentageWidget from "./widgets/PercentageWidget";
+import NumberWidget from "./widgets/NumberWidget";
 
 const { fields, widgets } = utils.getDefaultRegistry();
 
@@ -30,14 +28,12 @@ const formTheme: ThemeProps = {
     DisplayOnly: DisplayOnlyWidget,
     SelectWidget: SelectWidget,
     SelectParentWidget: SelectParentWidget,
-    MoneyWidget: MoneyWidget,
     PhoneNumberWidget,
     DueDateWidget: DueDateWidget,
     DateWidget,
     AdjustableCalculatedValueWidget,
     ReadOnlyCalculatedValueWidget,
-    PercentageWidget: PercentageWidget,
-    DecimalWidget,
+    NumberWidget,
   },
   ObjectFieldTemplate: ObjectFieldTemplate,
   FieldTemplate: FieldTemplate,
