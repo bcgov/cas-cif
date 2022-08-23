492fe7f0c2b3feb417269fd3a795d18ad1c400bc
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-08-23 13:44:08 -0700

    test: add sql unit tests

---
+++ schema/test/unit/tables/amendment_type_test.sql
@@ -0,0 +1,102 @@
+begin;
+
+select plan(11);
+
+select has_table('cif', 'amendment_type', 'table cif.amendment_type exists');
+
+select columns_are(
+  'cif',
+  'amendment_type',
+  ARRAY[
+    'name',
+    'created_at',
+    'created_by',
+    'updated_at',
+    'updated_by',
+    'archived_at',
+    'archived_by'
+  ],
+  'columns in cif.amendment_type match expected columns'
+);
+
+-- Test setup
+set jwt.claims.sub to '11111111-1111-1111-1111-111111111111';
+
+-- cif_admin
+set role cif_admin;
+select concat('current user is: ', (select current_user));
+
+select is(
+  (select count(*) from cif.amendment_type),
+  3::bigint,
+    'cif_admin can view all data in amendment_type table'
+);
+
+select lives_ok(
+  $$
+    insert into cif.amendment_type (name) values ('Test Status');
+  $$,
+    'cif_admin can insert data in amendment_type table'
+);
+
+select lives_ok(
+  $$
+    update cif.amendment_type set name='changed by admin' where name='Test Status';
+  $$,
+    'cif_admin can change data in amendment_type table'
+);
+
+select results_eq(
+  $$
+    select count(name) from cif.amendment_type where name= 'changed by admin'
+  $$,
+    ARRAY[1::bigint],
+    'Data was changed by cif_admin'
+);
+
+select throws_like(
+  $$
+    delete from cif.amendment_type where name='changed by admin';
+  $$,
+  'permission denied%',
+    'Administrator cannot delete rows from table amendment_type'
+);
+
+-- cif_internal
+set role cif_internal;
+select concat('current user is: ', (select current_user));
+
+select results_eq(
+  $$
+    select count(*) from cif.amendment_type
+  $$,
+  ARRAY['4'::bigint],
+    'cif_internal can view all data from amendment_type table'
+);
+
+select lives_ok(
+  $$
+    insert into cif.amendment_type (name) values ('Test Status 2');
+  $$,
+    'cif_internal can insert data in amendment_type table'
+);
+
+select lives_ok(
+  $$
+    update cif.amendment_type set name= 'changed by internal' where name='changed by admin';
+  $$,
+    'cif_internal can update data in the amendment_type table'
+);
+
+select throws_like(
+  $$
+    delete from cif.amendment_type where name='changed by internal';
+  $$,
+  'permission denied%',
+    'cif_internal cannot delete rows from amendment_type table'
+);
+
+
+select finish();
+
+rollback;
