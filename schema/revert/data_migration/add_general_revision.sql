0cef43dcc1d0f4be27182a8df7c142f5c4ed55ac
Brianna Cerkiewicz <brianna@button.is>
2022-08-30 13:02:56 -0700

    chore: remove unnecessary migration

--- schema/revert/data_migration/add_general_revision.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:data_migration/add_general_revision from pg
-
-begin;
-
-drop function cif.add_general_revision();
-
-commit;
--- schema/verify/data_migration/add_general_revision.sql
+++
@@ -1,7 +0,0 @@
--- Verify cif:data_migration/add_general_revision on pg
-
-begin;
-
-select pg_get_functiondef('cif.add_general_revision()'::regprocedure);
-
-rollback;
