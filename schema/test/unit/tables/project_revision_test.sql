fc7c0de12f1005bf2938e7aaf0791af99f47f74b
Brianna Cerkiewicz <brianna@button.is>
2022-08-30 12:58:50 -0700

    test: project_revision_test

--- schema/test/unit/tables/project_revision_test.sql
+++ schema/test/unit/tables/project_revision_test.sql
@@ -2,73 +2,29 @@
 
 begin;
 
-select plan(6);
-
+select plan(8);
 
 select has_table('cif', 'project_revision', 'table cif.project_revision exists');
-select columns_are(
-  'cif',
-  'project_revision',
-  ARRAY[
-'id',
-'project_id',
-'change_status',
-'change_reason',
-'is_first_revision',
-'created_by',
-'created_at',
-'updated_by',
-'updated_at',
-'revision_type',
-'comments'
-  ],
-  'columns in cif.project_revision match expected columns'
-);
 
 
 -- Test setup --
-
-insert into cif.operator (id, legal_name, trade_name, bc_registry_id, operator_code)
-overriding system value
-values (1, 'first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD');
-
-insert into cif.operator(id, legal_name)
-overriding system value
-values (1,'test operator');
-
-insert into cif.funding_stream(id, name, description)
-overriding system value
- values (1, 'test funding stream', 'desc');
-
-insert into cif.project(id, project_name, operator_id, funding_stream_rfp_id, project_status_id, proposal_reference, summary)
-overriding system value
-  values (1,
+insert into cif.operator(legal_name) values ('test operator');
+insert into cif.funding_stream(name, description) values ('test funding stream', 'desc');
+insert into cif.project(project_name, operator_id, funding_stream_rfp_id, project_status_id, proposal_reference, summary)
+  values (
     'test-project',
-    1,
-    1,
-    1,
+    (select id from cif.operator limit 1),
+    (select id from cif.funding_stream_rfp limit 1),
+    (select id from cif.project_status limit 1),
     'rfp',
     'summary'
   );
--- insert into cif.revision_type (type)
---   values
---     ('Amendment'),
---     ('General Revision'),
---     ('Minor Revision');
+
 
 -- Trigger tests --
 
 insert into cif.change_status(status, triggers_commit) values ('testcommitted', true), ('testpending', false), ('testpending_2', false);
-
--- project_revision data without revision_type
-insert into cif.project_revision(project_id, change_status) values
-(1, 'testpending'),
-(1, 'testcommitted');
-
--- project_revision data with revision_type
-insert into cif.project_revision(project_id, change_status, revision_type) values
-(2, 'testpending','Amendment'),
-(3, 'testpending','Minor Revision'),
+insert into cif.project_revision(project_id, change_status) values ((select id from cif.project limit 1), 'testpending'), ((select id from cif.project limit 1), 'testcommitted');
 
 select lives_ok(
   $$
@@ -93,19 +49,99 @@ select throws_ok(
   'prevents delete if the change status is committed'
 );
 
--- project_revision_001_add_revision_type tests
+-- test project_revision_001_add_revision_type
+truncate table cif.project_revision cascade;
+
+-- use the contents of the revert file to set the database to a state where revision_type isn't required
+alter table cif.project_revision drop column revision_type, drop column comments;
+
+select columns_are(
+  'cif',
+  'project_revision',
+  ARRAY[
+    'id',
+    'project_id',
+    'change_status',
+    'change_reason',
+    'is_first_revision',
+    'created_at',
+    'created_by',
+    'updated_at',
+    'updated_by'
+  ],
+  'revision_type and comments column were reverted and do not exist in cif.project_revision'
+);
+
+insert into cif.project(id, project_name, operator_id, funding_stream_rfp_id, project_status_id, proposal_reference, summary)
+overriding system value
+  values (
+    3,'test-project-333',
+    (select id from cif.operator limit 1),
+    (select id from cif.funding_stream_rfp limit 1),
+    (select id from cif.project_status limit 1),
+    'rfp333',
+    'summary333'
+  ),
+  (
+    4,'test-project-444',
+    (select id from cif.operator limit 1),
+    (select id from cif.funding_stream_rfp limit 1),
+    (select id from cif.project_status limit 1),
+    'rfp444',
+    'summary444'
+  );
+
+insert into cif.project_revision(project_id)
+values
+  (3),
+  (4);
+
+-- deploy the project_revision_001_add_revision_type migration
+alter table cif.project_revision disable trigger _100_committed_changes_are_immutable, disable trigger _100_timestamps;
+
+alter table cif.project_revision
+add column revision_type varchar(1000) not null references cif.revision_type(type) default 'General Revision',
+add column comments varchar(10000);
+
+alter table cif.project_revision enable trigger _100_committed_changes_are_immutable, enable trigger _100_timestamps;
+
+
+select columns_are(
+  'cif',
+  'project_revision',
+  ARRAY[
+    'id',
+    'project_id',
+    'change_status',
+    'change_reason',
+    'is_first_revision',
+    'comments',
+    'created_at',
+    'created_by',
+    'updated_at',
+    'updated_by',
+    'revision_type'
+  ],
+  'columns in cif.project_revision match expected columns after migration project_revision_001_add_revision_type'
+);
+
 
 select is(
   (select count(*) from cif.project_revision where revision_type='General Revision'),
   2::bigint,
   'project_revision_001_add_revision_type adds the General Revision default revision_type to revisions that do not have a type');
 
+
+truncate table cif.project_revision cascade;
+insert into cif.project_revision(project_id, revision_type)
+values
+  (3, 'Amendment'),
+  (4, 'Minor Revision');
+
 select is(
-  (select count(*) from cif.project_revision where revision_type=!'General Revision'),
-  2::bigint,
-  'project_revision_001_add_revision_type does not overwrite existing revision_type');
-
-
+  (select count(*) from cif.project_revision where revision_type='General Revision'),
+  0::bigint,
+  'project_revision_001_add_revision_type does not change the revision_type of projects that already have revision types');
 
 select finish();
 
