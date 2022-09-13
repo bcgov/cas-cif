74598ed117e4cb74635ff83d253ce417e47428cc
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-09-13 15:26:30 -0700

    chore: add amendments and revisions fixture

---
+++ schema/data/dev/009_cif_project_revision_logs.sql
@@ -0,0 +1,60 @@
+begin;
+
+do $$
+  declare
+  temp_project_id int;
+  temp_project_revision_id int;
+
+  begin
+    for temp_project_id in select id from cif.project
+    loop
+      -- first Amendment type project revision
+      select id from cif.create_project_revision(temp_project_id) into temp_project_revision_id;
+      update cif.project_revision
+        set revision_type = 'Amendment',
+        change_status = 'committed',
+        amendment_status = 'Approved'
+        where id = temp_project_revision_id;
+
+      insert into cif.project_revision_amendment_type(
+        project_revision_id,
+        amendment_type
+      )
+      values
+      (temp_project_revision_id, 'Scope'),
+      (temp_project_revision_id, 'Schedule');
+
+      -- first General Revision type project revision
+      select id from cif.create_project_revision(temp_project_id) into temp_project_revision_id;
+      update cif.project_revision
+        set revision_type = 'General Revision',
+        change_status = 'committed',
+        amendment_status = 'Approved'
+        where id = temp_project_revision_id;
+
+      -- first Minor Revision type project revision
+      select id from cif.create_project_revision(temp_project_id) into temp_project_revision_id;
+      update cif.project_revision
+        set revision_type = 'Minor Revision',
+        change_status = 'committed',
+        amendment_status = 'Approved'
+        where id = temp_project_revision_id;
+
+      -- second Amendment type project revision
+      select id from cif.create_project_revision(temp_project_id) into temp_project_revision_id;
+      update cif.project_revision
+        set revision_type = 'Amendment',
+        change_status = 'pending',
+        amendment_status = 'Pending Province Approval'
+        where id = temp_project_revision_id;
+
+      insert into cif.project_revision_amendment_type(
+        project_revision_id,
+        amendment_type
+      )
+      values
+      (temp_project_revision_id, 'Schedule');
+    end loop;
+  end
+$$;
+commit;
