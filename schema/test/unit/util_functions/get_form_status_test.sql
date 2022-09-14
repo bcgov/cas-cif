c214b31422340fcb5e82af29feb0fa09942468ad
Gurjeet Matharu <gurjeet@matharuco.com>
2022-09-14 14:49:29 -0700

    test: adding db unit tests for get_form_status

--- schema/test/unit/util_functions/get_form_status_test.sql
+++ schema/test/unit/util_functions/get_form_status_test.sql
@@ -4,71 +4,110 @@ begin;
 
 select * from no_plan();
 
-truncate cif.project restart identity cascade;
+truncate cif.form_change, cif.project_revision, cif.project_contact restart identity cascade;
 
-insert into cif.cif_user(id, uuid)
-  overriding system value
-  values (1, '11111111-1111-1111-1111-111111111111');
+select cif.create_project();
 
-insert into cif.operator (id, legal_name, trade_name, bc_registry_id, operator_code)
-overriding system value
-values
-  (1, 'first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD');
 
-insert into cif.project(id, operator_id, funding_stream_rfp_id, project_status_id, proposal_reference, summary, project_name)
-overriding system value
-values
-  (1, 1, 1, 1, '000', 'summary', 'project 1'), (2, 1, 1, 1, '001', 'summary', 'project 2');
+/* test 1 */
+update cif.form_change set new_form_data='{}'::jsonb
+  where project_revision_id=1
+    and form_data_table_name='project';
 
-insert into cif.project_revision(id, change_status, change_reason, project_id)
-overriding system value
-values
-  (1, 'pending', 'reason for change', 1),
-  (2, 'staged', 'reason for change', 2);
-
-insert into cif.form_change(id, change_status, new_form_data, operation, form_data_schema_name, form_data_table_name, json_schema_name, validation_errors, previous_form_change_id, project_revision_id)
-overriding system value
-values (
-    1,
-    'pending',
-  '{}',
-  'create',
-  'cif',
-  'project',
-  'schema',
-  '[]',
-  null,
-  1
-),(
-    2,
-    'pending',
-  '{"testField": "test value"}',
-  'create',
-  'cif',
-  'test_table',
-  'schema',
-  '[]',
-  null,
-  2
+select results_eq(
+  $$
+  select cif.get_form_status(1, 'project')
+  $$,
+  $$
+  values('Not Started')
+  $$,
+  'Returns Not Started when the project form is empty and the form is pristine'
 );
--- ,(
---     3,
---     'staged',
---   '{"testField": "updated test value"}',
---   'create',
---   'cif',
---   'test_table',
---   'schema',
---   '[]',
---   2,
---   2
--- );
 
+/* test 2 */
+insert into cif.form_change(
+  new_form_data,
+  operation,
+  form_data_schema_name,
+  form_data_table_name,
+  json_schema_name,
+  change_status,
+  project_revision_id
+)
+  values
+(
+  json_build_object(
+    'projectId', 1,
+    'contactId', 1,
+    'contactIndex', 1
+  ),
+  'create', 'cif', 'project_contact', 'project_contact', 'pending', 1
+);
+select results_eq(
+  $$
+  select cif.get_form_status(1, 'project_contact')
+  $$,
+  $$
+  values('In Progress')
+  $$,
+  'Returns In Progress when the project form is not empty and in pending status'
+);
 
---
-select is((select cif.get_form_status(1, 'project')), 'Not Started');
-select is((select cif.get_form_status(2, 'test_table')), 'In Progress');
--- select is((select cif.get_form_status('3', 'test_table')), 'Filled');
+/* test 3 */
+update cif.form_change set change_status='staged'
+  where project_revision_id=1
+    and form_data_table_name='project_contact';
+select results_eq(
+  $$
+  select cif.get_form_status(1, 'project_contact')
+  $$,
+  $$
+  values('Filled')
+  $$,
+  'Returns Filled when the project form staged'
+);
+
+/* test 4 */
+update cif.form_change set change_status='staged', validation_errors='["error"]'::jsonb
+  where project_revision_id=1
+    and form_data_table_name='project_contact';
+select results_eq(
+  $$
+  select cif.get_form_status(1, 'project_contact')
+  $$,
+  $$
+  values('Attention Required')
+  $$,
+  'Returns Attention Required when there are validation errors and form change is staged'
+);
+
+/* test 5 */
+update cif.form_change set change_status='pending', validation_errors='["error"]'::jsonb
+  where project_revision_id=1
+    and form_data_table_name='project_contact';
+select results_eq(
+  $$
+  select cif.get_form_status(1, 'project_contact')
+  $$,
+  $$
+  values('In Progress')
+  $$,
+  'Returns In Progress when there are validation errors and form change is pending'
+);
+
+/* test 6 */
+update cif.form_change set new_form_data=null, change_status='pending'
+  where project_revision_id=1
+    and form_data_table_name='project';
+select results_eq(
+  $$
+  select cif.get_form_status(1, 'project')
+  $$,
+  $$
+  values('Not Started')
+  $$,
+  'Returns Not Started when new form data is null and form change is pending'
+);
 
 
 select finish();
