b4f5b1b74b62de5edc5ee0ecb46e96cd74d5b8aa
Brianna Cerkiewicz <brianna@button.is>
2022-09-14 13:00:28 -0700

    chore: make the readonly widgets into inputs instead of dd

--- app/lib/theme/widgets/ReadOnlyCalculatedValueWidget.tsx
+++ app/lib/theme/widgets/ReadOnlyCalculatedValueWidget.tsx
@@ -1,41 +1,74 @@
 import { WidgetProps } from "@rjsf/core";
 import NumberFormat from "react-number-format";
 
-const ReadOnlyCalculatedValueWidget: React.FC<WidgetProps> = ({
-  id,
-  formContext,
-  label,
-  uiSchema,
-}) => {
+const ReadOnlyAdjustableCalculatedValueWidget: React.FC<WidgetProps> = (
+  props
+) => {
+  const { id, value, uiSchema, label, formContext } = props;
+
   // If we are using this widget to show numbers as money or percent, we can set `isMoney` or `isPercentage` to true in the uiSchema.
   const isMoney = uiSchema?.isMoney;
   const isPercentage = uiSchema?.isPercentage;
 
+  // If we need to set the amount of decimal places, we can set it in the uiSchema, otherwise there will be no decimal places.
+  const numberOfDecimalPlaces = isMoney
+    ? 2
+    : uiSchema.numberOfDecimalPlaces ?? 0;
+
   const calculatedValue =
     formContext[uiSchema.calculatedValueFormContextProperty];
 
+  const adjustedInputId = `${id}_adjusted`;
+
   return (
-    <>
-      <NumberFormat
-        fixedDecimalScale={isMoney || isPercentage}
-        prefix={isMoney ? "$" : ""}
-        suffix={isPercentage ? "%" : ""}
-        id={id}
-        decimalScale={isMoney || isPercentage ? 2 : 10} //Hardcoded for now, we can change it if we need to
-        value={
-          calculatedValue !== null && calculatedValue !== undefined
-            ? calculatedValue
-            : ""
+    <div>
+      {calculatedValue && (
+        <>
+          {
+            <NumberFormat
+              thousandSeparator
+              fixedDecimalScale={numberOfDecimalPlaces}
+              decimalScale={numberOfDecimalPlaces}
+              id={id}
+              prefix={isMoney ? "$" : ""}
+              suffix={isPercentage ? "%" : ""}
+              value={value}
+              displayType="text"
+            />
+          }
+        </>
+      )}
+      {value !== calculatedValue && (
+        <div className={calculatedValue && "adjustedValue"}>
+          {calculatedValue && <dt>{label} (Adjusted)</dt>}
+          <dd>
+            <NumberFormat
+              thousandSeparator
+              fixedDecimalScale={isMoney}
+              id={adjustedInputId}
+              prefix={isMoney ? "$" : ""}
+              suffix={isPercentage ? "%" : ""}
+              decimalScale={isMoney || isPercentage ? 2 : 10}
+              value={value}
+              displayType="text"
+            />
+          </dd>
+        </div>
+      )}
+      {!value && <em>Not added</em>}
+      <style jsx>{`
+        div.adjustedValue {
+          position: relative;
+          display: flex;
+          right: 12.1rem;
+          margin-top: 0.5em;
         }
-        readOnly
-        aria-label={label}
-        style={{
-          border: "none",
-          padding: "0 0 0 0.75em",
-        }}
-      />
-    </>
+        dt {
+          margin-right: 1em;
+        }
+      `}</style>
+    </div>
   );
 };
 
-export default ReadOnlyCalculatedValueWidget;
+export default ReadOnlyAdjustableCalculatedValueWidget;
