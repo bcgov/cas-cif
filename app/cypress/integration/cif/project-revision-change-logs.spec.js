f8614a4bcc8689303adbf9b3d581021132858d4e
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-09-14 12:15:19 -0700

    test: add cypress test

---
+++ app/cypress/integration/cif/project-revision-change-logs.spec.js
@@ -0,0 +1,32 @@
+describe("the project amendment and revisions page", () => {
+  beforeEach(() => {
+    cy.useMockedTime(new Date("June 10, 2020 09:00:00"));
+    cy.sqlFixture("e2e/dbReset");
+    cy.sqlFixture("dev/001_cif_user");
+    cy.sqlFixture("dev/002_cif_operator");
+    cy.sqlFixture("dev/003_cif_contact");
+    cy.sqlFixture("dev/004_cif_project");
+    cy.sqlFixture("dev/007_commit_project_revision");
+    cy.sqlFixture("dev/009_cif_project_revision_logs");
+    cy.mockLogin("cif_admin");
+  });
+
+  it("displays the list of project amendment and revisions", () => {
+    cy.visit("/cif/projects");
+    cy.findAllByRole("button", { name: /view/i }).first().click();
+    cy.findByText(/Amendments & Other Revisions/i).click();
+    cy.url().should("include", "/project-revision-change-logs");
+    cy.get("h2").contains(/Amendments & Other Revisions/i);
+    cy.happoAndAxe("Amendments & Other Revisions", "view", "main");
+    cy.get("tbody tr").should("have.length", 5);
+    cy.get("[placeholder='Filter']").first().type("Minor");
+    cy.get("button").contains("Apply").click();
+    cy.get("tbody tr").should("have.length", 1);
+    cy.get("button").contains("Clear").click();
+    cy.get("tbody tr").should("have.length", 5);
+    cy.get("thead th").contains("Effective Date").click();
+    cy.get("thead th").contains("Effective Date").click();
+    cy.url().should("include", "orderBy=EFFECTIVE_DATE_DESC"); //just to wait for the page to load
+    cy.get("tbody tr").first().contains("Pending");
+  });
+});
