1a01b51a818e1e6e3009dba34ac529ef31ffe6c8
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-09-15 11:11:25 -0700

    refactor: correct verify files

--- schema/verify/tables/project_001.sql
+++ schema/verify/tables/project_001.sql
@@ -2,12 +2,19 @@
 
 begin;
 
-select column_name
-from information_schema.columns
-where table_schema='cif' and table_name='project' and column_name='score';
-
-select column_name
-from information_schema.columns
-where table_schema='cif' and table_name='project' and column_name='project_type';
+do $$
+  begin
+    assert (
+      select true
+      from information_schema.columns
+      where table_schema='cif' and table_name='project' and column_name='score'
+    ), 'column "score" is not defined on table "project"';
+    assert (
+      select true
+      from information_schema.columns
+      where table_schema='cif' and table_name='project' and column_name='project_type'
+    ), 'column "project_type" is not defined on table "project"';
+  end;
+$$;
 
 rollback;
