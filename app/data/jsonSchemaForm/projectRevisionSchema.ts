3397b6c44621f97830cbfe0db2b83c299b829f38
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-10-13 12:12:35 -0700

    chore: add project revision form schema

---
+++ app/data/jsonSchemaForm/projectRevisionSchema.ts
@@ -0,0 +1,23 @@
+export const projectRevisionSchema = {
+  $schema: "http://json-schema.org/draft-07/schema",
+  type: "object",
+  required: ["revisionType"],
+  properties: {
+    revisionType: {
+      type: "string",
+      title: "Revision Type",
+      default: undefined,
+      anyOf: undefined,
+    },
+    comments: {
+      type: "string",
+      title: "General Comments",
+    },
+  },
+};
+
+export const projectRevisionUISchema = {
+  revisionType: {
+    "ui:widget": "radio",
+  },
+};
