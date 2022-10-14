e8a6775943a1afeb86888eedd6ed051d08d6606d
Gurjeet Matharu <gurjeet@matharuco.com>
2022-10-14 09:05:32 -0700

    test: updating e2e test

--- app/cypress/integration/cif/project-revision-create-revision-with-type.spec.js
+++ app/cypress/integration/cif/project-revision-create-revision-with-type.spec.js
@@ -14,9 +14,19 @@ describe("the project amendment and revisions page", () => {
   it("displays the list of project amendment and revisions", () => {
     cy.visit("/cif/projects");
     cy.findAllByRole("button", { name: /view/i }).first().click();
+    cy.findByRole("button", { name: /Edit/i }).click();
+    cy.findByRole("button", { name: /Submit Changes/i }).click();
+    cy.findByText(/Review and submit information/i).click();
+    cy.findByRole("button", { name: /Discard Project Revision/i }).click();
+    cy.findByText(/Proceed/i).click();
+
+    cy.findByText(/Amendments & Other Revisions/i).click();
     cy.findByText(/New Revision/i).click();
     cy.url().should("include", "/project-revision-create");
     cy.happoAndAxe("Project Revision Create", "view", "main");
+    cy.get("form").contains("Amendment", { matchCase: false });
+    cy.get("form").contains("General Revision", { matchCase: false });
+    cy.get("form").contains("Minor Revision", { matchCase: false });
     cy.get("button").contains("New Revision").click();
     cy.url().should("include", "/form/0");
   });
