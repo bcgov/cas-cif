993efb6438dde16a7ff705159a058f8e5187142d
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2022-10-28 13:03:21 -0700

    chore: update dev data

--- schema/data/dev/001_cif_user.sql
+++ schema/data/dev/001_cif_user.sql
@@ -1,6 +1,6 @@
 begin;
 
-insert into cif.cif_user (uuid, given_name, family_name, email_address)
+insert into cif.cif_user (session_sub, given_name, family_name, email_address)
 values
 
   ('00000000-0000-0000-0000-000000000000', 'cif_internal', 'Testuser', 'cif_internal@somemail.com'),
@@ -9,8 +9,8 @@ values
   ('00000000-0000-0000-0000-000000000003', 'Leslie', 'Knope', 'leslie.knope@gov.bc.ca'),
   ('00000000-0000-0000-0000-000000000004', 'Ron', 'Swanson', 'ronald.ulysses.swanson@gov.bc.ca'),
   ('00000000-0000-0000-0000-000000000005', 'April', 'Ludgate', 'april.ludgate@gov.bc.ca')
-on conflict (uuid) do update set
-uuid = excluded.uuid,
+on conflict (session_sub) do update set
+session_sub = excluded.session_sub,
 given_name = excluded.given_name,
 family_name = excluded.family_name,
 email_address = excluded.email_address;
