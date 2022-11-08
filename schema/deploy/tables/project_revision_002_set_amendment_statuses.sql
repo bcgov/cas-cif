91a513650915576b124563d74a98277c1f293d30
Brianna Cerkiewicz <brianna@button.is>
2022-11-08 12:58:03 -0800

    chore: set default and existing amendment statuses in proj rev tables

---
+++ schema/deploy/tables/project_revision_002_set_amendment_statuses.sql
@@ -0,0 +1,25 @@
+-- Deploy cif:tables/project_revision_002_set_amendment_statuses to pg
+
+begin;
+
+alter table cif.project_revision disable trigger _100_committed_changes_are_immutable, disable trigger _100_timestamps;
+
+alter table cif.project_revision alter column amendment_status set default 'Draft';
+
+update cif.project_revision
+set amendment_status =
+(case
+      when
+        (change_status='pending') or
+        (change_status='staged')
+        then 'Draft'
+    else 'Applied'
+    end);
+
+alter table cif.project_revision enable trigger _100_committed_changes_are_immutable, enable trigger _100_timestamps;
+
+comment on column cif.project_revision.revision_type is 'The type of the project revision (e.g. General Revision)';
+comment on column cif.project_revision.comments is 'Comments on the project revision';
+comment on column cif.project_revision.amendment_status is 'The status of the amendment of a project';
+
+commit;
