e3bfda599a3aa73a4d1696f5650cf1bede4ba5e3
Gurjeet Matharu <gurjeet@matharuco.com>
2022-11-09 15:48:55 -0800

    chore: wip on schemas

--- schema/deploy/mutations/delete_project_revision_and_amendments.sql
+++ schema/deploy/mutations/delete_project_revision_and_amendments.sql
@@ -1,7 +1,8 @@
 -- Deploy cif:mutations/delete_project_revision_and_amendments to pg
 
-BEGIN;
-
--- XXX Add DDLs here.
-
-COMMIT;
+create or replace function cif.delete_project_revision_and_amendments(revision_id integer) returns void as $$
+    begin
+        delete from cif.project_revision_amendment where project_revision_id = revision_id;
+        delete from cif.project_revision where id = revision_id;
+    end;
+$$ LANGUAGE plpgsql;
