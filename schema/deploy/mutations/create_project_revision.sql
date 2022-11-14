762add0b8c147f9965fd9beb4d3c39a370b76f71
Gurjeet Matharu <gurjeet@matharuco.com>
2022-11-14 11:13:47 -0800

    chore: renaming var

--- schema/deploy/mutations/create_project_revision.sql
+++ schema/deploy/mutations/create_project_revision.sql
@@ -10,15 +10,15 @@ as $function$
 declare
   revision_row cif.project_revision;
   form_change_record record;
-  _ammendment_type varchar(1000);
+  amendment_type varchar(1000);
 begin
   insert into cif.project_revision(project_id, change_status, revision_type)
   values ($1, 'pending', $2) returning * into revision_row;
 
-  foreach _ammendment_type in array $3
+  foreach amendment_type in array $3
     loop
       insert into cif.project_revision_amendment_type(project_revision_id, amendment_type)
-      values (revision_row.id, (select name from cif.amendment_type where cif.amendment_type.name = _ammendment_type));
+      values (revision_row.id, (select name from cif.amendment_type where cif.amendment_type.name = amendment_type));
     end loop;
 
   perform cif.create_form_change(
