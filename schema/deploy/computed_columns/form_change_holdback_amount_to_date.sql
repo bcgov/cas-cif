c5b90d8fa4c8675fd3fdeabe3308da1d20dfdc84
Dylan Leard <dylan@button.is>
2022-11-14 16:24:42 -0800

    chore: add holdback_amount_to_date computed column

---
+++ schema/deploy/computed_columns/form_change_holdback_amount_to_date.sql
@@ -0,0 +1,19 @@
+-- Deploy cif:computed_columns/form_change_holdback_amount_to_date to pg
+-- requires: tables/form_change
+
+begin;
+
+create or replace function cif.form_change_holdback_amount_to_date(parameter_fc cif.form_change)
+returns numeric as
+$fn$
+
+  select
+    round(sum(coalesce((fc.new_form_data->>'adjustedHoldbackAmount')::numeric, (fc.new_form_data->>'calculatedHoldbackAmount')::numeric)), 2)
+    from cif.form_change fc
+    where fc.project_revision_id = $1.project_revision_id
+    and (fc.new_form_data->>'hasExpenses')::boolean = true
+    and (fc.new_form_data->>'reportDueDate')::timestamptz <= ($1.new_form_data->>'reportDueDate')::timestamptz;
+
+$fn$ language sql stable;
+
+commit;
