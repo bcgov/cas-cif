1e10ba103038b5b5820f58288e9f3ab36e3dcc09
Dylan Leard <dylan@button.is>
2022-11-14 16:13:33 -0800

    chore: add net_payments_to_date computed column

--- schema/deploy/computed_columns/form_change_net_payments_to_date.sql
+++ schema/deploy/computed_columns/form_change_net_payments_to_date.sql
@@ -1,14 +1,14 @@
--- Deploy cif:computed_columns/form_change_eligible_expenses_to_date to pg
+-- Deploy cif:computed_columns/form_change_net_payments_to_date to pg
 -- requires: tables/form_change
 
 begin;
 
-create or replace function cif.form_change_eligible_expenses_to_date(parameter_fc cif.form_change)
+create or replace function cif.form_change_gross_payments_to_date(parameter_fc cif.form_change)
 returns numeric as
 $fn$
 
   select
-    round(sum((fc.new_form_data->>'totalEligibleExpenses')::numeric), 2)
+    round(sum(coalesce((fc.new_form_data->>'adjustedNetAmount')::numeric, (fc.new_form_data->>'calculatedNetAmount')::numeric)), 2)
     from cif.form_change fc
     where fc.project_revision_id = $1.project_revision_id
     and (fc.new_form_data->>'hasExpenses')::boolean = true
