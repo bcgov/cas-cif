cabf70bbd77a9599458e1b34d0cd8424d71af44c
Brianna Cerkiewicz <brianna@button.is>
2022-11-15 07:53:21 -0800

    chore: set revision_status to Draft on create project revision

--- schema/deploy/mutations/create_project_revision.sql
+++ schema/deploy/mutations/create_project_revision.sql
@@ -13,7 +13,7 @@ declare
   _amendment_type varchar(1000);
 begin
   insert into cif.project_revision(project_id, change_status, revision_type, revision_status)
-  values ($1, 'pending', $2, $3) returning * into revision_row;
+  values ($1, 'pending', $2, 'Draft') returning * into revision_row;
 
   foreach _amendment_type in array $3
     loop
