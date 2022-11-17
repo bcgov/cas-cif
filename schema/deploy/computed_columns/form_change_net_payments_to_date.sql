d56051ddcf2c7dfa1d3e20a62ffa34fee3089cc8
Dylan Leard <dylan@button.is>
2022-11-17 15:11:55 -0800

    chore: us maximumAmount when adjusted and calculated values do not exist

--- schema/deploy/computed_columns/form_change_net_payments_to_date.sql
+++ schema/deploy/computed_columns/form_change_net_payments_to_date.sql
@@ -8,13 +8,14 @@ returns numeric as
 $fn$
 
   select
-    round(sum(coalesce((fc.new_form_data->>'adjustedNetAmount')::numeric, (fc.new_form_data->>'calculatedNetAmount')::numeric)), 2)
+    round(sum(coalesce((fc.new_form_data->>'adjustedNetAmount')::numeric, (fc.new_form_data->>'calculatedNetAmount')::numeric, (fc.new_form_data->>'maximumAmount')::numeric - ((fc.new_form_data->>'maximumAmount')::numeric * (parameter_fc.new_form_data->>'holdbackPercentage')::numeric / 100))), 2)
     from cif.form_change fc
     where fc.project_revision_id = $1.project_revision_id
+    and json_schema_name = 'milestone'
     and (fc.new_form_data->>'hasExpenses')::boolean = true;
 
 $fn$ language sql stable;
 
-comment on function cif.form_change_net_payments_to_date(cif.form_change) is 'Computed column returns cumulative sum of all net payments. Preference for value selection is adjustedNetAmount > calculuatedNetAmount';
+comment on function cif.form_change_net_payments_to_date(cif.form_change) is 'Computed column returns cumulative sum of all net payments. Preference for value selection is adjustedNetAmount > calculuatedNetAmount > amount calculated via maximum milestone amount';
 
 commit;
