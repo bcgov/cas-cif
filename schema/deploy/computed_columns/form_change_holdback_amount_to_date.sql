00a767d07c85457fb7a2bf33df07bea219c99856
Dylan Leard <dylan@button.is>
2022-11-25 14:27:28 -0800

    chore: update functions to ignore incomplete milestones

--- schema/deploy/computed_columns/form_change_holdback_amount_to_date.sql
+++ schema/deploy/computed_columns/form_change_holdback_amount_to_date.sql
@@ -8,7 +8,7 @@ returns numeric as
 $fn$
 
   select
-    round(sum(coalesce((fc.new_form_data->>'adjustedHoldbackAmount')::numeric, (fc.new_form_data->>'calculatedHoldbackAmount')::numeric, (fc.new_form_data->>'maximumAmount')::numeric * (parameter_fc.new_form_data->>'holdbackPercentage')::numeric / 100)), 2)
+    round(sum(coalesce((fc.new_form_data->>'adjustedHoldbackAmount')::numeric, (fc.new_form_data->>'calculatedHoldbackAmount')::numeric, 0)), 2)
     from cif.form_change fc
     where fc.project_revision_id = $1.project_revision_id
     and json_schema_name = 'milestone'
