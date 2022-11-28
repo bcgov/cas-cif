cd6ef5a405df5dff33edbfc2818578f9c5c49b13
SeSo <sobhani.sepehr@gmail.com>
2022-11-28 11:07:36 -0800

    chore: using form change id as a parameter to discard

--- schema/verify/mutations/discard_additional_funding_source_form_change.sql
+++ schema/verify/mutations/discard_additional_funding_source_form_change.sql
@@ -2,6 +2,6 @@
 
 begin;
 
-select pg_get_functiondef('cif.discard_additional_funding_source_form_change(int, int)'::regprocedure);
+select pg_get_functiondef('cif.discard_additional_funding_source_form_change(int)'::regprocedure);
 
 rollback;
