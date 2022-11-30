9e80de2802bf3b3e652e2925a17d343396c33f1e
Brianna Cerkiewicz <brianna@button.is>
2022-11-30 11:36:14 -0800

    chore: apply suggestions from code review

--- schema/deploy/tables/revision_status.sql
+++ schema/deploy/tables/revision_status.sql
@@ -9,10 +9,6 @@ alter table cif.revision_status add column is_amendment_specific boolean;
 update cif.revision_status set is_amendment_specific = false where name in ('Draft', 'Applied');
 update cif.revision_status set is_amendment_specific = true where name not in ('Draft', 'Applied');
 
-alter table cif.project_revision rename amendment_status to revision_status;
-
-
-
 delete from cif.revision_status where name='Approved';
 
 alter table cif.revision_status alter column is_amendment_specific set not null;
