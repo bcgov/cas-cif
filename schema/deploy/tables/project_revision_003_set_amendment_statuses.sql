72e5b1c8dfee5dc4c98b82cb8eedc289ad0f6636
Brianna Cerkiewicz <brianna@button.is>
2022-11-30 11:36:14 -0800

    chore: apply suggestions from code review

--- schema/deploy/tables/project_revision_003_set_amendment_statuses.sql
+++ schema/deploy/tables/project_revision_003_set_amendment_statuses.sql
@@ -4,14 +4,16 @@ begin;
 
 alter table cif.project_revision disable trigger _100_committed_changes_are_immutable, disable trigger _100_timestamps;
 
-alter table cif.project_revision alter column amendment_status set default 'Draft';
-alter table cif.project_revision alter column amendment_status set not null;
+
+alter table cif.project_revision rename amendment_status to revision_status;
+alter table cif.project_revision alter column revision_status set default 'Draft';
+alter table cif.project_revision alter column revision_status set not null;
 
 
 update cif.project_revision
-set amendment_status =
+set revision_status =
 (case
-      when (amendment_status='Approved')
+      when (revision_status='Approved')
         then 'Applied'
       when
         (change_status='pending') or
@@ -24,6 +26,6 @@ alter table cif.project_revision enable trigger _100_committed_changes_are_immut
 
 comment on column cif.project_revision.revision_type is 'The type of the project revision (e.g. General Revision)';
 comment on column cif.project_revision.comments is 'Comments on the project revision';
-comment on column cif.project_revision.amendment_status is 'The status of the amendment of a project';
+comment on column cif.project_revision.revision_status is 'The status of the revision of a project';
 
 commit;
