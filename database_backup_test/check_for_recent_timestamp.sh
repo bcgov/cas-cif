691d0b0616b8b9cc205a02a96718561bfcae03f8
Dylan Leard <dylan@button.is>
2022-12-05 14:05:17 -0800

    chore: add dags & cronjobs to test backup cluster

--- database_backup_test/check_for_recent_timestamp.sh
+++ database_backup_test/check_for_recent_timestamp.sh
@@ -2,7 +2,8 @@
 set -xeuo pipefail
 
 declare -i VALUE
-VALUE=$(psql -qtAX -d cif -c "select count(*) from cif_private.full_backup_log where full_backup_timestamp > now() - interval '1 day'")
+export PGPASSWORD="$3"
+VALUE=$(psql -U postgres -h "$2" -qtAX -d "$1" -c "select count(*) from cif_private.full_backup_log where full_backup_timestamp > now() - interval '1 day'")
 
 if [[ $VALUE -lt 1 ]] ; then
   echo 'no timestamp found'
