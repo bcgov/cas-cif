3957f1cfbbf9b238160e1d164a4a80271bcda6c0
Gurjeet Matharu <gurjeet@matharuco.com>
2022-12-06 17:46:13 -0800

    chore: adding update project revision mutation

--- app/lib/theme/widgets/SelectWithNofifyWidget.tsx
+++ app/lib/theme/widgets/SelectWithNofifyWidget.tsx
@@ -1,6 +1,7 @@
 import { WidgetProps } from "@rjsf/core";
 import Dropdown from "@button-inc/bcgov-theme/Dropdown";
 import { Button } from "@button-inc/bcgov-theme";
+import { useUpdateProjectRevision } from "mutations/ProjectRevision/updateProjectRevision";
 
 interface Option {
   type: string;
@@ -21,6 +22,7 @@ const SelectWithNotifyWidget: React.FunctionComponent<WidgetProps> = (
     required,
     uiSchema,
     value,
+    formContext,
   } = props;
 
   if (!(schema && schema.anyOf && typeof schema.anyOf !== "undefined")) {
@@ -28,29 +30,72 @@ const SelectWithNotifyWidget: React.FunctionComponent<WidgetProps> = (
   }
   const options = schema.anyOf as Array<Option>;
 
+  const [updateProjectRevision, isUpdatingProjectRevision] =
+    useUpdateProjectRevision();
+  const revisionId = formContext.revisionId;
+  const revisionStatus = formContext.revisionStatus;
+  const handleUpdate = () => {
+    return new Promise((resolve, reject) =>
+      updateProjectRevision({
+        variables: {
+          input: {
+            id: revisionId,
+            projectRevisionPatch: {
+              pendingActionsFrom: value,
+            },
+          },
+        },
+        optimisticResponse: {
+          updateProjectRevision: {
+            projectRevision: {
+              id: revisionId,
+              pendingActionsFrom: value,
+            },
+          },
+        },
+        onCompleted: resolve,
+        onError: reject,
+        debounceKey: revisionId,
+      })
+    );
+  };
+
   return (
     <div>
-      <Dropdown
-        id={id}
-        onChange={(e) => onChange(e.target.value || undefined)}
-        placeholder={placeholder}
-        size={(uiSchema && uiSchema["bcgov:size"]) || "large"}
-        required={required}
-        value={value}
-        aria-label={label}
-      >
-        <option key={`option-placeholder-${id}`} value={undefined}>
-          {placeholder}
-        </option>
-        {options.map((opt) => {
-          return (
-            <option key={opt.enum[0]} value={opt.enum[0]}>
-              {opt.title}
+      {revisionStatus == "Draft" ? (
+        <>
+          <Dropdown
+            id={id}
+            onChange={(e) => onChange(e.target.value || undefined)}
+            placeholder={placeholder}
+            size={(uiSchema && uiSchema["bcgov:size"]) || "large"}
+            required={required}
+            value={value}
+            aria-label={label}
+          >
+            <option key={`option-placeholder-${id}`} value={undefined}>
+              {placeholder}
             </option>
-          );
-        })}
-      </Dropdown>
-      <Button>Notify</Button>
+            {options.map((opt) => {
+              return (
+                <option key={opt.enum[0]} value={opt.enum[0]}>
+                  {opt.title}
+                </option>
+              );
+            })}
+          </Dropdown>
+          <Button
+            onClick={handleUpdate}
+            disabled={isUpdatingProjectRevision}
+            type="submit"
+          >
+            Update
+          </Button>
+          <Button>Notify</Button>
+        </>
+      ) : (
+        <em>{formContext.pendingActionsFrom}</em>
+      )}
       <style jsx>
         {`
           div :global() {
@@ -58,6 +103,9 @@ const SelectWithNotifyWidget: React.FunctionComponent<WidgetProps> = (
           }
           div :global(.pg-select) {
             width: 18em;
+            margin-right: 1em;
+          }
+          div :global(.pg-button) {
             margin-right: 1em;
           }
         `}
