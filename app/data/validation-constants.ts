1083007f40bba3b1d281431bbf527063750018d0
batunpc <batuhnpc@gmail.com>
2022-12-08 01:24:51 -0500

    regex for key_email and key_phone_no in validation constants

---
+++ app/data/validation-constants.ts
@@ -0,0 +1,7 @@
+export const REGEX_RFP_DIGITS = /\d{3,4}/;
+export const REGEX_EMAIL = /^[\.\w-]+@([\w-]+\.)+[\w-]{2,4}$/;
+export const REGEX_PHONE_NO = /^(\+?\d{1,2}[\s,-]?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+
+export const KEY_RFP_DIGITS = "rfpDigits";
+export const KEY_EMAIL = "email";
+export const KEY_PHONE_NO = "phone";
