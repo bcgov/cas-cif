219878488b5bed8d68d065a70d83cb58da8bb500
batunpc <batuhnpc@gmail.com>
2022-12-08 01:34:07 -0500

    update customFormats file

--- app/data/jsonSchemaForm/customFormats.ts
+++ app/data/jsonSchemaForm/customFormats.ts
@@ -1,16 +1,18 @@
+import { KEY_EMAIL, KEY_PHONE_NO, KEY_RFP_DIGITS, REGEX_EMAIL, REGEX_PHONE_NO, REGEX_RFP_DIGITS } from "data/validation-constants";
+
 /*
  * Any custom formats defined here must also be defined in app/server/middleware/graphql/validateRecord.ts to make AJV aware of them.
  * Example: ajv.addFormat('<format_name>', <regex>);
  */
 
 export const customFormats = {
-  rfpDigits: /\d{3,4}/,
-  email: /^[\.\w-]+@([\w-]+\.)+[\w-]{2,4}$/,
-  phone: /^(\+?\d{1,2}[\s,-]?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
+  [KEY_RFP_DIGITS]: REGEX_RFP_DIGITS,
+  [KEY_EMAIL]: REGEX_EMAIL,
+  [KEY_PHONE_NO]: REGEX_PHONE_NO,
 };
 
 export const customFormatsErrorMessages = {
-  rfpDigits: "Please enter 3 or 4 digits for the random RFP digits",
-  email: "Please enter in the format: name@example.com",
-  phone: "Please enter in a valid phone number format (e.g. 123 456 7890)",
+  KEY_RFP_DIGITS: "Please enter 3 or 4 digits for the random RFP digits",
+  KEY_EMAIL: "Please enter in the format: name@example.com",
+  KEY_PHONE_NO: "Please enter in a valid phone number format (e.g. 123 456 7890)",
 };
