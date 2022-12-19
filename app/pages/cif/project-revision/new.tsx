94cbd60526d6cf9073bc3a11f02f675e727fd404
Brianna Cerkiewicz <brianna@button.is>
2022-12-19 10:24:30 -0800

    chore: create new project page

--- app/pages/cif/project-revision/new.tsx
+++ app/pages/cif/project-revision/new.tsx
@@ -3,19 +3,20 @@ import FormBase from "components/Form/FormBase";
 import SelectRfpWidget from "components/Form/SelectRfpWidget";
 import DefaultLayout from "components/Layout/DefaultLayout";
 import projectSchema from "data/jsonSchemaForm/projectSchema";
-import { JSONSchema7 } from "json-schema";
+import { JSONSchema7, JSONSchema7TypeName } from "json-schema";
 import withRelayOptions from "lib/relay/withRelayOptions";
 import EmptyObjectFieldTemplate from "lib/theme/EmptyObjectFieldTemplate";
 import { useCreateProjectMutation } from "mutations/Project/createProject";
 import { useRouter } from "next/router";
-import { graphql, usePreloadedQuery, useFragment } from "react-relay/hooks";
+import { useState } from "react";
+import { graphql, usePreloadedQuery } from "react-relay/hooks";
 import { RelayProps, withRelay } from "relay-nextjs";
 import { getProjectRevisionFormPageRoute } from "routes/pageRoutes";
 import { newProjectRevisionQuery } from "__generated__/newProjectRevisionQuery.graphql";
 
 const schema: JSONSchema7 = {
   $schema: projectSchema.$schema,
-  type: projectSchema.type,
+  type: projectSchema.type as JSONSchema7TypeName,
   required: ["fundingStreamRfpId"],
   properties: {
     fundingStreamRfpId: {
@@ -34,14 +35,13 @@ const uiSchema = {
     },
   },
 };
-// Forgetting to spread `newProjectForm_query` in `useFragment()`'s parent's fragment.--THERE ISN'T A PARENT FRAGMENT, JUST A PARENT QUERY?
+
 export const pageQuery = graphql`
   query newProjectRevisionQuery {
     query {
       session {
         ...DefaultLayout_session
       }
-      #   ...newProjectForm_query
       ...SelectRfpWidget_query
     }
   }
@@ -50,104 +50,57 @@ export const pageQuery = graphql`
 export function ProjectRevisionNew({
   preloadedQuery,
 }: RelayProps<{}, newProjectRevisionQuery>) {
-  const { session, query } = usePreloadedQuery(pageQuery, preloadedQuery);
-
-  //   const query = useFragment(
-  //     graphql`
-  //       fragment newProjectForm_query on Query {
-  //         ...SelectRfpWidget_query
-  //       }
-  //     `,
-  //     pageQuery.query
-  //   );
-
-  console.log("query in new", query);
+  const { query } = usePreloadedQuery(pageQuery, preloadedQuery);
 
   const router = useRouter();
-
-  //   const handleChange = (changeData: any) => {
-  //     const updatedFormData = {
-  //       ...projectRevision.projectFormChange.newFormData,
-  //       ...changeData,
-  //     };
-
-  //     return new Promise((resolve, reject) =>
-  //       updateNewProject({
-  //         variables: {
-  //           input: {
-  //             rowId: projectRevision.projectFormChange.rowId,
-  //             formChangePatch: {
-  //               newFormData: updatedFormData,
-  //             },
-  //           },
-  //         },
-  //         optimisticResponse: {
-  //           updateFormChange: {
-  //             formChange: {
-  //               id: projectRevision.projectFormChange.id,
-  //               newFormData: updatedFormData,
-  //               isUniqueValue: projectRevision.projectFormChange.isUniqueValue,
-  //               changeStatus: "pending",
-  //               projectRevisionByProjectRevisionId: undefined,
-  //             },
-  //           },
-  //         },
-  //         onCompleted: resolve,
-  //         onError: reject,
-  //         debounceKey: projectRevision.projectFormChange.id,
-  //       })
-  //     );
-  //   };
-
+  const [formState, setFormState] = useState({});
   const [createProject, isCreatingProject] = useCreateProjectMutation();
-  //   chek for errors--can't use handlestage, is rjsf regular validation enough?
-  const handleSubmit = async () => {
+
+  const handleSubmit = (formData) => {
+    console.log("formData", formData);
     createProject({
-      variables: { input: {} },
-      onCompleted: (response) => {
+      variables: {
+        input: {
+          fundingStreamRpfId: formData.fundingStreamRfpId,
+        },
+      },
+      onCompleted: (response) =>
         router.push(
           getProjectRevisionFormPageRoute(
             response.createProject.projectRevision.id,
             0
           )
-        );
-      },
+        ),
     });
-    //redirect probably
-  };
-
-  const handleError = () => {
-    // handleStage();
   };
 
   return (
     <>
-      <DefaultLayout session={session}>
+      <DefaultLayout session={query.session}>
         <header>
           <h2>New Project</h2>
         </header>
         <FormBase
-          tagName={"dl"}
-          className="project-revision-view-form"
           schema={schema}
           uiSchema={uiSchema}
           ObjectFieldTemplate={EmptyObjectFieldTemplate}
-          //   formData={projectRevision}
           formContext={{
             query,
           }}
           id="ProjectRevisionNewForm"
-          //   onChange={(change) => handleChange(change.formData)}
-          onSubmit={handleSubmit}
-          onError={handleError}
+          onSubmit={(e) => handleSubmit(e.formData)}
+          onChange={({ formData }) => {
+            setFormState(formData);
+          }}
+          formData={formState}
           widgets={{
             SelectRfpWidget: SelectRfpWidget,
           }}
         >
-          <Button type="submit" size="small">
+          <Button type="submit" size="small" disabled={isCreatingProject}>
             Confirm
           </Button>
-          <em>These fields are immutable once selected</em>
+          <em> These fields are immutable once selected</em>
         </FormBase>
       </DefaultLayout>
       <style jsx>{`
