a311c59255d4dda63907c149b2f4299962656edb
Brianna Cerkiewicz <brianna@button.is>
2022-12-29 14:20:55 -0800

    chore: remove proponentSharePercentage from db

--- schema/revert/tables/funding_parameter_004_add_proponents_share_perecentage_col.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:tables/tables/funding_parameter_004_add_proponents_share_perecentage_col from pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-
-COMMIT;
--- schema/verify/tables/funding_parameter_004_add_proponents_share_perecentage_col.sql
+++
@@ -1,7 +0,0 @@
--- Verify cif:tables/tables/funding_parameter_004_add_proponents_share_perecentage_col on pg
-
-BEGIN;
-
--- XXX Add verifications here.
-
-ROLLBACK;
