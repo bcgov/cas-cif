129aace612eb73c864f5dba7b65f300720e79d18
SeSo <sobhani.sepehr@gmail.com>
2023-01-04 17:19:34 -0800

    test: add unit test

---
+++ app/tests/unit/components/ProjectRevision/CollapsibleFormWidget.test.tsx
@@ -0,0 +1,138 @@
+import { screen } from "@testing-library/react";
+import ProjectFormSummary from "components/Form/ProjectFormSummary";
+import CollapsibleFormWidget from "components/ProjectRevision/CollapsibleFormWidget";
+import ComponentTestingHelper from "tests/helpers/componentTestingHelper";
+import { graphql } from "react-relay";
+import compiledCollapsibleFormWidgetQuery, {
+  CollapsibleFormWidgetQuery,
+} from "__generated__/CollapsibleFormWidgetQuery.graphql";
+
+const testQuery = graphql`
+  query CollapsibleFormWidgetQuery @relay_test_operation {
+    query {
+      projectRevision(id: "Test Project Revision ID") {
+        ...CollapsibleFormWidget_projectRevision
+      }
+    }
+  }
+`;
+
+const mockQueryPayload = {
+  ProjectRevision() {
+    return {
+      isFirstRevision: false,
+      projectFormChange: {
+        newFormData: {
+          proposalReference: "Test Proposal Reference",
+          operatorId: 2,
+          fundingStreamRfpId: 1,
+          projectStatusId: 1,
+          summary: "Test Summary",
+          projectName: "Test Project Name",
+          totalFundingRequest: 100.0,
+          score: 8.333,
+          projectType: "test project type",
+        },
+        operation: "UPDATE",
+        isPristine: false,
+        asProject: {
+          operatorByOperatorId: {
+            legalName: "Test Legal Name",
+            bcRegistryId: "Test BC Registry ID",
+          },
+          fundingStreamRfpByFundingStreamRfpId: {
+            year: 2020,
+            fundingStreamByFundingStreamId: {
+              description: "Test Funding Stream Description",
+            },
+          },
+          projectStatusByProjectStatusId: {
+            name: "Test Project Status Name",
+          },
+        },
+        formChangeByPreviousFormChangeId: {
+          newFormData: {
+            proposalReference: "Test Proposal Reference PREVIOUS",
+            operatorId: 1,
+            fundingStreamRfpId: 1,
+            projectStatusId: 1,
+            summary: "Test Summary",
+            projectName: "Test Project Name",
+            totalFundingRequest: 100.0,
+            score: 1,
+            projectType: "test project type PREVIOUS",
+          },
+          asProject: {
+            operatorByOperatorId: {
+              legalName: "Test Legal Name PREVIOUS",
+              bcRegistryId: "Test BC Registry ID",
+            },
+            fundingStreamRfpByFundingStreamRfpId: {
+              year: 2020,
+              fundingStreamByFundingStreamId: {
+                description: "Test Funding Stream Description",
+              },
+            },
+            projectStatusByProjectStatusId: {
+              name: "Test Project Status Name",
+            },
+          },
+        },
+      },
+    };
+  },
+};
+
+const componentTestingHelper =
+  new ComponentTestingHelper<CollapsibleFormWidgetQuery>({
+    component: CollapsibleFormWidget,
+    testQuery: testQuery,
+    compiledQuery: compiledCollapsibleFormWidgetQuery,
+    getPropsFromTestQuery: (data) => ({
+      title: "Test Title",
+      formItems: [
+        {
+          title: "Project Overview",
+          formConfiguration: {
+            formIndex: 0,
+            slug: "projectOverview",
+            editComponent: "ProjectForm",
+            viewComponent: ProjectFormSummary,
+          },
+        },
+      ],
+      projectRevision: data.query.projectRevision,
+    }),
+    defaultQueryResolver: mockQueryPayload,
+    defaultQueryVariables: {},
+  });
+
+describe("The CollapsibleFormWidget", () => {
+  it("Displays diffs of the the data fields that have changed", () => {
+    componentTestingHelper.loadQuery();
+    componentTestingHelper.renderComponent();
+
+    expect(
+      screen.getByRole("checkbox", { name: /test title/i })
+    ).toBeInTheDocument();
+    expect(screen.getByText("Test Proposal Reference")).toBeInTheDocument();
+    expect(
+      screen.getByText("Test Proposal Reference PREVIOUS")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Test Legal Name (Test BC Registry ID)")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Test Legal Name PREVIOUS (Test BC Registry ID)")
+    ).toBeInTheDocument();
+    expect(screen.getByText("test project type PREVIOUS")).toBeInTheDocument();
+
+    // testing some classnames that are used to style the diff
+    expect(screen.getByText("Test Proposal Reference")).toHaveClass(
+      "revisionDiffNew"
+    );
+    expect(screen.getByText("Test Proposal Reference PREVIOUS")).toHaveClass(
+      "revisionDiffOld"
+    );
+  });
+});
