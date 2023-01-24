7a99a059a56ec7cd1c984a71c47ed302549166f9
Brianna Cerkiewicz <brianna@button.is>
2023-01-24 14:46:35 -0800

    chore: create form_change_calculated_ei_performance

---
+++ schema/revert/computed_columns/form_change_calculated_ei_performance.sql
@@ -0,0 +1,7 @@
+-- Revert cif:computed_columns/form_change_calculated_ei_performance from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/verify/computed_columns/form_change_calculated_ei_performance.sql
@@ -0,0 +1,7 @@
+-- Verify cif:computed_columns/form_change_calculated_ei_performance on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
