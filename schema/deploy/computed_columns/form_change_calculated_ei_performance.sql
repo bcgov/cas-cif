2984056c1e61e4917093fcb2dc0c610113f59435
Brianna Cerkiewicz <brianna@button.is>
2023-01-26 14:23:35 -0800

    chore: apply suggestions from code review

--- schema/deploy/computed_columns/form_change_calculated_ei_performance.sql
+++ schema/deploy/computed_columns/form_change_calculated_ei_performance.sql
@@ -12,7 +12,7 @@ $fn$
       when (fc.new_form_data->>'baselineEmissionIntensity' is null)
         or (fc.new_form_data->>'targetEmissionIntensity' is null)
         or (fc.new_form_data->>'postProjectEmissionIntensity' is null)
-        or (fc.new_form_data->>'baselineEmissionIntensity' = fc.new_form_data->>'targetEmissionIntensity')
+        or (((fc.new_form_data->>'baselineEmissionIntensity')::numeric - (fc.new_form_data->>'targetEmissionIntensity')::numeric) != 0)
         then null
       else
         round(((fc.new_form_data->>'baselineEmissionIntensity')::numeric - (fc.new_form_data->>'postProjectEmissionIntensity')::numeric)/((fc.new_form_data->>'baselineEmissionIntensity')::numeric - (fc.new_form_data->>'targetEmissionIntensity')::numeric)*100,2)
