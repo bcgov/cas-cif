7a83bbfce2f28d41fee821e8ff12ccec3d21ec9e
Brianna Cerkiewicz <brianna@button.is>
2023-01-31 11:43:49 -0800

    chore: create separate schemas for EP and IA funding forms in db

---
+++ schema/deploy/data/insert_json_schema_form_data_separate_EP_and_IA_funding_schemas.sql
@@ -0,0 +1,20 @@
+-- Deploy cif:data/insert_json_schema_form_data_separate_EP_and_IA_funding_schemas to pg
+
+begin;
+
+with rows as (
+insert into cif.form(slug, form_change_commit_handler, json_schema, description)
+values
+-- funding_parameter_IA and funding_parameter_EP to be removed when funding agreement form gets refactored to be one form change, just here to pass the fkey
+('funding_parameter_EP', default, '{}'::jsonb, 'OBSOLETE (see comment above) schema data relating to funding_parameter_EP'),
+('funding_parameter_IA', default, '{}'::jsonb, 'OBSOLETE (see comment above) schema data relating to funding_parameter_IA'),
+-- brianna delete this once T's PR is in
+('project_summary_report', default, '{}'::jsonb, 'OBSOLETE (see comment above) schema data relating to funding_parameter_IA')
+on conflict(slug) do update
+set json_schema=excluded.json_schema,
+    description=excluded.description,
+    json_schema_generator=excluded.json_schema_generator
+returning 1
+) select 'Inserted ' || count(*) || ' rows into cif.form' from rows;
+
+commit;
---
+++ schema/verify/data/insert_json_schema_form_data_separate_EP_and_IA_funding_schemas.sql
@@ -0,0 +1,13 @@
+-- Verify cif:data/insert_json_schema_form_data_separate_EP_and_IA_funding_schemas on pg
+
+begin;
+
+do $$
+  begin
+    assert (
+      (select count(*) from cif.form) = 14
+    ), 'The proper number of values were inserted in the cif.form table';
+  end;
+$$;
+
+rollback;
