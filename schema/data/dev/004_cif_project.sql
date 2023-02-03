07e517b8abe2131ca8efd629da6d87d7990a2dce
Brianna Cerkiewicz <brianna@button.is>
2023-02-03 14:46:44 -0800

    chore: add IA projects to dev data

--- schema/data/dev/004_cif_project.sql
+++ schema/data/dev/004_cif_project.sql
@@ -3,8 +3,8 @@ begin;
 do $$
   declare current_revision cif.project_revision;
   begin
+  -- ep projects
     for index in 1..50 loop
-
       current_revision := cif.create_project(1);
 
       update cif.form_change
@@ -12,9 +12,9 @@ do $$
           'operatorId', 1,
           'fundingStreamRfpId', 1,
           'projectStatusId', 1,
-          'proposalReference', lpad(index::text, 3, '0'),
+          'proposalReference', 'EP' || lpad(index::text, 3, '0'),
           'summary', 'lorem ipsum dolor sit amet adipiscing eli',
-          'projectName', 'Test Project ' || lpad(index::text, 3, '0'),
+          'projectName', 'Test EP Project ' || lpad(index::text, 3, '0'),
           'totalFundingRequest', cast(rpad(index::text, 3, '0') as bigint),
           'sectorName', 'Agriculture',
           'projectType', 'Carbon Capture',
@@ -31,6 +31,61 @@ do $$
         json_schema_name,
         project_revision_id
       )
+      values
+      (
+        json_build_object(
+          'cifUserId', least(index, 6),
+          'projectId', (select form_data_record_id from cif.form_change where form_data_table_name='project' and project_revision_id=current_revision.id),
+          'projectManagerLabelId', 1
+          ),
+        'create', 'cif', 'project_manager', 'pending', 'project_manager', current_revision.id);
+
+      insert into cif.form_change(
+        new_form_data,
+        operation,
+        form_data_schema_name,
+        form_data_table_name,
+        change_status,
+        json_schema_name,
+        project_revision_id
+      )
+      values
+      (
+        json_build_object(
+          'contactId', index,
+          'projectId', (select form_data_record_id from cif.form_change where form_data_table_name='project' and project_revision_id=current_revision.id),
+          'contactIndex', 1
+          ),
+        'create', 'cif', 'project_contact', 'pending', 'project_contact', current_revision.id);
+    end loop;
+
+  -- ia projects
+    for index in 1..5 loop
+
+      current_revision := cif.create_project(2);
+
+      update cif.form_change
+      set new_form_data= jsonb_build_object(
+          'operatorId', 2,
+          'fundingStreamRfpId', 2,
+          'projectStatusId', 2,
+          'proposalReference', 'IA' || lpad(index::text, 3, '0'),
+          'summary', 'lorem ipsum dolor sit amet adipiscing eli',
+          'projectName', 'Test IA Project ' || lpad(index::text, 3, '0'),
+          'totalFundingRequest', cast(rpad(index::text, 5, '0') as bigint),
+          'sectorName', 'Cement',
+          'projectType', 'Fuel Switching'
+          )
+      where project_revision_id=current_revision.id and form_data_table_name='project';
+
+      insert into cif.form_change(
+        new_form_data,
+        operation,
+        form_data_schema_name,
+        form_data_table_name,
+        change_status,
+        json_schema_name,
+        project_revision_id)
       values
       (
         json_build_object(
