1bb5d7b89d4c4d854f1eb1fe48569c95e42af58c
SeSo <sobhani.sepehr@gmail.com>
2023-02-13 13:21:33 -0800

    chore: update image import path

--- app/components/Layout/Navigation.tsx
+++ app/components/Layout/Navigation.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { BaseNavigation } from "@button-inc/bcgov-theme/Navigation";
 import { BaseHeader } from "@button-inc/bcgov-theme/Header";
 import LogoutForm from "components/Session/LogoutForm";
