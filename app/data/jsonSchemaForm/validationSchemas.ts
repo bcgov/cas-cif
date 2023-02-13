9ab2654adf4c3007f9230e3e5e5d7be15c04a099
Brianna Cerkiewicz <brianna@button.is>
2023-02-13 14:58:47 -0800

    chore: use prod schemas in validation plugin

--- app/data/jsonSchemaForm/validationSchemas.ts
+++ app/data/jsonSchemaForm/validationSchemas.ts
@@ -1,5 +1,4 @@
 import projectContactSchema from "./projectContactSchema";
-import { projectReportingRequirementSchema } from "./projectReportingRequirementSchema";
 import projectManagerSchema from "./projectManagerSchema";
 import projectSchema from "./projectSchema";
 import contactSchema from "./contactSchema";
@@ -17,7 +16,6 @@ import { fundingParameterIASchema } from "./fundingParameterIASchema";
 const validationSchemas = {
   project_contact: projectContactSchema,
   project_manager: projectManagerSchema,
-  reporting_requirement: projectReportingRequirementSchema,
   project: projectSchema,
   contact: contactSchema,
   operator: operatorSchema,
