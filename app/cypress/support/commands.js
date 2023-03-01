4cbb25b4e950247d86d2bc6449ffae35cdfcbda0
SeSo <sobhani.sepehr@gmail.com>
2023-03-01 08:30:21 -0800

    test: possible solution to e2e test hydration issues on CI

--- app/cypress/support/commands.js
+++ app/cypress/support/commands.js
@@ -577,7 +577,8 @@ Cypress.on("uncaught:exception", (err) => {
   // we check if the error is
   if (
     err.message.includes("Minified React error #425;") ||
-    err.message.includes("Minified React error #418;")
+    err.message.includes("Minified React error #418;") ||
+    err.message.includes("Minified React error #422;")
   ) {
     return false;
   }
