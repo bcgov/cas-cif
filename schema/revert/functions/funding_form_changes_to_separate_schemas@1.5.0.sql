327aa174953e74b53bb40aef03d0f991a1a3ffbc
SeSo <sobhani.sepehr@gmail.com>
2023-03-06 13:43:16 -0800

    chore: remove rework job

--- schema/revert/functions/funding_form_changes_to_separate_schemas@1.5.0.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:functions/funding_form_changes_to_separate_schemas from pg
-
-begin;
-
-drop function cif_private.funding_form_changes_to_separate_schemas;
-
-commit;
--- schema/verify/functions/funding_form_changes_to_separate_schemas@1.5.0.sql
+++
@@ -1,7 +0,0 @@
--- Verify cif:functions/funding_form_changes_to_separate_schemas on pg
-
-begin;
-
-select pg_get_functiondef('cif_private.funding_form_changes_to_separate_schemas()'::regprocedure);
-
-rollback;
