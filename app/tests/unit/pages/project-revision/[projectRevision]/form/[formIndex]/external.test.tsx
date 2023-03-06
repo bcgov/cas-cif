24c826144824f67a4873058361bb6de478dcd983
Gurjeet Matharu <gurjeet@matharuco.com>
2023-03-06 13:07:13 -0500

    test: moving external tests out and fixing test name

---
+++ app/tests/unit/pages/project-revision/[projectRevision]/form/[formIndex]/external.test.tsx
@@ -0,0 +1,87 @@
+import "@testing-library/jest-dom";
+import { screen, within } from "@testing-library/react";
+
+import PageTestingHelper from "tests/helpers/pageTestingHelper";
+import externalCompiledQuery from "__generated__/viewExternalProjectRevisionQuery.graphql";
+import ExternalProjectFormPage from "pages/cif-external/project-revision/[projectRevision]/view";
+import { FormIndexPageQuery } from "__generated__/FormIndexPageQuery.graphql";
+
+const defaultMockResolver = {
+  ProjectRevision(context, generateId) {
+    return {
+      id: `mock-proj-rev-${generateId()}`,
+      projectId: 123,
+      changeStatus: "pending",
+      projectByProjectId: {
+        proposalReference: "001",
+      },
+      projectFormChange: {
+        id: `mock-project-form-${generateId()}`,
+        newFormData: {
+          someProjectData: "test2",
+        },
+        asProject: {
+          fundingStreamRfpByFundingStreamRfpId: {
+            fundingStreamByFundingStreamId: {
+              name: "EP",
+            },
+          },
+        },
+      },
+      managerFormChanges: {
+        edges: [],
+      },
+      milestoneReportStatuses: {
+        edges: [],
+      },
+    };
+  },
+};
+
+const externalPageTestingHelper = new PageTestingHelper<FormIndexPageQuery>({
+  pageComponent: ExternalProjectFormPage,
+  compiledQuery: externalCompiledQuery,
+  defaultQueryResolver: defaultMockResolver,
+  defaultQueryVariables: {
+    projectRevision: "mock-id",
+  },
+});
+
+describe("The Project Overview Page (external)", () => {
+  beforeEach(() => {
+    externalPageTestingHelper.reinit();
+    externalPageTestingHelper.setMockRouterValues({
+      pathname: "/cif-external/project-revision/[projectRevision]/view",
+      query: { projectRevision: "mock-id" },
+    });
+  });
+
+  it("renders the task list in the left navigation", () => {
+    externalPageTestingHelper.loadQuery();
+    externalPageTestingHelper.renderPage();
+
+    expect(
+      within(
+        screen.getByRole("navigation", { name: "side navigation" })
+      ).getByText(/Application Overview/i)
+    ).toBeInTheDocument();
+
+    expect(
+      within(
+        screen.getByRole("navigation", { name: "side navigation" })
+      ).getByText(/Attachments/i)
+    ).toBeInTheDocument();
+
+    expect(
+      within(
+        screen.getByRole("navigation", { name: "side navigation" })
+      ).getByText(/Review/i)
+    ).toBeInTheDocument();
+
+    expect(
+      within(
+        screen.getByRole("navigation", { name: "side navigation" })
+      ).getByText(/Declaration/i)
+    ).toBeInTheDocument();
+  });
+});
