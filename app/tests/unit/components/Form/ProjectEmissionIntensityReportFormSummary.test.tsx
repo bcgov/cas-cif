ef5c9027b26226bb9111b0dc0477616c1fc555ee
SeSo <sobhani.sepehr@gmail.com>
2023-03-09 15:00:38 -0800

    test: add test to cover summary form when value is zero

--- app/tests/unit/components/Form/ProjectEmissionIntensityReportFormSummary.test.tsx
+++ app/tests/unit/components/Form/ProjectEmissionIntensityReportFormSummary.test.tsx
@@ -129,4 +129,140 @@ describe("the emission intensity report form component", () => {
     componentTestingHelper.renderComponent();
     expect(screen.getByText("squirtle")).toBeInTheDocument();
   });
+
+  it("displays the correct data when we have zero values and decimal points on BEI/TEI/PEI and Total Lifetime Emission Reduction and creating an emission intensity report form", () => {
+    const customPayload = {
+      ProjectRevision() {
+        return {
+          isFirstRevision: false,
+          summaryEmissionIntensityReportFormChange: {
+            edges: [
+              {
+                node: {
+                  id: "mock-emission-intensity-report-form-change-id",
+                  rowId: 2,
+                  isPristine: false,
+                  newFormData: {
+                    teimpReporting: {
+                      baselineEmissionIntensity: 0,
+                      targetEmissionIntensity: 0.12345678,
+                      postProjectEmissionIntensity: 0,
+                      totalLifetimeEmissionReduction: 123,
+                    },
+                  },
+                  operation: "CREATE",
+                  formChangeByPreviousFormChangeId: {
+                    newFormData: {},
+                  },
+                },
+              },
+            ],
+          },
+        };
+      },
+    };
+    componentTestingHelper.loadQuery(customPayload);
+    componentTestingHelper.renderComponent();
+
+    expect(
+      screen.getByText("Baseline Emission Intensity (BEI)")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Target Emission Intensity (TEI)")
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText("Post-Project Emission Intensity (PEI) (optional)")
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText("Total Project Lifetime Emissions Reductions (optional)")
+    ).toBeInTheDocument();
+
+    expect(screen.getAllByText("ADDED")).toHaveLength(4);
+    expect(screen.getAllByText("0")).toHaveLength(2);
+    expect(screen.getAllByText("0.12345678")).toHaveLength(1);
+    expect(screen.getByText("0.12345678")).toHaveClass(
+      "diffReviewAndSubmitInformationNew"
+    );
+    expect(screen.getAllByText("123")).toHaveLength(1);
+    expect(screen.getByText("123")).toHaveClass(
+      "diffReviewAndSubmitInformationNew"
+    );
+  });
+  it("displays the correct data when we have zero values and decimal points on BEI/TEI/PEI and Total Lifetime Emission Reduction and updating an emission intensity report form", () => {
+    const customPayload = {
+      ProjectRevision() {
+        return {
+          isFirstRevision: false,
+          summaryEmissionIntensityReportFormChange: {
+            edges: [
+              {
+                node: {
+                  id: "mock-emission-intensity-report-form-change-id",
+                  rowId: 2,
+                  isPristine: false,
+                  newFormData: {
+                    teimpReporting: {
+                      baselineEmissionIntensity: 0.87654321,
+                      targetEmissionIntensity: 0,
+                      postProjectEmissionIntensity: null,
+                      totalLifetimeEmissionReduction: null,
+                    },
+                  },
+                  operation: "UPDATE",
+                  formChangeByPreviousFormChangeId: {
+                    newFormData: {
+                      baselineEmissionIntensity: 0,
+                      targetEmissionIntensity: 0.12345678,
+                      postProjectEmissionIntensity: 654,
+                      totalLifetimeEmissionReduction: 456,
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        };
+      },
+    };
+    componentTestingHelper.loadQuery(customPayload);
+    componentTestingHelper.renderComponent();
+
+    expect(
+      screen.getByText("Baseline Emission Intensity (BEI)")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Target Emission Intensity (TEI)")
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText("Post-Project Emission Intensity (PEI) (optional)")
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText("Total Project Lifetime Emissions Reductions (optional)")
+    ).toBeInTheDocument();
+
+    expect(screen.getAllByText("REMOVED")).toHaveLength(2);
+    expect(screen.getAllByText("0")).toHaveLength(2);
+    expect(screen.getAllByText("0")[0]).toHaveClass(
+      "diffReviewAndSubmitInformationOld"
+    );
+    expect(screen.getAllByText("0")[1]).toHaveClass(
+      "diffReviewAndSubmitInformationNew"
+    );
+    expect(screen.getByText("0.87654321")).toHaveClass(
+      "diffReviewAndSubmitInformationNew"
+    );
+    expect(screen.getByText("0.12345678")).toHaveClass(
+      "diffReviewAndSubmitInformationOld"
+    );
+    expect(screen.getByText("654")).toHaveClass(
+      "diffReviewAndSubmitInformationOld"
+    );
+    expect(screen.getByText("456")).toHaveClass(
+      "diffReviewAndSubmitInformationOld"
+    );
+  });
 });
