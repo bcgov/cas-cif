0390039e028938fc424629f03cc8a9421f7d1a7e
Brianna Cerkiewicz <brianna@button.is>
2023-03-13 12:00:51 -0700

    chore: remove now unused additional funding form migrations

--- schema/verify/mutations/add_additional_funding_source_to_revision.sql
+++ schema/verify/mutations/add_additional_funding_source_to_revision.sql
@@ -2,6 +2,6 @@
 
 begin;
 
-select pg_get_functiondef('cif.add_additional_funding_source_to_revision(int, int)'::regprocedure);
+select cif_private.verify_function_not_present('cif','add_additional_funding_source_to_revision', 2);
 
 rollback;
