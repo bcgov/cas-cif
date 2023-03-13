5dc337d4e40819b65718173648a5c72a64d4d1df
Brianna Cerkiewicz <brianna@button.is>
2023-03-13 16:11:44 -0700

    test: array field template jest test

--- app/tests/unit/lib/theme/AdditionalFundingSourcesArrayFieldTemplate.test.tsx
+++ app/tests/unit/lib/theme/AdditionalFundingSourcesArrayFieldTemplate.test.tsx
@@ -1,29 +1,17 @@
 import { render, screen } from "@testing-library/react";
-import { fundingParameterEPUiSchema } from "data/jsonSchemaForm/fundingParameterEPUiSchema";
+import FormBase from "components/Form/FormBase";
 import { JSONSchema7 } from "json-schema";
 import AdditionalFundingSourcesArrayFieldTemplate from "lib/theme/AdditionalFundingSourcesArrayFieldTemplate";
-import schema from "../../../../../schema/data/prod/json_schema/funding_parameter_EP.json";
+import epSchema from "../../../../../schema/data/prod/json_schema/funding_parameter_EP.json";
+import iaSchema from "../../../../../schema/data/prod/json_schema/funding_parameter_IA.json";
 
 describe("AdditionalFundingSourcesArrayFieldTemplate", () => {
   it("Displays only an Add button when there are no additional funding sources", () => {
+    // cannot render the ArrayFieldComponent directly because rjsf does some behind-the-scenes work to map the items, and outside of a form the following error occurs: Objects are not valid as a React child
     render(
-      <AdditionalFundingSourcesArrayFieldTemplate
-        // DescriptionField={}
-        // TitleField={}
-        canAdd={true}
-        // className={}
-        disabled={false}
-        // idSchema={}
-        items={[]}
-        // onAddClick={}
-        // readonly={}
-        // required={}
-        schema={
-          schema.schema.properties.additionalFundingSources as JSONSchema7
-        }
-        // item.children.props.uiSchema.amount[
-        // formData={}
-        // registry={}
+      <FormBase
+        schema={epSchema.schema as JSONSchema7}
+        ArrayFieldTemplate={AdditionalFundingSourcesArrayFieldTemplate}
       />
     );
     expect(
@@ -34,70 +22,19 @@ describe("AdditionalFundingSourcesArrayFieldTemplate", () => {
 
   it("Displays a form and a remove button for every item, and displays an Add button", () => {
     render(
-      <div>
-        <AdditionalFundingSourcesArrayFieldTemplate
-          // DescriptionField={}
-          // TitleField={}
-          canAdd={true}
-          // className={}
-          disabled={false}
-          // idSchema={}
-          items={[
-            {
-              children: {
-                key: null,
-                type: jest.fn(),
-                props: {},
-              },
-              className: "array-item",
-              disabled: false,
-              hasMoveDown: false,
-              hasMoveUp: false,
-              hasRemove: true,
-              hasToolbar: true,
-              index: 0,
-              key: "key0",
-              onAddIndexClick: jest.fn(),
-              onDropIndexClick: jest.fn(),
-              onReorderClick: jest.fn(),
-              readonly: false,
-            },
-            {
-              children: [],
-              // {
-              //   key: null,
-              //   type: jest.fn(),
-              //   props: {},
-              // },
-              className: "array-item",
-              disabled: false,
-              hasMoveDown: false,
-              hasMoveUp: false,
-              hasRemove: true,
-              hasToolbar: true,
-              index: 1,
-              key: "key1",
-              onAddIndexClick: jest.fn(),
-              onDropIndexClick: jest.fn(),
-              onReorderClick: jest.fn(),
-              readonly: false,
-            },
-          ]}
-          // onAddClick={}
-          // readonly={}
-          // required={}
-          schema={
-            schema.schema.properties.additionalFundingSources as JSONSchema7
-          }
-          uiSchema={fundingParameterEPUiSchema.additionalFundingSources}
-          // title={}
-          // formContext={}
-          // formData={}
-          // registry={}
-        />
-      </div>
+      <FormBase
+        schema={iaSchema.schema as JSONSchema7}
+        ArrayFieldTemplate={AdditionalFundingSourcesArrayFieldTemplate}
+        formData={{
+          additionalFundingSources: [
+            { source: "a", amount: 5, status: "Approved" },
+            { source: "b", amount: 5, status: "Approved" },
+          ],
+        }}
+      />
     );
-    expect(screen.getByText(/additional funding source/)).toBeVisible();
+    expect(screen.getByText(/additional funding source 1/i)).toBeVisible();
+    expect(screen.getByText(/additional funding source 2/i)).toBeVisible();
     expect(screen.getAllByRole("button", { name: /remove/i })).toHaveLength(2);
     expect(
       screen.getByRole("button", { name: /add funding source/i })
