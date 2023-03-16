af835ff9a1809b9cb85230aac5c2f9fa55d5271f
SeSo <sobhani.sepehr@gmail.com>
2023-03-15 22:12:50 -0700

    chore: add assertion to make CI e2e test happy

--- app/cypress/e2e/cif/projects.cy.js
+++ app/cypress/e2e/cif/projects.cy.js
@@ -43,8 +43,7 @@ describe("the projects page", () => {
     cy.get("thead th").contains("Project Name").click();
     cy.get("h2").contains("Projects");
     cy.get("tbody tr").should("have.length", 20);
-    cy.contains("Test IA Project 005");
-    cy.url().should("include", "orderBy=PROJECT_NAME_DESC");
     cy.contains("Test EP Project 050").should("be.visible");
+    cy.url().should("include", "orderBy=PROJECT_NAME_DESC");
   });
 });
