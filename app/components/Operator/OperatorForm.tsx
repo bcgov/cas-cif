70ad29dde4e50f21434c9e7fccd7ac5c7c7e77db
Gurjeet Matharu <gurjeet@matharuco.com>
2023-03-15 18:55:05 -0700

    feat: using db for operator form schema

--- app/components/Operator/OperatorForm.tsx
+++ app/components/Operator/OperatorForm.tsx
@@ -1,12 +1,31 @@
 import { Button } from "@button-inc/bcgov-theme";
 import FormBase from "components/Form/FormBase";
-import operatorSchema from "data/jsonSchemaForm/operatorSchema";
 import { JSONSchema7 } from "json-schema";
 import { FormPageFactoryComponentProps } from "lib/pages/relayFormPageFactory";
+import { graphql, useFragment } from "react-relay";
+import { OperatorForm_query$key } from "__generated__/OperatorForm_query.graphql";
 
-const OperatorForm: React.FC<FormPageFactoryComponentProps> = (props) => {
+interface Props extends FormPageFactoryComponentProps {
+  query: OperatorForm_query$key;
+}
+
+const OperatorForm: React.FC<Props> = (props) => {
+  const { operatorFormBySlug } = useFragment(
+    graphql`
+      fragment OperatorForm_query on Query {
+        operatorFormBySlug: formBySlug(slug: "operator") {
+          jsonSchema
+        }
+      }
+    `,
+    props.query
+  );
   return (
-    <FormBase {...props} schema={operatorSchema as JSONSchema7} uiSchema={{}}>
+    <FormBase
+      {...props}
+      schema={operatorFormBySlug.jsonSchema as JSONSchema7}
+      uiSchema={{}}
+    >
       <Button type="submit" style={{ marginRight: "1rem" }}>
         Submit
       </Button>
