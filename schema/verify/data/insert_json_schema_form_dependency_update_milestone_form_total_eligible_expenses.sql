6bc5ad806a31d7dc5589b9dd7e10806ef2e444af
SeSo <sobhani.sepehr@gmail.com>
2023-03-16 14:32:58 -0700

    feat: add dependency to milestone schema

---
+++ schema/verify/data/insert_json_schema_form_dependency_update_milestone_form_total_eligible_expenses.sql
@@ -0,0 +1,13 @@
+-- Verify cif:data/insert_json_schema_form_dependency_update_milestone_form_total_eligible_expenses on pg
+
+begin;
+
+do $$
+  begin
+    assert (
+      (select count(*) from cif.form) = 13
+    ), 'The proper number of values were inserted in the cif.form table';
+  end;
+$$;
+
+rollback;
