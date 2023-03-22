1140fce1c320ffdc6e11ad8238ed3871524b5eb9
Gurjeet Matharu <gurjeet@matharuco.com>
2023-03-22 14:56:25 -0700

    chore: wip

--- app/components/Operator/OperatorForm.tsx
+++ app/components/Operator/OperatorForm.tsx
@@ -1,39 +1,39 @@
-import { Button } from "@button-inc/bcgov-theme";
 import FormBase from "components/Form/FormBase";
+import withRelayOptions from "lib/relay/withRelayOptions";
+import { Button } from "@button-inc/bcgov-theme";
 import { JSONSchema7 } from "json-schema";
-import { FormPageFactoryComponentProps } from "lib/pages/relayFormPageFactory";
-import { graphql, useFragment } from "react-relay";
-import { OperatorForm_query$key } from "__generated__/OperatorForm_query.graphql";
+import { OperatorFormQuery } from "__generated__/OperatorFormQuery.graphql";
+import { graphql, usePreloadedQuery } from "react-relay/hooks";
+import { withRelay, RelayProps } from "relay-nextjs";
 
-interface Props extends FormPageFactoryComponentProps {
-  query: OperatorForm_query$key;
-}
+export const Query = graphql`
+  query OperatorFormQuery {
+    contactFormBySlug: formBySlug(slug: "operator") {
+      jsonSchema
+    }
+  }
+`;
 
-const OperatorForm: React.FC<Props> = (props) => {
-  const { operatorFormBySlug } = useFragment(
-    graphql`
-      fragment OperatorForm_query on Query {
-        operatorFormBySlug: formBySlug(slug: "operator") {
-          jsonSchema
-        }
-      }
-    `,
-    props.query
-  );
+export function OperatorForm({
+  preloadedQuery,
+}: RelayProps<{}, OperatorFormQuery>) {
+  const contactFormBySlug = usePreloadedQuery(Query, preloadedQuery);
+  console.log(contactFormBySlug);
+  console.log("----");
+  return null;
   return (
     <FormBase
-      {...props}
       schema={operatorFormBySlug.jsonSchema as JSONSchema7}
       uiSchema={{}}
     >
       <Button type="submit" style={{ marginRight: "1rem" }}>
         Submit
       </Button>
-      <Button type="button" variant="secondary" onClick={props.onDiscard}>
+      <Button type="button" variant="secondary">
         Discard Changes
       </Button>
     </FormBase>
   );
-};
+}
 
-export default OperatorForm;
+export default withRelay(OperatorForm, Query, withRelayOptions);
