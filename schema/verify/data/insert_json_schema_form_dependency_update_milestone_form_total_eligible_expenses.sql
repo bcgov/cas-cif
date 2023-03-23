bd79a0782820c8092093e94c4e1f48687b232213
SeSo <sobhani.sepehr@gmail.com>
2023-03-23 15:39:33 -0700

    chore: fix post-rebase conflict

--- schema/verify/data/insert_json_schema_form_dependency_update_milestone_form_total_eligible_expenses.sql
+++ schema/verify/data/insert_json_schema_form_dependency_update_milestone_form_total_eligible_expenses.sql
@@ -5,7 +5,7 @@ begin;
 do $$
   begin
     assert (
-      (select count(*) from cif.form) = 13
+      (select count(*) from cif.form) = 14
     ), 'The proper number of values were inserted in the cif.form table';
   end;
 $$;
