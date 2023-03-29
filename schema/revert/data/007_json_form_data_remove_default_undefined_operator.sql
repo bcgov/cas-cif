7f807f48ed16a2bbc3c2f74dc75e83bcd6224d42
Gurjeet Matharu <gurjeet@matharuco.com>
2023-03-29 15:55:42 -0700

    chore: create migration for removing default in operator

---
+++ schema/revert/data/007_json_form_data_remove_default_undefined_operator.sql
@@ -0,0 +1,7 @@
+-- Revert cif:data/007_json_form_data_remove_default_undefined_operator from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/verify/data/007_json_form_data_remove_default_undefined_operator.sql
@@ -0,0 +1,13 @@
+-- Verify cif:data/007_json_form_data_remove_default_undefined_operator on pg
+
+begin;
+
+do $$
+  begin
+    assert (
+      (select count(*) from cif.form) = 13
+    ), 'The proper number of values were inserted in the cif.form table';
+  end;
+$$;
+
+rollback;
