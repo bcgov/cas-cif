802a53ad7e1f111be321fe00f732cf6b9b231c41
Brianna Cerkiewicz <brianna@button.is>
2023-03-30 14:57:13 -0700

    chore: prep work for reworking post-release

--- schema/revert/computed_columns/project_revision_total_project_value@1.5.1.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:computed_columns/project_revision_total_project_value from pg
-
-begin;
-
-drop function cif.project_revision_total_project_value;
-
-commit;
--- schema/revert/mutations/add_additional_funding_source_to_revision@1.4.1.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:mutations/add_additional_funding_source_to_revision from pg
-
-begin;
-
-drop function cif.add_additional_funding_source_to_revision;
-
-commit;
