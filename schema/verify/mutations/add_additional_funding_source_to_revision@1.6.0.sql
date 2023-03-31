461ba60252f9468f01ef9b9267a35f2a044f93bf
Brianna Cerkiewicz <brianna@button.is>
2023-03-31 14:38:55 -0700

    chore: rework functions post-rebase

--- schema/verify/mutations/add_additional_funding_source_to_revision@1.6.0.sql
+++ schema/verify/mutations/add_additional_funding_source_to_revision@1.6.0.sql
@@ -2,6 +2,6 @@
 
 begin;
 
-select cif_private.verify_function_not_present('cif','add_additional_funding_source_to_revision', 2);
+select pg_get_functiondef('cif.add_additional_funding_source_to_revision(int, int)'::regprocedure);
 
 rollback;
