bacd31fe54289ba4481170cce90c5990440171f0
Gurjeet Matharu <gurjeet@matharuco.com>
2023-04-13 11:22:05 -0700

    test: updated tests to use mocked or placeholder data

---
+++ app/tests/unit/pages/cif-external/project-revision/index.test.tsx
@@ -0,0 +1,63 @@
+import { screen } from "@testing-library/react";
+import { ExternalProjectRevisionSummary } from "pages/cif-external/project-revision/[projectRevision]";
+import PageTestingHelper from "tests/helpers/pageTestingHelper";
+import compiledProjectRevisionExternalSummaryQuery, {
+  ProjectRevisionExternalSummaryQuery,
+} from "__generated__/ProjectRevisionExternalSummaryQuery.graphql";
+
+const defaultMockResolver = {
+  ProjectRevision(context, generateId) {
+    return {
+      id: `mock-id`,
+      projectId: 123,
+      changeStatus: "pending",
+      projectByProjectId: {
+        proposalReference: "001",
+      },
+      projectFormChange: {
+        id: `mock-project-form-${generateId()}`,
+        newFormData: {
+          projectName: "My Fancy Project",
+          legalName: "Optimus Prime",
+          fundingStreamRfpId: 7,
+        },
+      },
+    };
+  },
+};
+
+const pageTestingHelper =
+  new PageTestingHelper<ProjectRevisionExternalSummaryQuery>({
+    pageComponent: ExternalProjectRevisionSummary,
+    compiledQuery: compiledProjectRevisionExternalSummaryQuery,
+    defaultQueryResolver: defaultMockResolver,
+    defaultQueryVariables: {
+      projectRevision: "mock-id",
+    },
+  });
+
+describe("External Review Page", () => {
+  beforeEach(() => {
+    pageTestingHelper.reinit();
+  });
+
+  it("displays review statement", () => {
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
+    expect(
+      screen.getByText(/Review your responses to ensure correct submission./i)
+    ).toBeInTheDocument();
+  });
+
+  it("displays the application form review", () => {
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
+    screen.logTestingPlaygroundURL();
+    expect(screen.getByText(/RFP Year ID/i)).toBeInTheDocument();
+    expect(screen.getByText(/7/i)).toBeInTheDocument();
+    expect(screen.getByText(/Project Name/i)).toBeInTheDocument();
+    expect(screen.getByText(/My Fancy Project/i)).toBeInTheDocument();
+    expect(screen.getByText(/Legal name/i)).toBeInTheDocument();
+    expect(screen.getByText(/Optimus Prime/i)).toBeInTheDocument();
+  });
+});
