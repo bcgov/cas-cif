ee4e09c7b07dd12a36092460b62c64f037f51f4e
SeSo <sobhani.sepehr@gmail.com>
2023-05-02 16:07:01 -0700

    fix: fix migration equality in-place

--- schema/revert/functions/migration_funding_parameter_form_changes_to_single_form_change@1.7.0.sql
+++
@@ -1,7 +0,0 @@
--- Revert cif:functions/migration_funding_parameter_form_changes_to_single_form_change from pg
-
-begin;
-
-drop function cif_private.migration_funding_parameter_form_changes_to_single_form_change;
-
-commit;
