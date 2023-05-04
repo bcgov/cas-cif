2c5e9b7b616c175636f3e1682fb56028ff454d4b
SeSo <sobhani.sepehr@gmail.com>
2023-05-04 15:12:25 -0700

    chore: mutation to create a form_change with project attachment fields

---
+++ app/mutations/attachment/addProjectAttachmentToRevision.ts
@@ -0,0 +1,31 @@
+import { graphql } from "react-relay";
+import type { addProjectAttachmentToRevisionMutation } from "addProjectAttachmentToRevisionMutation.graphql";
+import useMutationWithErrorMessage from "mutations/useMutationWithErrorMessage";
+
+const mutation = graphql`
+  mutation addProjectAttachmentToRevisionMutation(
+    $connections: [ID!]!
+    $input: AddProjectAttachmentToRevisionInput!
+  ) {
+    addProjectAttachmentToRevision(input: $input) {
+      formChangeEdge @appendEdge(connections: $connections) {
+        cursor
+        node {
+          id
+          rowId
+          newFormData
+          operation
+          changeStatus
+        }
+      }
+    }
+  }
+`;
+
+const useAddProjectAttachmentToRevision = () =>
+  useMutationWithErrorMessage<addProjectAttachmentToRevisionMutation>(
+    mutation,
+    () => "An error occurred while attempting to create an attachment."
+  );
+
+export { mutation, useAddProjectAttachmentToRevision };
---
+++ schema/verify/mutations/add_project_attachment_to_revision.sql
@@ -0,0 +1,7 @@
+-- Verify cif:mutations/add_project_attachment_to_revision on pg
+
+begin;
+
+select pg_get_functiondef('cif.add_project_attachment_to_revision(int, int, int)'::regprocedure);
+
+rollback;
