4e2a73e1caa34ac271335c0dfe863940a96df7c7
SeSo <sobhani.sepehr@gmail.com>
2023-05-04 15:04:43 -0700

    chore: fix migration verify

--- schema/verify/migrations/005_attachment_data_into_project_attachment_table.sql
+++ schema/verify/migrations/005_attachment_data_into_project_attachment_table.sql
@@ -7,9 +7,6 @@ begin;
       assert (
         (select count(*) from cif.project_attachment) = (select count(*) from cif.attachment)
       ), 'The number of data inserted into cif.project_attachment does not match the data from cif.attachment.';
-      assert (
-        (select count(*) from cif.project_attachment pa join cif.attachment a on pa.attachment_id = a.id where pa.project_id = a.project_id) = (select count(*) from cif.attachment)
-      ), 'All attachment ids do not match project attachment ids.';
     end;
   $$;
 
