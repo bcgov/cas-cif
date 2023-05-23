bdc15baddf1ad1fda6736289783b57a96b95773b
Brianna Cerkiewicz <brianna@button.is>
2023-05-23 15:50:21 -0700

    chore: remove project_id from add_project_attachment_to_revision

--- schema/deploy/mutations/add_project_attachment_to_revision.sql
+++ schema/deploy/mutations/add_project_attachment_to_revision.sql
@@ -2,7 +2,8 @@
 
 begin;
 
-create or replace function cif.add_project_attachment_to_revision(project_id int, attachment_id int, revision_id int)
+drop function cif.add_project_attachment_to_revision(project_id int, attachment_id int, revision_id int);
+create or replace function cif.add_project_attachment_to_revision(revision_id int,attachment_id int)
 returns cif.form_change
 as $function$
   insert into cif.form_change(
@@ -17,14 +18,14 @@ as $function$
 ) values (
     format(
       '{"projectId": %s, "attachmentId": %s }',
-      $1,
+      (select form_data_record_id from cif.form_change where form_data_schema_name='cif' and form_data_table_name='project' and project_revision_id=$1),
       $2
     )::jsonb,
     'create',
     'cif',
     'project_attachment',
     (select nextval(pg_get_serial_sequence('cif.project_attachment', 'id'))),
-    $3,
+    $1,
     'pending',
     'project_attachment'
   ) returning *;
@@ -35,7 +36,7 @@ grant usage, select on sequence cif.project_attachment_id_seq to cif_internal, c
 
 comment on function cif.add_project_attachment_to_revision is
 $comment$
-  Creates a new project_attachment form_change record for the given project_id, attachment_id, and revision_id.
+  Creates a new project_attachment form_change record for the given revision_id and attachment_id.
 $comment$;
 
 commit;
