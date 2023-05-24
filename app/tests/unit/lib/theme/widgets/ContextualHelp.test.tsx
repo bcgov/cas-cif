f7085420a99fcc0f92de28f7f55d3b89d4efc291
SeSo <sobhani.sepehr@gmail.com>
2023-05-24 16:57:01 -0700

    test: add unit tests

---
+++ app/tests/unit/lib/theme/widgets/ContextualHelp.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+import ContextualHelp from "lib/theme/widgets/ContextualHelp";
+
+describe("ContextualHelp component", () => {
+  const titleMock = "Tooltip Title";
+  const labelMock = "Field Label";
+
+  test("renders the tooltip with correct title and label and matches the snapshot", () => {
+    const componentUnderTest = render(
+      <ContextualHelp
+        title={titleMock}
+        icon={faExclamationCircle}
+        label={labelMock}
+      />
+    );
+    const tooltipElement = screen.getByRole("tooltip");
+    const tooltipIcon = screen.getByRole("img", {
+      hidden: true,
+    });
+    expect(tooltipElement).toBeInTheDocument();
+    expect(tooltipElement).toHaveAttribute(
+      "aria-labelledby",
+      "field-label-tooltip"
+    );
+    expect(tooltipIcon).toBeInTheDocument();
+
+    expect(componentUnderTest.container).toMatchSnapshot();
+  });
+
+  test("renders the tooltip with default icon when not provided", () => {
+    render(<ContextualHelp title={titleMock} label={labelMock} />);
+
+    const defaultIcon = screen.getByRole("img", {
+      hidden: true,
+    });
+    expect(defaultIcon).toHaveAttribute("data-icon", "exclamation-circle");
+  });
+});
