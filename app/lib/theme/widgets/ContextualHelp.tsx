1bd32230d418e01bf9d935490ac9bf46789e69ed
SeSo <sobhani.sepehr@gmail.com>
2023-05-26 11:03:52 -0700

    chore: design and accessibility tweaks to Tooltip component

--- app/lib/theme/widgets/ContextualHelp.tsx
+++ app/lib/theme/widgets/ContextualHelp.tsx
@@ -2,9 +2,10 @@ import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Tooltip from "@mui/material/Tooltip";
+import { BC_GOV_LINKS_COLOR, TASKLIST_GREY_BG_COLOR } from "../colors";
 
 /**
- * @param {string} title - The title of the tooltip
+ * @param {string} title - The title of the tooltip to be displayed (can be HTML)
  * @param {IconDefinition} icon - The icon to display in the tooltip; defaults to faExclamationCircle
  * @param {string} placement - The placement of the tooltip; defaults to "top"
  * @param {string} label - The label of the tooltip to be used as the aria-labelledby attribute; this gets passed by the parent component(FieldLabel)
@@ -12,7 +13,7 @@ import Tooltip from "@mui/material/Tooltip";
  * To use this component, simply add "ui:tooltip" object to the uiSchema of the field you want to add a tooltip to.
  * Example:
  * "ui:tooltip": {
- *   "title": "This is the title of the tooltip",
+ *   "title": "<div>Some content here with <a>Links</a> and other HTML tags</div>",
  *   "icon": faExclamationCircle,
  *   "placement": "top"
  * }
@@ -21,14 +22,26 @@ import Tooltip from "@mui/material/Tooltip";
 interface ContextualHelpProps {
   title: string;
   icon?: IconDefinition;
-  placement?: "top" | "bottom" | "left" | "right";
+  placement?:
+    | "top"
+    | "bottom"
+    | "left"
+    | "right"
+    | "bottom-end"
+    | "bottom-start"
+    | "left-end"
+    | "left-start"
+    | "right-end"
+    | "right-start"
+    | "top-end"
+    | "top-start";
   label: string;
 }
 
 const ContextualHelp: React.FC<ContextualHelpProps> = ({
   title,
   icon = faExclamationCircle,
-  placement = "top",
+  placement = "bottom",
   label,
 }) => {
   // generate a unique id for the tooltip based on the label
@@ -36,21 +49,37 @@ const ContextualHelp: React.FC<ContextualHelpProps> = ({
   return (
     <>
       <Tooltip
-        title={title}
+        title={<span dangerouslySetInnerHTML={{ __html: title }} />}
         arrow
         placement={placement}
         role="tooltip"
         id={tooltipId}
         tabIndex={0}
         className="tooltip"
+        aria-label={tooltipId}
       >
-        <span aria-labelledby={tooltipId}>
-          <FontAwesomeIcon icon={icon} name="tooltip" color="#767676" />
+        <span>
+          <FontAwesomeIcon
+            icon={icon}
+            name="tooltip"
+            color={BC_GOV_LINKS_COLOR}
+          />
         </span>
       </Tooltip>
       <style jsx>{`
         .tooltip {
           cursor: pointer;
+        }
+        :global(.MuiTooltip-tooltip) {
+          background-color: ${TASKLIST_GREY_BG_COLOR};
+          box-shadow: 5px 5px 5px #888888;
+          color: black;
+        }
+        :global(.MuiTooltip-tooltip .MuiTooltip-arrow) {
+          color: ${TASKLIST_GREY_BG_COLOR};
+        }
+        :global(.MuiTooltip-tooltip a) {
+          color: ${BC_GOV_LINKS_COLOR};
         }
       `}</style>
     </>
