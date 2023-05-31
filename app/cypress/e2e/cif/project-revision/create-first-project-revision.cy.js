01e0b65cdfdb3d3d6e615742874c86d8409ce695
Brianna Cerkiewicz <brianna@button.is>
2023-05-31 15:10:25 -0700

    test: temporarily comment out attachments e2e pending hard delete

--- app/cypress/e2e/cif/project-revision/create-first-project-revision.cy.js
+++ app/cypress/e2e/cif/project-revision/create-first-project-revision.cy.js
@@ -161,8 +161,8 @@ describe("when creating a project, the project page", () => {
     // Add attachments
     cy.url().should("include", "/form/8");
     // temporarily commented out until hard delete is implemented in #1703
-    cy.get("input[type=file]").selectFile("./cypress/fixtures/1dummy.pdf");
-    cy.findByText("1dummy.pdf").should("be.visible");
+    // cy.get("input[type=file]").selectFile("./cypress/fixtures/1dummy.pdf");
+    // cy.findByText("1dummy.pdf").should("be.visible");
     cy.findByText(/Submit project attachments/i).click();
 
     //review and submit
@@ -274,7 +274,7 @@ describe("when creating a project, the project page", () => {
       .should("have.text", "$12.00");
     // attachments sections
     // temporarily commented out until hard delete is implemented in #1703
-    cy.findByText("1dummy.pdf").should("be.visible");
+    // cy.findByText("1dummy.pdf").should("be.visible");
 
     cy.happoAndAxe("Project summary Form", "filled", "main", true);
     cy.findByRole("button", { name: /^submit/i }).click();
