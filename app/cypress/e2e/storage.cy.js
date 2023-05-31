ad892a277e72679455c53251c73e5580ae551e5e
Brianna Cerkiewicz <brianna@button.is>
2023-05-31 14:58:44 -0700

    test: remove unused cypress upload test

--- app/cypress/e2e/storage.cy.js
+++
@@ -1,7 +0,0 @@
-// TODO add tests for document upload/download once the storage service is round-tripping
-// describe("The attachments component", () => {
-//   it("uploads a file", () => {
-//     cy.visit("/upload");
-//     cy.get('[data-cy="file-input"]').attachFile("cif-test-document.docx");
-//   });
-// });
