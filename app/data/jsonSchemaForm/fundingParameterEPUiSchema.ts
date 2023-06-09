7a5ade89c3d3b41aeab6babd8ddc02731ec20531
Sam Warren <samwarren4@gmail.com>
2023-06-09 12:18:12 -0700

    chore: add tooltips for Budgets, Expenses & Payments

--- app/data/jsonSchemaForm/fundingParameterEPUiSchema.ts
+++ app/data/jsonSchemaForm/fundingParameterEPUiSchema.ts
@@ -21,26 +21,41 @@ export const fundingParameterEPUiSchema = {
   ],
   totalProjectValue: {
     "ui:widget": "CalculatedValueWidget",
+    "ui:tooltip": {
+      text: "<div>Total Project Value = Maximum Funding Amount + Proponent Cost + Additional Funding Amount (Source 1) + Additional Funding Amount (Source 2) + ... + Additional Funding amount (Source N).</div>",
+    },
     isMoney: true,
     hideOptional: true,
     calculatedValueFormContextProperty: "calculatedTotalProjectValue",
   },
   maxFundingAmount: {
     "ui:widget": "NumberWidget",
+    "ui:tooltip": {
+      text: "<div>The maximum payment amount that the proponent can receive from CIF.</div>",
+    },
     isMoney: true,
   },
   provinceSharePercentage: {
     "ui:widget": "NumberWidget",
+    "ui:tooltip": {
+      text: "<div>Typically, 50% at most.</div>",
+    },
     isPercentage: true,
     numberOfDecimalPlaces: 2,
   },
   holdbackPercentage: {
     "ui:widget": "NumberWidget",
+    "ui:tooltip": {
+      text: "<div><ul><li>Typically, 10%.</li><li>2019 projects may have percentages higher than 10%.</li></ul></div>",
+    },
     isPercentage: true,
     numberOfDecimalPlaces: 2,
   },
   proponentsSharePercentage: {
     "ui:widget": "CalculatedValueWidget",
+    "ui:tooltip": {
+      text: "<div><ul><li>Proponent's share percentage = Proponent Cost / Total Project Value</li><li>The Proponent's Share Percentage should be NO LESS than 25%.</li><li>For 2021 projects, the Proponent's Share Percentage should be NO LESS than 25%.</li></ul></div>",
+    },
     numberOfDecimalPlaces: 2,
     isPercentage: true,
     calculatedValueFormContextProperty: "calculatedProponentsSharePercentage",
@@ -68,6 +83,9 @@ export const fundingParameterEPUiSchema = {
   },
   proponentCost: {
     "ui:widget": "NumberWidget",
+    "ui:tooltip": {
+      text: "<div>The amount of expenses paid by the proponent.</div>",
+    },
     isMoney: true,
   },
   contractStartDate: {
@@ -78,24 +96,36 @@ export const fundingParameterEPUiSchema = {
   },
   eligibleExpensesToDate: {
     "ui:widget": "CalculatedValueWidget",
+    "ui:tooltip": {
+      text: "<div>The accumulated Total Eligible Expenses from all completed Milestone Reports.</div>",
+    },
     calculatedValueFormContextProperty: "calculatedEligibleExpensesToDate",
     hideOptional: true,
     isMoney: true,
   },
   grossPaymentsToDate: {
     "ui:widget": "CalculatedValueWidget",
+    "ui:tooltip": {
+      text: "<div><ul><li>The accumulated Gross Payment Amounts from all completed Milestone Reports.</li><li>The Total Gross Payment Amoun To Date should be NO GREATER than the Maximum Funding Amount.</li></ul></div>",
+    },
     calculatedValueFormContextProperty: "calculatedGrossPaymentsToDate",
     hideOptional: true,
     isMoney: true,
   },
   holdbackAmountToDate: {
     "ui:widget": "CalculatedValueWidget",
+    "ui:tooltip": {
+      text: "<div>The accumulated Holdback Payment Amounts from all completed Milestone Reports.</div>",
+    },
     calculatedValueFormContextProperty: "calculatedHoldbackAmountToDate",
     hideOptional: true,
     isMoney: true,
   },
   netPaymentsToDate: {
     "ui:widget": "CalculatedValueWidget",
+    "ui:tooltip": {
+      text: "<div>The accumulated Net Payment Amounts from all completed Milestone Reports.</div>",
+    },
     calculatedValueFormContextProperty: "calculatedNetPaymentsToDate",
     hideOptional: true,
     isMoney: true,
@@ -111,6 +141,9 @@ export const fundingParameterEPUiSchema = {
       amount: {
         "ui:title": `Additional Funding Amount`,
         "ui:widget": "NumberWidget",
+        "ui:tooltip": {
+          text: "<div><ul><li>The amount of payment from Funding Source N other than CIF.</li><li>Typically found in the original proposal (solicitation folder of the LAN).</li></ul></div>",
+        },
         isMoney: true,
       },
       status: {
