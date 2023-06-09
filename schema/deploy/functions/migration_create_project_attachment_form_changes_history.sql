b4eb4ae062a1ee2c7a4309003dacb245fa859422
SeSo <sobhani.sepehr@gmail.com>
2023-06-09 11:23:53 -0700

    chore: refactor to use sql cte instead of loop

--- schema/deploy/functions/migration_create_project_attachment_form_changes_history.sql
+++ schema/deploy/functions/migration_create_project_attachment_form_changes_history.sql
@@ -24,43 +24,52 @@ begin
       from cif.form_change
       where id = form_change_record.id
       and (select change_status from cif.project_revision where id = form_change_record.project_revision_id) = 'committed';
+
     -- select all project_revisions for the project_id of the form_change that have a higher id than the project_revision_id of the form_change
-    for project_revision_record in select * from cif.project_revision
+    with higher_project_revisions as (
+      select *
+      from cif.project_revision
       where project_id = (form_change_record.new_form_data->>'projectId')::integer
-      and id > form_change_record.project_revision_id
-    loop
-      -- if project revision doesn't have the exact same form_change:
-      -- copy the form_change to project_revision and set the change_status to project_revision change_status and update the created_at and updated_at to be the same as the project_revision
-      -- this is done to make sure that this function can be run multiple times without creating duplicate form_changes
-      if not exists (
-        select * from cif.form_change
-        where project_revision_id = project_revision_record.id
+        and id > form_change_record.project_revision_id
+    )
+    -- if project revision doesn't have the exact same form_change:
+    -- copy the form_change to project_revision and set the change_status to project_revision change_status and update the created_at and updated_at to be the same as the project_revision
+    insert into cif.form_change (
+      operation,
+      form_data_schema_name,
+      json_schema_name,
+      project_revision_id,
+      form_data_table_name,
+      form_data_record_id,
+      new_form_data,
+      change_status,
+      created_by,
+      created_at,
+      updated_by,
+      updated_at
+    )
+    select
+      'update', -- Need to set this to update to prevent duplicate key error on inserting the same form_change
+      'cif',
+      'project_attachment',
+      higher_project_revisions.id,
+      'project_attachment',
+      form_change_record.form_data_record_id,
+      form_change_record.new_form_data,
+      higher_project_revisions.change_status,
+      form_change_record.created_by,
+      higher_project_revisions.created_at,
+      form_change_record.updated_by,
+      higher_project_revisions.updated_at
+    from
+      higher_project_revisions
+    where not exists (
+      select *
+      from cif.form_change
+      where project_revision_id = higher_project_revisions.id
         and form_data_record_id = form_change_record.form_data_record_id
         and form_data_table_name = 'project_attachment'
-      ) then
-        insert into cif.form_change (
-          operation, form_data_schema_name,
-          json_schema_name, project_revision_id,
-          form_data_table_name, form_data_record_id,
-          new_form_data, change_status,
-          created_by, created_at,
-          updated_by, updated_at
-        ) values (
-          'update', -- need to set this to update to prevent duplicate key error on inserting the same form_change
-          'cif',
-          'project_attachment',
-          project_revision_record.id,
-          'project_attachment',
-          form_change_record.form_data_record_id,
-          form_change_record.new_form_data,
-          project_revision_record.change_status,
-          form_change_record.created_by,
-          project_revision_record.created_at,
-          form_change_record.updated_by,
-          project_revision_record.updated_at
-        );
-      end if;
-    end loop;
+    );
   end loop;
 end;
 
