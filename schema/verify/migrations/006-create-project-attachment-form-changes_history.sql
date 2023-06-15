af14145d95e8fe373da84600e513f726cfc9a2c6
Dylan Leard <dylan@button.is>
2023-06-15 16:17:31 -0700

    chore: fix sqitch verify file

--- schema/verify/migrations/006-create-project-attachment-form-changes_history.sql
+++ schema/verify/migrations/006-create-project-attachment-form-changes_history.sql
@@ -6,10 +6,13 @@ begin;
     begin
       assert (
         select count(*) = 0
-        from cif.form_change
-        where form_data_table_name='project_attachment'
-        and change_status='pending'
-      ), 'There are pending project_attachment form changes.';
+          from cif.form_change fc
+          join cif.project_revision pr
+          on fc.project_revision_id = pr.id
+          and form_data_table_name = 'project_attachment'
+          and fc.change_status = 'pending'
+          and pr.change_status = 'committed'
+      );
     end;
   $$;
 
