7e58da0850bc73017f7067aa6a901258ce3b0303
Brianna Cerkiewicz <brianna@button.is>
2023-06-16 09:02:25 -0700

    chore: migration to reformat old form changes

--- schema/deploy/functions/migration_emission_intensity_report_form_changes_to_single_form_change.sql
+++ schema/deploy/functions/migration_emission_intensity_report_form_changes_to_single_form_change.sql
@@ -6,59 +6,54 @@ create or replace function cif_private.migration_emission_intensity_report_form_
 returns void as
 $migration$
 
-   with ei_form_changes as (
-    select
-      reporting_requirement_form_change.id as reporting_requirement_id,
-      coalesce(reporting_requirement_form_change.new_form_data, '{}'::jsonb) as reporting_requirement_data,
-      ei_form_change.id as ei_id,
-      coalesce(ei_form_change.new_form_data, '{}'::jsonb) as ei_data
-    from cif.form_change as reporting_requirement_form_change
-    left join cif.form_change as ei_form_change
-      on (ei_form_change.new_form_data->>'reportingRequirementId')::numeric = reporting_requirement_form_change.form_data_record_id
-      and (ei_form_change.new_form_data->>'reportingRequirementId')::numeric is not null
-    where reporting_requirement_form_change.form_data_table_name = 'reporting_requirement'
-      and reporting_requirement_form_change.json_schema_name = 'reporting_requirement'
-      and reporting_requirement_form_change.new_form_data->>'reportType' in ('TEIMP')
-  ),
-  new_data as (
-      select
-        reporting_requirement_id as id,
-        jsonb_strip_nulls(
-          jsonb_build_object(
-            -- reporting requirement data
-            'reportType', reporting_requirement_data->'reportType',
-            'reportDueDate', reporting_requirement_data->'reportDueDate',
-            'submittedDate', reporting_requirement_data->'submittedDate',
-            'comments', reporting_requirement_data->'comments',
-            'reportingRequirementIndex', reporting_requirement_data->'reportingRequirementIndex',
-            -- ei report data
-            'measurement_period_start_date', ei_data->'measurement_period_start_date',
-            'measurement_period_end_date', ei_data->'measurement_period_end_date',
-            'emission_functional_unit', ei_data->'emission_functional_unit',
-            'production_functional_unit', ei_data->'production_functional_unit',
-            'baseline_emission_intensity', ei_data->'baseline_emission_intensity',
-            'target_emission_intensity', ei_data->'target_emission_intensity',
-            'post_project_emission_intensity', ei_data->'post_project_emission_intensity',
-            'total_lifetime_emission_reduction', ei_data->'total_lifetime_emission_reduction',
-            'adjusted_emissions_intensity_performance', ei_data->'adjusted_emissions_intensity_performance',
-            'adjusted_holdback_payment_amount', ei_data->'adjusted_holdback_payment_amount',
-            'date_sent_to_csnr', ei_data->'date_sent_to_csnr'
-          )
-        ) as new_form_data
-      from ei_form_changes
-  ),
-  deleted_form_changes as (
-    delete from cif.form_change where id in (
-      select ei_id as id from ei_form_changes
-    ) returning *
-  )
-  update cif.form_change fc
-  set
-    new_form_data = new_data.new_form_data,
-    json_schema_name = 'reporting_requirement'
-  from new_data
-  where fc.id=new_data.id;
 
+with
+  raw_ei_data as
+        (
+          select id, new_form_data, project_revision_id, (new_form_data->>'reportingRequirementId')::int as rep_req_id
+          from cif.form_change
+          where json_schema_name = 'emission_intensity_report'
+        ),
+  -- Some form changes have nested, duplicated key-value pairs. This CTE standardizes the new_form_data structure
+  ei_data as
+        (
+          select id, project_revision_id, rep_req_id,  jsonb_strip_nulls(jsonb_build_object(
+              'measurementPeriodStartDate', raw_ei_data.new_form_data->>'measurementPeriodStartDate',
+              'measurementPeriodEndDate', raw_ei_data.new_form_data->>'measurementPeriodEndDate',
+              'emissionFunctionalUnit', raw_ei_data.new_form_data->>'emissionFunctionalUnit',
+              'productionFunctionalUnit', raw_ei_data.new_form_data->>'productionFunctionalUnit',
+              'baselineEmissionIntensity', raw_ei_data.new_form_data->>'baselineEmissionIntensity',
+              'targetEmissionIntensity', raw_ei_data.new_form_data->>'targetEmissionIntensity',
+              'postProjectEmissionIntensity', raw_ei_data.new_form_data->>'postProjectEmissionIntensity',
+              'totalLifetimeEmissioSnReduction',  raw_ei_data.new_form_data->>'totalLifetimeEmissioSnReduction',
+              'adjustedEmissionsIntensityPerformance', raw_ei_data.new_form_data->>'adjustedEmissionsIntensityPerformance',
+              'adjustedHoldbackPaymentAmount', raw_ei_data.new_form_data->>'adjustedHoldbackPaymentAmount',
+              'dateSentToCsnr', raw_ei_data.new_form_data->>'dateSentToCsnr'))::jsonb
+            as clean_new_form_data
+        from raw_ei_data
+        )
+
+  update cif.form_change fc1
+      set
+        -- Need to rewrite rather than just copy the new_form_data because project id key was project_id instead of projectId
+        new_form_data =  jsonb_strip_nulls(jsonb_build_object(
+          'projectId', new_form_data->>'project_id',
+          'reportType', new_form_data->>'reportType',
+          'reportDueDate', new_form_data->>'reportDueDate',
+          'submittedDate', new_form_data->>'submittedDate',
+          'comments', new_form_data->>'comments',
+          'reportingRequirementIndex', new_form_data->>'reportingRequirementIndex'
+        ))::jsonb || (
+                select clean_new_form_data from ei_data
+                where ei_data.rep_req_id = fc1.form_data_record_id
+                and ei_data.project_revision_id = fc1.project_revision_id
+                and ei_data.rep_req_id is not null
+              ),
+        json_schema_name = 'emission_intensity'
+      where json_schema_name = 'emission_intensity_reporting_requirement';
+
+delete from cif.form_change
+    where json_schema_name = 'emission_intensity_report';
 
 $migration$ language sql volatile;
 
--- schema/test/unit/functions/migration_emission_intensity_report_form_changes_to_single_form_change_test.sql
+++ schema/test/unit/functions/migration_emission_intensity_report_form_changes_to_single_form_change_test.sql
@@ -3,7 +3,7 @@
 begin;
 
 
-select plan(5);
+select plan(6);
 
 -- Test Setup --
 
@@ -11,6 +11,7 @@ truncate table cif.project restart identity cascade;
 
 insert into cif.operator(id, legal_name) overriding system value values (1, 'Test Operator');
 
+select cif.create_project(1);
 select cif.create_project(1);
 select cif.create_project(1);
 update cif.form_change
@@ -25,13 +26,17 @@ update cif.form_change
       'sectorName', 'Agriculture',
       'projectType', 'Carbon Capture',
       'score', 50
-      )
+      )::jsonb
   where form_data_table_name='project';
 
 
--- insert into cif.form(slug, json_schema, description) values ('emission_intensity', '{}', 'test');
+insert into cif.form(slug, json_schema, description) values
+('emission_intensity_reporting_requirement', '{}', 'test'),
+('emission_intensity_report', '{}', 'test');
+
 
 insert into cif.form_change(
+    id,
       new_form_data,
       operation,
       form_data_schema_name,
@@ -41,69 +46,135 @@ insert into cif.form_change(
       change_status,
       json_schema_name
     )
+    overriding system value
 values
        -- first project with no data
-       ('{"projectId": 1, "reportType": "TEIMP", "reportingRequirementIndex": 1 }'::jsonb,
+
+       (101,
+        '{"project_id": 1, "reportType": "TEIMP", "reportingRequirementIndex": 1 }'::jsonb,
         'create',
         'cif',
         'reporting_requirement',
         1,
         1,
         'pending',
-        'emission_intensity')
+        'emission_intensity_reporting_requirement')
        ,
-        -- second project with all the data
-        (json_build_object(
+       (102,
+        '{"reportingRequirementId": 1 }'::jsonb,
+        'create',
+        'cif',
+        'emission_intensity_report',
+        null,
+        1,
+        'pending',
+        'emission_intensity_report'),
+        -- second project with all the new_form_data unnested
+        (201,
+        json_build_object(
+            'project_id',2,
+            'reportType','TEIMP',
+            'reportingRequirementIndex', 1,
+            'reportDueDate', '2023-06-15T23:59:59.999-07:00',
+            'submittedDate', '2023-06-30T23:59:59.999-07:00',
+            'comments', 'comments'
+            )::jsonb,
+        'create',
+        'cif',
+        'reporting_requirement',
+        2,
+        2,
+        'pending',
+        'emission_intensity_reporting_requirement'),
+        (202,
+        json_build_object(
+            'reportingRequirementId', 2,
             'adjustedEmissionsIntensityPerformance', 98,
             'baselineEmissionIntensity', 324.25364,
-            'comments', 'comments',
             'dateSentToCsnr', '2023-07-01T23:59:59.999-07:00',
             'emissionFunctionalUnit','tCO2e',
             'measurementPeriodEndDate','2023-07-01T23:59:59.999-07:00',
             'measurementPeriodStartDate', '2023-06-01T23:59:59.999-07:00',
             'postProjectEmissionIntensity', 124.35,
             'productionFunctionalUnit', 'unit',
-            'reportDueDate', '2023-06-15T23:59:59.999-07:00',
-            'reportType', 'TEIMP',
-            'reportingRequirementIndex', 1,
-            'submittedDate', '2023-06-30T23:59:59.999-07:00',
             'targetEmissionIntensity', 23.2357,
             'totalLifetimeEmissionReduction', 44.4224
-            ),
+            )::jsonb,
+        'create',
+        'cif',
+        'emission_intensity_report',
+        null,
+        2,
+        'pending',
+        'emission_intensity_report'),
+        -- third project with nested json schema
+        (301,
+        '{
+            "comments": "comments",
+            "project_id": 3,
+            "reportType": "TEIMP",
+            "reportDueDate": "2023-06-01T23:59:59.999-07:00",
+            "submittedDate": "2023-06-15T23:59:59.999-07:00",
+            "reportingRequirementIndex": 1
+            }'::jsonb,
         'create',
         'cif',
         'reporting_requirement',
-        2,
-        2,
+        3,
+        3,
         'pending',
-        'emission_intensity'),
+        'emission_intensity_reporting_requirement'),
+        (302,
+        '{
+            "teimpReporting": {
+              "emissionFunctionalUnit": "tCO2e",
+              "targetEmissionIntensity": 466.555588,
+              "measurementPeriodEndDate": "2023-06-15T23:59:59.999-07:00",
+              "productionFunctionalUnit": "unit",
+              "baselineEmissionIntensity": 2569.555,
+              "measurementPeriodStartDate": "2023-06-01T23: 59: 59.999-07:00",
+              "postProjectEmissionIntensity": 654.12345678,
+              "totalLifetimeEmissionReduction": 66
+            },
+            "uponCompletion": {
+              "dateSentToCsnr": "2023-06-22T23: 59: 59.999-07:00",
+              "adjustedEmissionsIntensityPerformance": 95.86
+            },
+            "emissionFunctionalUnit": "tCO2e",
+            "targetEmissionIntensity": 466.555588,
+            "measurementPeriodEndDate": "2023-06-15T23:59:59.999-07:00",
+            "productionFunctionalUnit": "unit",
+            "baselineEmissionIntensity": 2569.555,
+            "measurementPeriodStartDate": "2021-06-01T23: 59:59.999-07:00",
+            "postProjectEmissionIntensity": 654.12345678,
+            "adjustedEmissionsIntensityPerformance": 95.86,
+            "dateSentToCsnr": "2023-06-22T23:59:59.999-07:00",
+            "totalLifetimeEmissionReduction": 66,
+            "reportingRequirementId": 3
+          }'::jsonb,
+        'create',
+        'cif',
+        'emission_intensity_report',
+        null,
+        3,
+        'pending',
+        'emission_intensity_report'),
         -- An existing reporting requirement, different from a emission intensity report
-        ('{"projectId": 1, "reportType": "Quarterly", "reportingRequirementIndex": 1 }'::jsonb,
+        (401,
+        '{"projectId": 1, "reportType": "Quarterly", "reportingRequirementIndex": 1 }'::jsonb,
         'create',
         'cif',
         'reporting_requirement',
         3,
         1,
         'pending',
-        'reporting_requirement'),
-
-        -- A separate form change without a revision that should be left untouched
-        -- even if it makes little sense, context-wise
-        (
-          '{"reportingRequirementId": 12, "holdbackAmountToDate": 12345 }'::jsonb,
-          'create',
-          'cif',
-          'emission_intensity',
-          null,
-          null,
-          'committed',
-          'reporting_requirement');
+        'reporting_requirement');
 
 
 select cif.commit_project_revision(1);
 
 -- We can't use the cif.create_project_revision() function since it is already updated with the new schema, so we create the form_changes manually.
-insert into cif.project_revision(project_id, change_status) values (1, 'pending');
+-- insert into cif.project_revision(project_id, change_status) values (1, 'pending');
 
 select cif.create_form_change(
   operation => 'update',
@@ -118,6 +189,8 @@ select cif.create_form_change(
 -- End Test Setup --
 
 
+
+
 -- At this point, we have 3 revisions:
   -- One committed (id=1) and one pending (id=3) for the project_id=1
   -- One pending (id=2) for the project_id=2
@@ -129,18 +202,28 @@ select cif_private.migration_emission_intensity_report_form_changes_to_single_fo
 
 alter table cif.form_change enable trigger _100_committed_changes_are_immutable, enable trigger _100_timestamps;
 
+
+
+
 select is(
   (select count(*) from cif.form_change where (form_data_table_name='emission_intensity_report')),
   0::bigint,
   'There are no form_change records left for the emission_intensity_report table'
 );
 
+select is(
+  (select count(*) from cif.form_change where (json_schema_name='emission_intensity_reporting_requirement')),
+  0::bigint,
+  'There are no form_change records left using the emission_intensity_reporting_requirement json schema'
+);
+
 
 select is(
   (select count(*) from cif.form_change where form_data_table_name='reporting_requirement' and json_schema_name='emission_intensity'),
-  3::bigint,
+  4::bigint, -- existing ids 101, 201, 301, and 5 which was created as part of the new project revision
   'The reporting requirements for emission intensity reports have been converted to the new schema'
 );
+
 
 -- it takes the emission_intensity data from existing revisions and puts them into the new json schema
   -- for committed form changes
@@ -150,9 +233,16 @@ select results_eq(
     select id, change_status, new_form_data from cif.form_change where form_data_table_name='reporting_requirement' and json_schema_name='emission_intensity' order by id;
   $$,
   $$
---   monkeyfuzz projectId is in here
-   values (3, 'committed'::varchar, '{"reportType": "TEIMP", "reportingRequirementIndex": 1}'::jsonb),
-          (6, 'pending'::varchar, '{
+   values
+        (5, 'pending'::varchar, '{
+            "reportType": "TEIMP",
+            "reportingRequirementIndex": 1,
+            "projectId": 1}'::jsonb),
+        (101, 'committed'::varchar, '{
+            "reportType": "TEIMP",
+            "reportingRequirementIndex": 1,
+            "projectId": 1}'::jsonb),
+        (102, 'pending'::varchar, '{
             "adjustedEmissionsIntensityPerformance": 98,
             "baselineEmissionIntensity": 324.25364,
             "comments": "comments",
@@ -167,44 +257,62 @@ select results_eq(
             "reportingRequirementIndex": 1,
             "submittedDate": "2023-06-30T23:59:59.999-07:00",
             "targetEmissionIntensity": 23.2357,
-            "totalLifetimeEmissionReduction": 44.4224
+            "totalLifetimeEmissionReduction": 44.4224,
+            "projectId": 2
           }'::jsonb),
-          (12, 'pending'::varchar, '{"reportType": "TEIMP", "reportingRequirementIndex": 1}'::jsonb)
-
+        (103,'pending'::varchar, '{
+            "comments": "comments",
+            "project_id": 3,
+            "reportType": "TEIMP",
+            "reportDueDate": "2023-06-01T23:59:59.999-07:00",
+            "submittedDate": "2023-06-15T23:59:59.999-07:00",
+            "reportingRequirementIndex": 1,
+            "emissionFunctionalUnit": "tCO2e",
+            "targetEmissionIntensity": 466.555588,
+            "measurementPeriodEndDate": "2023-06-15T23:59:59.999-07:00",
+            "productionFunctionalUnit": "unit",
+            "baselineEmissionIntensity": 2569.555,
+            "measurementPeriodStartDate": "2023-06-01T23: 59: 59.999-07:00",
+            "postProjectEmissionIntensity": 654.12345678,
+            "totalLifetimeEmissionReduction": 66,
+            "dateSentToCsnr": "2023-06-22T23: 59: 59.999-07:00",
+            "adjustedEmissionsIntensityPerformance": 95.86,
+            "totalLifetimeEmissionReduction": 66,
+            "reportingRequirementId": 66
+          }'::jsonb)
   $$,
   'It transforms the double (reporting_requirement, emission_intensity_report) into one single form_change without nulls'
 );
 
--- it transforms the existing form_change for the reporting_requirement into the new format
 select results_eq(
   $$
-    select form_data_table_name::text, json_schema_name::text, change_status::text from cif.form_change where id in (3,6,12)
+    select json_schema_name::text, form_data_table_name::text, change_status::text from cif.form_change where json_schema_name='emission_intensity' order by id -- monkeyfuzz figure out ids
   $$,
   $$
     values ('emission_intensity','reporting_requirement', 'pending'),
            ('emission_intensity','reporting_requirement', 'committed'),
+           ('emission_intensity','reporting_requirement', 'pending'),
            ('emission_intensity','reporting_requirement', 'pending')
+
   $$,
   'It transforms the existing reporting_requirement form_change to the emission_intensity schema, and keeps the change_status'
 );
 
--- it doesn't touch the form changes not in a revision
-select results_eq(
-  $$
-    select id, new_form_data from cif.form_change where id=10
-  $$,
-  $$
-    values (10,'{"holdbackAmountToDate": 12345, "reportingRequirementId": 12}'::jsonb)
-  $$,
-  'It leaves the form_changes without a project revision'
-);
 
 -- it doesn't touch other reporting requirement form changes.
-select is(
-  (select new_form_data from cif.form_change where id=9),
-  ('{"projectId": 1, "reportType": "Quarterly", "reportingRequirementIndex": 1}'::jsonb),
+select results_eq(
+  $$
+    select id, new_form_data, change_status from cif.form_change where (new_form_data->>'reportType') not in ('TEIMP')
+  $$,
+  $$
+    values
+    (401, '{"projectId": 1, "reportType": "Quarterly", "reportingRequirementIndex": 1}'::jsonb,'committed'::varchar),
+    (6, '{"projectId": 1, "reportType": "Quarterly", "reportingRequirementIndex": 1}'::jsonb, 'pending'::varchar)
+
+  $$,
   'It doesn''t touch the form changes that are not for the emission intensity report.'
 );
+
 
 -- It is idempotent
 -- We re-run the first 3 tests to make sure nothing has changed
@@ -216,53 +324,7 @@ select cif_private.migration_milestone_form_changes_to_single_form_change();
 alter table cif.form_change enable trigger _100_committed_changes_are_immutable, enable trigger _100_timestamps;
 
 
-
-select is(
-  (select count(*) from cif.form_change where (form_data_table_name='emission_intensity_report')),
-  0::bigint,
-  'There are no form_change records left for the emission_intensity_report table'
-);
-
-
-select is(
-  (select count(*) from cif.form_change where form_data_table_name='reporting_requirement' and json_schema_name='emission_intensity'),
-  3::bigint,
-  'The reporting requirements for emission intensity reports have been converted to the new schema'
-);
-
--- it takes the emission_intensity data from existing revisions and puts them into the new json schema
-  -- for committed form changes
-  -- for pending form changes
-select results_eq(
-  $$
-    select id, change_status, new_form_data from cif.form_change where form_data_table_name='reporting_requirement' and json_schema_name='emission_intensity' order by id;
-  $$,
-  $$
-   values (3, 'committed'::varchar, '{"reportType": "TEIMP", "reportingRequirementIndex": 1}'::jsonb),
-          (6, 'pending'::varchar, '{
-            "adjustedEmissionsIntensityPerformance": 98,
-            "baselineEmissionIntensity": 324.25364,
-            "comments": "comments",
-            "dateSentToCsnr": "2023-07-01T23:59:59.999-07:00",
-            "emissionFunctionalUnit":"tCO2e",
-            "measurementPeriodEndDate":"2023-07-01T23:59:59.999-07:00",
-            "measurementPeriodStartDate": "2023-06-01T23:59:59.999-07:00",
-            "postProjectEmissionIntensity": 124.35,
-            "productionFunctionalUnit": "unit",
-            "reportDueDate": "2023-06-15T23:59:59.999-07:00",
-            "reportType": "TEIMP",
-            "reportingRequirementIndex": 1,
-            "submittedDate": "2023-06-30T23:59:59.999-07:00",
-            "targetEmissionIntensity": 23.2357,
-            "totalLifetimeEmissionReduction": 44.4224
-          }'::jsonb),
-          (12, 'pending'::varchar, '{"reportType": "TEIMP", "reportingRequirementIndex": 1}'::jsonb)
-
-  $$,
-  'It transforms the double (reporting_requirement, emission_intensity_report) into one single form_change without nulls'
-);
-
-
+-- monkeyfuzz add first three tests once they're complete here
 
 
 select finish();
