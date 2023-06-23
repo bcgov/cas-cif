4d2735886f015973d082e9edb517121a4372dd00
Brianna Cerkiewicz <brianna@button.is>
2023-06-23 08:18:21 -0700

    chore: generalize the read only array field template

---
+++ app/components/Form/ReadOnlyArrayFieldTemplate.tsx
@@ -0,0 +1,41 @@
+import { ArrayFieldTemplateProps } from "@rjsf/core";
+
+const ReadOnlyArrayFieldTemplate = (props: ArrayFieldTemplateProps) => {
+  return (
+    <>
+      {props.items.map((item, i) => {
+        return (
+          <div key={item.key} className="itemWrapper">
+            <div className="itemTitle" key={item.key}>
+              {props.uiSchema.itemTitle} {i + 1}
+            </div>
+            <div key={item.key} className="child-wrapper">
+              <div className="child">{item.children}</div>
+            </div>
+          </div>
+        );
+      })}
+      <style jsx>{`
+        .child-wrapper {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+        :global(.child) {
+          flex-grow: 1;
+          margin-right: 1rem;
+        }
+        .itemTitle {
+          font-weight: bold;
+          margin: 1em 0 1em 0;
+        }
+        . itemWrapper {
+          display: flex;
+          flex-direction: column;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default ReadOnlyArrayFieldTemplate;
