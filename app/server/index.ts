7a65d0748d75d0f4992d2dffc07adb919e1eaaea
Brianna Cerkiewicz <brianna@button.is>
2023-06-28 10:48:11 -0700

    chore: create and use attachmentDeleteRouter

--- app/server/index.ts
+++ app/server/index.ts
@@ -17,6 +17,7 @@ import ssoMiddleware from "./middleware/sso";
 import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.js";
 import config from "../config";
 import attachmentDownloadRouter from "./middleware/attachmentDownloadRouter";
+import { attachmentDeleteRouter } from "/home/briannacerkiewicz/cas-cif/app/server/middleware/attachmentDeleteRouter";
 
 const port = config.get("port");
 const dev = config.get("env") !== "production";
@@ -59,6 +60,8 @@ app.prepare().then(async () => {
   server.use(graphQlMiddleware());
 
   server.use(attachmentDownloadRouter);
+
+  server.use(attachmentDeleteRouter);
 
   server.get("*", async (req, res) => {
     return handle(req, res);
---
+++ app/server/middleware/attachmentDeleteRouter.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { performQuery } from "./graphql";
+import { Storage } from "@google-cloud/storage";
+import config from "../../config";
+
+export const attachmentDeleteRouter = Router();
+
+const attachmentDetailsQuery = `query AttachmentDetailsQuery($attachmentId: ID!){
+  attachment(id: $attachmentId) {
+    file
+  }
+}`;
+
+export const handleDelete = async (req, res, next) => {
+  try {
+    const attachmentQueryVariables = {
+      attachmentId: req.params.attachmentId,
+    };
+
+    const result = await performQuery(
+      attachmentDetailsQuery,
+      attachmentQueryVariables,
+      req
+    );
+
+    const {
+      data: {
+        attachment: { file },
+      },
+    } = result;
+
+    const storageClient = new Storage();
+    const bucketName = config.get("attachmentsBucket");
+
+    const response = await storageClient.bucket(bucketName).file(file).delete();
+    return res.sendStatus(response[0].statusCode);
+  } catch (error) {
+    next(error);
+  }
+};
+
+attachmentDeleteRouter.get("/delete/:attachmentId", handleDelete);
