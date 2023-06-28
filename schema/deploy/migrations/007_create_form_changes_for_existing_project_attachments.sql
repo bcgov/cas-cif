d6f5db171fe36ea90d5d2a8024524d365617b594
SeSo <sobhani.sepehr@gmail.com>
2023-06-28 15:45:14 -0700

    chore: add triggers to migration

--- schema/deploy/migrations/007_create_form_changes_for_existing_project_attachments.sql
+++ schema/deploy/migrations/007_create_form_changes_for_existing_project_attachments.sql
@@ -4,10 +4,10 @@
 
 begin;
 
-  alter table cif.form_change disable trigger _100_timestamps;
+  alter table cif.form_change disable trigger _100_committed_changes_are_immutable, disable trigger _set_previous_form_change_id, disable trigger _100_timestamps;
 
   select cif_private.migration_create_form_changes_for_existing_project_attachments();
 
-  alter table cif.form_change enable trigger _100_timestamps;
+  alter table cif.form_change enable trigger _100_committed_changes_are_immutable, enable trigger _set_previous_form_change_id, enable trigger _100_timestamps;
 
 commit;
