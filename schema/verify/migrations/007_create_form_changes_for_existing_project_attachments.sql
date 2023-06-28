b393b7794b81044b2015a5169f461a462e230fac
SeSo <sobhani.sepehr@gmail.com>
2023-06-28 16:01:50 -0700

    chore: add migrations verify file

--- schema/verify/migrations/007_create_form_changes_for_existing_project_attachments.sql
+++ schema/verify/migrations/007_create_form_changes_for_existing_project_attachments.sql
@@ -3,9 +3,22 @@
 begin;
 
   do $$
-    -- TODO: implement this
     begin
-      assert (1=1);
+      assert (
+        select count(*) = 0
+        from cif.form_change
+        where form_data_table_name='project_attachment'
+        and new_form_data is null
+      );
+      assert(
+        select count(*) = 0
+        from cif.project_attachment
+        where project_id not in (
+          select (new_form_data->>'projectId')::int from cif.form_change
+          where form_data_table_name = 'project_attachment'
+          and new_form_data is not null
+        )
+      );
     end;
   $$;
 
