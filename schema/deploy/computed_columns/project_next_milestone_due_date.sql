6b2b338f419672f4be67c9d787833d2fcb4d7f99
SeSo <sobhani.sepehr@gmail.com>
2023-06-30 10:48:14 -0700

    fix: fix project_next_milestone_due_date computed_column and exclude archived reports

--- schema/deploy/computed_columns/project_next_milestone_due_date.sql
+++ schema/deploy/computed_columns/project_next_milestone_due_date.sql
@@ -8,25 +8,27 @@ as
 $function$
     with upcoming as (
         select
-                min(reporting_requirement.report_due_date) as next_due_date
+                min(rr.report_due_date) as next_due_date
             from
-                cif.reporting_requirement
-                join cif.report_type on report_type.name = reporting_requirement.report_type
+                cif.reporting_requirement rr
+                join cif.report_type on report_type.name = rr.report_type
             and
-                reporting_requirement.project_id = $1.id
+                rr.project_id = $1.id
                 and report_type.is_milestone
-                and reporting_requirement.submitted_date is null
+                and rr.submitted_date is null
+                and rr.archived_at is null
             ),
     last_submitted as (
         select
-                max(reporting_requirement.report_due_date) as last_submitted_date
+                max(rr.report_due_date) as last_submitted_date
             from
-                cif.reporting_requirement
-                join cif.report_type on report_type.name = reporting_requirement.report_type
+                cif.reporting_requirement rr
+                join cif.report_type on report_type.name = rr.report_type
             and
-                reporting_requirement.project_id = $1.id
+                rr.project_id = $1.id
                 and report_type.is_milestone
-                and reporting_requirement.submitted_date is not null
+                and rr.submitted_date is not null
+                and rr.archived_at is null
     )
     select coalesce(
         (select next_due_date from upcoming),
