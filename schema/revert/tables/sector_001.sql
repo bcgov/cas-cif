83038316f3f03f3f77644638d16c6f69c7813a08
Sam Warren <samwarren4@gmail.com>
2023-06-30 15:51:17 -0700

    chore: update revert to one-liner

--- schema/revert/tables/sector_001.sql
+++ schema/revert/tables/sector_001.sql
@@ -2,7 +2,6 @@
 
 begin;
 
-delete from cif.sector where sector_name = 'Waste';
-delete from cif.sector where sector_name = 'Manufacturing';
+delete from cif.sector where sector_name in ('Waste', 'Manufacturing');
 
 commit;
