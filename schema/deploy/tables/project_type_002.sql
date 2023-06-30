0e81c83925cde2355fab63f195594a02972a0978
Sam Warren <samwarren4@gmail.com>
2023-06-30 10:43:23 -0700

    chore: create sqitch files for project type

---
+++ schema/deploy/tables/project_type_002.sql
@@ -0,0 +1,7 @@
+-- Deploy cif:tables/project_type_002 to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/revert/tables/project_type_002.sql
@@ -0,0 +1,7 @@
+-- Revert cif:tables/project_type_002 from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
