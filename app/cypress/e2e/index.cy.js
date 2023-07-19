87a2d87a0f5826601ef85921d7e1243becb7b3fe
Brianna Cerkiewicz <brianna@button.is>
2023-07-19 15:15:08 -0700

    test: temporarily delete cypress tests for faster CI

--- app/cypress/e2e/index.cy.js
+++
@@ -1,9 +0,0 @@
-describe("The index page", () => {
-  it("contains the login button ", () => {
-    cy.visit("/");
-    cy.contains("h1", "Welcome").should("be.visible");
-    cy.contains("h3", "CleanBC Industry Fund").should("be.visible");
-    cy.findByRole("button", { name: /Administrator Login/i }).should("exist");
-    cy.findByRole("button", { name: /External User Login/i }).should("exist");
-  });
-});
