3d8196bd09a27db91ee50cc0b4b84aa44388e4d6
Brianna Cerkiewicz <brianna@button.is>
2023-07-20 15:37:56 -0700

    chore: remove loop from discard mutation

--- schema/deploy/mutations/discard_project_attachment_form_change.sql
+++ schema/deploy/mutations/discard_project_attachment_form_change.sql
@@ -2,26 +2,22 @@
 
 begin;
 
-create or replace function cif.discard_project_attachment_form_change(form_change_id int)
-returns setof cif.form_change
+drop function cif.discard_project_attachment_form_change;
+create function cif.discard_project_attachment_form_change(form_change_id int)
+returns cif.form_change
 as $discard_project_attachment_form_change$
 declare
 form_change_record record;
 
 begin
 
-  for form_change_record in select * from cif.form_change
-    where id = $1
-    and form_data_table_name = 'project_attachment'
-  loop
     if form_change_record.operation = 'create' then
       delete from cif.form_change where id = $1;
-      return next form_change_record;
+      return form_change_record;
     else
       update cif.form_change set operation = 'archive' where id = $1;
-      return next form_change_record;
+      return form_change_record;
     end if;
-  end loop;
 
 end;
 
