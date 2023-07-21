4c1108e84d61ad0e6c0e3e4871633d998bd6a255
Brianna Cerkiewicz <brianna@button.is>
2023-07-21 10:04:17 -0700

    test: debug CI cypress tests

--- app/cypress/e2e/cif/project-revision/create-first-project-revision.cy.js
+++ app/cypress/e2e/cif/project-revision/create-first-project-revision.cy.js
@@ -180,10 +180,9 @@ describe("when creating a project, the project page", () => {
     cy.get("input[type=file]").selectFile("@mockFile");
     cy.screenshot();
 
-    // cy.wait("@gqladdProjectAttachmentToRevisionMutation")
-    //   .its("response")
-    //   .should("have.property", "body");
-    cy.wait(10000);
+    cy.wait("@gqladdProjectAttachmentToRevisionMutation")
+      .its("response")
+      .should("have.property", "body");
     cy.findByText("mock.pdf").should("be.visible");
 
     cy.findByText(/Submit project attachments/i).click();
