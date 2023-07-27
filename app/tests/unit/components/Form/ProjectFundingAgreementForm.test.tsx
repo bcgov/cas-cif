cfb1e96e00e1050f8335a23764497fe9ea63400c
Gurjeet Matharu <gurjeet@matharuco.com>
2023-07-26 18:57:20 -0700

    chore: removing mocked router

--- app/tests/unit/components/Form/ProjectFundingAgreementForm.test.tsx
+++ app/tests/unit/components/Form/ProjectFundingAgreementForm.test.tsx
@@ -9,7 +9,6 @@ import compiledFormIndexPageQuery, {
 import { ProjectFundingAgreementForm_projectRevision$data } from "__generated__/ProjectFundingAgreementForm_projectRevision.graphql";
 import projectFundingParameterEPSchema from "/schema/data/prod/json_schema/funding_parameter_EP.json";
 import projectFundingParameterIASchema from "/schema/data/prod/json_schema/funding_parameter_IA.json";
-import { getProjectRevisionFormPageRoute } from "routes/pageRoutes";
 
 const testQuery = graphql`
   query ProjectFundingAgreementFormQuery @relay_test_operation {
@@ -790,10 +789,5 @@ describe("The ProjectFundingAgreementForm", () => {
     const radioButton = screen.getByLabelText("No");
     userEvent.click(radioButton);
     expect(submitButton).toBeEnabled();
-    userEvent.click(submitButton);
-
-    expect(componentTestingHelper.router.push).toHaveBeenCalledWith(
-      getProjectRevisionFormPageRoute("Test Project Revision ID", 8)
-    );
   });
 });
