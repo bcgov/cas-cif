956ed340ec68d3a58637da5860371960c3254b63
Gurjeet Matharu <gurjeet@matharuco.com>
2023-08-02 15:33:12 -0700

    test: updating tests to account for awaiting approval

--- schema/test/unit/computed_columns/form_change_total_project_value_test.sql
+++ schema/test/unit/computed_columns/form_change_total_project_value_test.sql
@@ -1,6 +1,6 @@
 begin;
 
-select plan(3);
+select plan(4);
 
 /** SETUP **/
 
@@ -12,7 +12,7 @@ insert into cif.operator (legal_name) values ('test operator');
 
 insert into cif.project_revision(id, change_status)
   overriding system value
-  values (1, 'pending'),(2, 'pending'), (3, 'pending');
+  values (1, 'pending'),(2, 'pending'), (3, 'pending'), (4, 'pending');
 
 -- funding parameter without max funding amount and proponent cost
 insert into cif.form_change(
@@ -98,6 +98,48 @@ values
     ),
 'create', 'cif', 'funding_parameter', 'pending', 'funding_parameter_EP', 3);
 
+-- funding parameter with max funding amount, proponent cost and additional funding sources
+insert into cif.form_change(id,
+  new_form_data,
+  operation,
+  form_data_schema_name,
+  form_data_table_name,
+  change_status,
+  json_schema_name,
+  project_revision_id
+)
+overriding system value
+values
+(
+  4,
+  json_build_object(
+    'projectId', 2,
+    'maxFundingAmount', 200000,
+    'proponentCost', 150000,
+    'additionalFundingSources', json_build_array(
+      json_build_object(
+        'source', 'cheese import taxes-1',
+        'amount', 1000,
+        'status', 'Approved'
+      ),
+      json_build_object(
+        'source', 'cheese export taxes-2',
+        'amount', 2000,
+        'status', 'Approved'
+      ),
+      json_build_object(
+        'source', 'cheese import taxes-3',
+        'amount', 3000,
+        'status', 'Approved'
+      ),
+      json_build_object(
+        'source', 'cheese export taxes-4',
+        'amount', 4000,
+        'status', 'Denied'
+      )
+    )
+  ),
+'create', 'cif', 'funding_parameter', 'pending', 'funding_parameter_EP', 4);
 /** END SETUP **/
 
 select is(
@@ -118,8 +160,8 @@ select is(
       from cif.form_change where id=2
     ) select * from cif.form_change_total_project_value((select * from record))
   ),
-  355000::numeric,
-  'Returns sum of proponent cost and max funding amount and additional funding sources with status Approved'
+  null,
+  'Returns null as there is a funding source with status of Awaiting Approval'
 );
 
 select is(
@@ -130,7 +172,18 @@ select is(
     ) select * from cif.form_change_total_project_value((select * from record))
   ),
   500000::numeric,
-  'Returns null when additional funding sources are missing'
+  'Returns sum of proponent cost and max funding amount when additional funding sources are missing'
+);
+
+select is(
+  (
+    with record as (
+      select row(form_change.*)::cif.form_change
+      from cif.form_change where id=4
+    ) select * from cif.form_change_total_project_value((select * from record))
+  ),
+  356000::numeric,
+  'Returns sum of proponent cost and max funding amount when additional funding sources are either denied or approved'
 );
 
 select finish();
