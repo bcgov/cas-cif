65f61fe01f7c2dec5f0a1609dea275f8fa16a141
Brianna Cerkiewicz <77306764+BCerki@users.noreply.github.com>
2023-08-04 15:06:28 -0700

    chore: apply suggestions from code review
    Co-authored-by: Dylan Leard <dylan@button.is>

--- schema/deploy/functions/migration_rebuild_project_summary_report_history.sql
+++ schema/deploy/functions/migration_rebuild_project_summary_report_history.sql
@@ -7,15 +7,16 @@ returns void as
 $migration$
 
     with records as (
-        select fc1.previous_form_change_id as prfid, fc2.form_data_record_id as fdrid, fc2.id as id2
+        select fc1.id as id1,fc1.previous_form_change_id as prfid, fc2.form_data_record_id as fdrid, fc2.id as id2
         from cif.form_change as fc1
         join cif.form_change as fc2
         on fc1.previous_form_change_id=fc2.id
+        and fc1.json_schema_name='project_summary_report'
     )
 
-    update cif.form_change set form_data_record_id=
+    update cif.form_change fc set form_data_record_id=
         (
-        select fdrid from records where prfid=id2 limit 1
+        select fdrid from records where fc.id=id1
         )
         where form_data_record_id is null
         and change_status='committed'
