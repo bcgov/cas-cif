9e1e0a261297ad9160fed221d5932b25f32b7a39
Brianna Cerkiewicz <brianna@button.is>
2023-08-09 16:00:33 -0700

    test: pgtap test for milestone status computed column

--- schema/test/unit/computed_columns/project_milestone_status_test.sql
+++ schema/test/unit/computed_columns/project_milestone_status_test.sql
@@ -1,167 +1,89 @@
 begin;
+select plan(4);
 
-select plan(2);
+truncate cif.operator restart identity cascade;
+truncate cif.project restart identity cascade;
+truncate cif.project_revision restart identity cascade;
+truncate cif.form_change restart identity cascade;
+set jwt.claims.sub to '11111111-1111-1111-1111-111111111111';
 
-/** SETUP **/
 
-insert into cif.project_revision(id, change_status)
-  overriding system value
-  values
-    (1, 'pending'),
-    (2, 'pending');
+/** Create mock operator **/
+insert into cif.operator (id, legal_name, trade_name, bc_registry_id, operator_code)
+overriding system value
+values
+  (1, 'first operator legal name', 'first operator trade name', 'AB1234567', 'ABCD'),
+  (2, 'second operator legal name', 'second operator lorem ipsum dolor sit amet limited', 'BC1234567', 'EFGH'),
+  (3, 'third operator legal name', 'third operator trade name', 'EF3456789', 'IJKL');
 
-insert into cif.form_change(
-  id,
-  operation,
-  form_data_schema_name,
-  form_data_table_name,
-  form_data_record_id,
-  project_revision_id,
-  json_schema_name,
-  new_form_data
-) overriding system value
-values (
-  1,
-  'create',
-  'cif',
-  'reporting_requirement',
-  1,
-  1,
-  'milestone',
-  '{"reportType": "General Milestone", "hasExpenses": true, "reportDueDate": "2022-11-14 15:09:36.264005-08", "calculatedHoldbackAmount": 10000, "reportingRequirementIndex": 1, "certifierProfessionalDesignation": "Professional Engineer"}'
-),
-(
-  2,
-  'create',
-  'cif',
-  'reporting_requirement',
-  2,
-  1,
-  'milestone',
-  '{"reportType": "General Milestone", "hasExpenses": true, "reportDueDate": "2022-11-15 15:09:36.264005-08", "adjustedHoldbackAmount": 30000, "calculatedHoldbackAmount": 20000, "reportingRequirementIndex": 2, "certifierProfessionalDesignation": "Professional Engineer"}'
-),
-(
-  3,
-  'create',
-  'cif',
-  'reporting_requirement',
-  3,
-  1,
-  'milestone',
-  '{"reportType": "General Milestone", "hasExpenses": true, "reportDueDate": "2022-11-10 15:09:36.264005-08", "maximumAmount": 50000, "reportingRequirementIndex": 4, "certifierProfessionalDesignation": "Professional Engineer"}'
-),
-(
-  4,
-  'create',
-  'cif',
-  'reporting_requirement',
-  3,
-  1,
-  'milestone',
-  '{"reportType": "Performance Milestone", "hasExpenses": false, "reportDueDate": "2022-11-10 15:09:36.264005-08", "adjustedHoldbackAmount": 20000, "reportingRequirementIndex": 4, "certifierProfessionalDesignation": "Professional Engineer"}'
-),
-(
-  5,
-  'create',
-  'cif',
-  'funding_parameter',
-  3,
-  1,
-  'funding_parameter_EP',
-  '{"holdbackPercentage": 10}'
-),
--- with dateSentToCSNR
-(
-  6,
-  'create',
-  'cif',
-  'reporting_requirement',
-  1,
-  2,
-  'milestone',
-  '{"reportType": "General Milestone", "hasExpenses": true, "reportDueDate": "2022-11-14 15:09:36.264005-08", "calculatedHoldbackAmount": 10000, "reportingRequirementIndex": 1, "certifierProfessionalDesignation": "Professional Engineer",
-  "dateSentToCsnr": "2022-11-14 15:09:36.264005-08"}'
-),
-(
-  7,
-  'create',
-  'cif',
-  'reporting_requirement',
-  2,
-  2,
-  'milestone',
-  '{"reportType": "General Milestone", "hasExpenses": true, "reportDueDate": "2022-11-15 15:09:36.264005-08", "adjustedHoldbackAmount": 30000, "calculatedHoldbackAmount": 20000, "reportingRequirementIndex": 2, "certifierProfessionalDesignation": "Professional Engineer",
-  "dateSentToCsnr": "2022-11-14 15:09:36.264005-08"}'
-),
-(
-  8,
-  'create',
-  'cif',
-  'reporting_requirement',
-  3,
-  2,
-  'milestone',
-  '{"reportType": "General Milestone", "hasExpenses": true, "reportDueDate": "2022-11-10 15:09:36.264005-08", "maximumAmount": 50000, "reportingRequirementIndex": 4, "certifierProfessionalDesignation": "Professional Engineer",
-  "dateSentToCsnr": "2022-11-14 15:09:36.264005-08"}'
-),
-(
-  9,
-  'create',
-  'cif',
-  'reporting_requirement',
-  3,
-  2,
-  'milestone',
-  '{"reportType": "Performance Milestone", "hasExpenses": false, "reportDueDate": "2022-11-10 15:09:36.264005-08", "adjustedHoldbackAmount": 20000, "reportingRequirementIndex": 4, "certifierProfessionalDesignation": "Professional Engineer",
-  "dateSentToCsnr": "2022-11-14 15:09:36.264005-08"}'
-),
-(
-  10,
-  'create',
-  'cif',
-  'funding_parameter',
-  3,
-  2,
-  'funding_parameter_EP',
-  '{"holdbackPercentage": 10}'
-);
+/** Create mock project **/
+insert into cif.project(id, operator_id, funding_stream_rfp_id, project_status_id, proposal_reference, summary, project_name)
+overriding system value
+values
+  (1, 1, 1, 1, '000', 'summary', 'project 1'),
+  (2, 2, 1, 1, '001', 'summary', 'project 2'),
+  (3, 3, 1, 1, '002', 'summary', 'project 3');
 
-/** SETUP END **/
-select '-----------';
-with upcoming_report as
-  (select cif.project_revision_upcoming_reporting_requirement_form_change(
-      (select row(project_revision.*)::cif.project_revision pr
-      from cif.project_revision
-      where id = 1),
-    'milestone')
-  ) select * from upcoming_report;
-select '-----------';
+
+/** Create mock reporting requirement **/
+insert into cif.reporting_requirement(id, project_id, report_type, report_due_date, submitted_date, comments, reporting_requirement_index, description, archived_at)
+overriding system value
+values
+-- project 1, id=2 is late
+(1, 1, 'General Milestone', CURRENT_DATE + INTERVAL '1 day', null, 'milestone 1', 1, 'upcoming milestone', null),
+(2, 1, 'General Milestone', '1999-01-23 13:42:59.085 -0800', null, 'milestone 2', 1, 'late milestone', null),
+(3, 1, 'General Milestone', '2023-01-16 13:42:59.085 -0800', '2023-01-06 13:42:59.085 -0800', 'milestone 3', 1, 'completed milestone', null),
+-- project 2, all milestones complete
+(4, 2, 'General Milestone', '2023-04-15 13:42:59.085 -0800', '2023-02-15 13:42:59.085 -0800', 'milestone 4', 1, 'completed milestone', null) ,
+(5, 2, 'General Milestone', '2023-08-05 13:42:59.085 -0800', '2023-06-11 13:42:59.085 -0800', 'milestone 5', 1, 'completed milestone', null),
+(6, 2, 'General Milestone', '2023-03-02 13:42:59.085 -0800', '2023-02-21 13:42:59.085 -0800', 'milestone 6', 1, 'completed milestone', null),
+(7, 2, 'General Milestone', '2023-04-15 13:42:59.085 -0800', '2023-04-018 13:42:59.085 -0800', 'milestone 7', 1, 'completed milestone', null),
+-- project 3, all milestones upcoming, id=11 checks for a same-day due date
+(8, 3, 'General Milestone', now() + interval '1 day', null, 'milestone 8', 1, 'upcoming milestone', null),
+(9, 3, 'General Milestone', now() + interval '1 day', null, 'milestone 9', 1, 'upcoming milestone', null),
+(10, 3, 'General Milestone', now() + interval '1 day', null, 'milestone 10', 1, 'upcoming milestone', null),
+(11, 3, 'General Milestone', now(), null, 'milestone 10', 1, 'upcoming milestone', '2023-04-06 13:42:59.085 -0800');
 
 select is(
   (
-    with record as (
-    select row(form_change.*)::cif.form_change
-    from cif.form_change where id=5
-    ) select cif.form_change_maximum_performance_milestone_amount((select * from record))
+    select cif.project_milestone_status((select row(project.*)::cif.project from cif.project where id=123))
   ),
   (
-    null
+    'none'
   ),
-  'Returns null when not all payments have been completed'
+  'Returns none when there are no milestones'
 );
 
 select is(
   (
-    with record as (
-    select row(form_change.*)::cif.form_change
-    from cif.form_change where id=6
-    ) select cif.form_change_maximum_performance_milestone_amount((select * from record))
+    select cif.project_milestone_status((select row(project.*)::cif.project from cif.project where id=1))
   ),
   (
-    40000::numeric
+    'late'
   ),
-  'Returns the correct sum of all milestones with expenses when all payments have been paid'
+  'Returns late when a milestone is overdue'
 );
+
+select is(
+  (
+    select cif.project_milestone_status((select row(project.*)::cif.project from cif.project where id=3))
+  ),
+  (
+    'on track'
+  ),
+  'Returns onTrack when no milestones are overdue'
+);
+
+select is(
+  (
+    select cif.project_milestone_status((select row(project.*)::cif.project from cif.project where id=2))
+  ),
+  (
+    'complete'
+  ),
+  'Returns complete when all milestones are complete'
+);
+
 
 select finish();
 
