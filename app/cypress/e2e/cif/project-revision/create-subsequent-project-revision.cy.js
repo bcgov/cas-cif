00c7c8e7051fdf34c38c226e5ee7e01f445cdfcd
Brianna Cerkiewicz <brianna@button.is>
2023-08-25 08:59:19 -0700

    test: debug e2e tests

--- app/cypress/e2e/cif/project-revision/create-subsequent-project-revision.cy.js
+++ app/cypress/e2e/cif/project-revision/create-subsequent-project-revision.cy.js
@@ -28,6 +28,7 @@ describe("the project amendment and revisions page", () => {
     cy.get('[type="radio"]').check("Amendment");
     cy.get(".checkbox").contains("Scope").click();
     cy.get("button").contains("New Revision").click();
+    cy.get("button").contains("New Revision").click();
     cy.wait(10000);
     // cy.wait("@gqlcreateProjectRevisionMutation")
     //   .its("response")
@@ -42,6 +43,7 @@ describe("the project amendment and revisions page", () => {
     cy.findByText(/New Revision/i).click();
     cy.url().should("include", "/create");
     cy.get('[type="radio"]').check("General Revision");
+    cy.get("button").contains("New Revision").click();
     cy.get("button").contains("New Revision").click();
     cy.wait(10000);
     // cy.wait("@gqlcreateProjectRevisionMutation")
