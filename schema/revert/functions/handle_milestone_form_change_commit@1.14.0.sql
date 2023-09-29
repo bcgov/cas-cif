bece9972a626d09e68eb1a57a4456fe93f240e26
SeSo <sobhani.sepehr@gmail.com>
2023-09-29 13:50:14 -0700

    fix: update milestone form change commit handler to not update archived records

--- schema/revert/functions/handle_milestone_form_change_commit@1.14.0.sql
+++ schema/revert/functions/handle_milestone_form_change_commit@1.14.0.sql
@@ -107,7 +107,7 @@ begin
           gross_amount = adjusted_or_calculated_gross_amount,
           net_amount = adjusted_or_calculated_net_amount,
           date_sent_to_csnr = (fc.new_form_data->>'dateSentToCsnr')::timestamptz
-        where reporting_requirement_id = fc.form_data_record_id and archived_at is null;
+        where reporting_requirement_id = fc.form_data_record_id;
       -- otherwise we create a new payment record
       else
         insert into cif.payment(
@@ -123,14 +123,14 @@ begin
         );
       end if;
     else
-      update cif.payment set archived_at = now() where reporting_requirement_id = fc.form_data_record_id and archived_at is null;
+      update cif.payment set archived_at = now() where reporting_requirement_id = fc.form_data_record_id;
     end if;
 
   elsif fc.operation = 'archive' then
 
     update cif.reporting_requirement set archived_at = now() where id = fc.form_data_record_id;
     update cif.milestone_report set archived_at = now() where reporting_requirement_id = fc.form_data_record_id;
-    update cif.payment set archived_at = now() where reporting_requirement_id = fc.form_data_record_id and archived_at is null;
+    update cif.payment set archived_at = now() where reporting_requirement_id = fc.form_data_record_id;
 
   end if;
 
