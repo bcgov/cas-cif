6a1bcad257fd3848acf82b9fa5436842a946dc01
Mike Vesprini <mike@button.is>
2023-11-20 15:11:23 -0800

    chore: fix revert sqitch function

--- schema/revert/mutations/commit_form_change_internal.sql
+++ schema/revert/mutations/commit_form_change_internal.sql
@@ -30,7 +30,7 @@ begin
 end;
   $$ language plpgsql volatile;
 
-grant execute on function cif_private.commit_form_change_internal to cif_internal, cif_external, cif_admin;
+grant execute on function cif_private.commit_form_change_internal(fc cif.form_change) to cif_internal, cif_external, cif_admin;
 
 comment on function cif_private.commit_form_change_internal(cif.form_change) is 'Commits the form change and calls the corresponding commit handler.';
 
