df054fb7351b94ffdd319fb7178af06b10a0951e
Mike Vesprini <mike@button.is>
2023-12-06 20:32:58 -0800

    chore: handle the various cases for creating form changes in concurrent revisions, untested

--- schema/deploy/mutations/commit_form_change_internal.sql
+++ schema/deploy/mutations/commit_form_change_internal.sql
@@ -34,24 +34,151 @@ begin
   where id = fc.id;
 
   if pending_project_revision_id is not null then
-    -- If the committing form change is a create, then it needs to be created in the pending revision with an update operation.
+  /**
+        project_contact
+        project_manager
+        project_summary_report
+        funding_parameter_EP
+            operator (don't belong to project revisinos and thus aren't affected by concurrency)
+        milestone
+        project_attachment
+        funding_parameter_IA
+        project (create doesn't apply, update is handled.)
+        reporting_requirement
+            contact (don't belong to project revisinos and thus aren't affected by concurrency)
+        emission_intensity
+  */
+    
     if fc.operation = 'create' then
-      select id into new_fc_in_pending_id from cif.create_form_change(
-        operation => 'update'::cif.form_change_operation,
-        form_data_schema_name => 'cif',
-        form_data_table_name => fc.form_data_table_name,
-        form_data_record_id => fc.form_data_record_id,
-        project_revision_id => pending_project_revision_id,
-        json_schema_name => fc.json_schema_name,
-        new_form_data => fc.new_form_data
-      );
-      update cif.form_change set previous_form_change_id = fc.id where id = new_fc_in_pending_id;
+    -- These are the forms that a project can have at most one of.
+    -- If pending has created one alredy, then we need to set the previous_form_change_id and the form_data_record_id.
+      if (
+        (fc.json_schema_name in ('funding_parameter_EP', 'funding_parameter_IA', 'emission_intensity', 'project_summary_report')) and
+        ((select count(id) from cif.form_change where project_revision_id = pending_project_revision_id and json_schema_name = fc.json_schema_name) > 0)
+      ) then
+        -- MIKE consider updating null fields in pending with values from committing.
+        update cif.form_change set
+          previous_form_change_id = fc.id,
+          form_data_record_id = recordId
+        where project_revision_id = pending_project_revision_id
+          and json_schema_name = fc.json_schema_name;
+      elsif (
+        fc.json_schema_name = 'project_contact'
+        and (select count(id) from cif.form_change where project_revision_id=pending_project_revision_id and json_schema_name=fc.json_schema_name) > 0
+      ) then
+      -- if pending has any contacts, then create the new form change in pending and update the contactIndex to be the highest
+      -- contactIndex in pending + 1. If it doesn't then the catch-all for 'create' will handle it
+        select id into new_fc_in_pending_id from cif.create_form_change(
+          operation => 'update'::cif.form_change_operation,
+          form_data_schema_name => 'cif',
+          form_data_table_name => fc.form_data_table_name,
+          form_data_record_id => recordId,
+          project_revision_id => pending_project_revision_id,
+          json_schema_name => fc.json_schema_name,
+          new_form_data => (fc.new_form_data || format('{"contactIndex": %s}',
+            (select max(new_form_data ->> contactIndex) from cif.form_change
+              where project_revision_id=pending_project_revision_id
+              and json_schema_name = fc.json_schema_name
+            ) + 1)
+          )
+        );
+        update cif.form_change set previous_form_change_id = fc.id where id = new_fc_in_pending_id;
+
+      elsif (
+        fc.json_schema_name = 'project_manager'
+        and (
+          (select count(id) from cif.form_change
+          where project_revision_id=pending_project_revision_id
+          and json_schema_name=fc.json_schema_name
+          and new_form_data ->> 'projectManagerLabelId' = fc.new_form_data ->> 'projectManagerLabelId') > 0
+        )
+      ) then
+      -- If the projectManagerLabelId already exists in pending, set the form_data_record_id and previous_form_change_id.
+      -- If not, then the catch all case will handle it.
+        update cif.form_change set
+          previous_form_change_id = fc.id,
+          form_data_record_id = recordId
+        where id = (select id from cif.form_change
+          where project_revision_id=pending_project_revision_id
+          and json_schema_name=fc.json_schema_name
+          and new_form_data ->> 'projectManagerLabelId' = fc.new_form_data ->> 'projectManagerLabelId');
+
+      elsif (
+        fc.json_schema_name = 'reporting_requirement'
+        and (
+          (select count(id) from cif.form_change where project_revision_id = pending_project_revision_id
+            and json_schema_name = fc.json_schema_name
+            and new_form_data ->> 'reportType' = fc.new_form_data ->> 'reportType')
+        > 0)
+      ) then
+      -- If  reporting_requirements of this reportType already exist in pending, create the new form_change and set the reportingRequirementIndex
+      -- to the highest existing in pending plus 1. If committing is the first reporting_requirement of this reportType, then the catch-all works.
+        select id into new_fc_in_pending_id from cif.create_form_change(
+          operation => 'update'::cif.form_change_operation,
+          form_data_schema_name => 'cif',
+          form_data_table_name => fc.form_data_table_name,
+          form_data_record_id => recordId,
+          project_revision_id => pending_project_revision_id,
+          json_schema_name => fc.json_schema_name,
+          new_form_data => (fc.new_form_data || format('{"reportingRequirementIndex": %s}',
+            (select max(new_form_data ->> reportingRequirementIndex) from cif.form_change 
+              where project_revision_id=pending_project_revision_id
+              and json_schema_name = fc.json_schema_name
+              and new_form_data ->> 'reportType' = fc.new_form_data ->> 'reportType'
+            ) + 1)
+          )
+        );
+        update cif.form_change set previous_form_change_id = fc.id where id = new_fc_in_pending_id;
+
+      elsif (
+        fc.json_schema_name = 'milestone'
+        and (
+          (select count(id) from cif.form_change where project_revision_id = pending_project_revision_id
+            and json_schema_name = fc.json_schema_name)
+        > 0)
+      ) then
+      -- If committing is creating a milestone and milestones exist, create the milestone in pending and set the reportingRequirementIndex
+      -- to be the max existing in pending plus 1. If pending has no milestones, the catch-all works.
+        select id into new_fc_in_pending_id from cif.create_form_change(
+          operation => 'update'::cif.form_change_operation,
+          form_data_schema_name => 'cif',
+          form_data_table_name => fc.form_data_table_name,
+          form_data_record_id => recordId,
+          project_revision_id => pending_project_revision_id,
+          json_schema_name => fc.json_schema_name,
+          new_form_data => (fc.new_form_data || format('{"reportingRequirementIndex": %s}',
+            (select max(new_form_data ->> reportingRequirementIndex) from cif.form_change 
+              where project_revision_id=pending_project_revision_id
+              and json_schema_name = fc.json_schema_name
+            ) + 1)
+          )
+        );
+        update cif.form_change set previous_form_change_id = fc.id where id = new_fc_in_pending_id;
+      /**
+      This next case acts as the catch-all for 'create'. It applies to any scenario in which the pending revision does not have an equivalent
+      form change to the one being committed. This includes unordered lists (e.g. attachments, milestones, etc.), the unique forms
+      like funding_parameter when pending has not created one yet, and ordered/pseudo ordered lists when the equivalent index does not
+      exist in pending.
+      **/
+      else
+
+        select id into new_fc_in_pending_id from cif.create_form_change(
+          operation => 'update'::cif.form_change_operation,
+          form_data_schema_name => 'cif',
+          form_data_table_name => fc.form_data_table_name,
+          form_data_record_id => recordId,
+          project_revision_id => pending_project_revision_id,
+          json_schema_name => fc.json_schema_name,
+          new_form_data => fc.new_form_data
+        );
+        update cif.form_change set previous_form_change_id = fc.id where id = new_fc_in_pending_id;
+      end if;
 
     elsif fc.operation = 'update' then
       -- store the pending project revisions corresponding form_change, and its parent
       select * into pending_form_change from cif.form_change
         where project_revision_id = pending_project_revision_id
-        and form_data_table_name = fc.form_data_table_name
+        and json_schema_name = fc.json_schema_name
         and form_data_record_id = fc.form_data_record_id limit 1;
       select * into parent_of_pending_form_change from cif.form_change
         where id = pending_form_change.previous_form_change_id limit 1;
@@ -93,7 +220,6 @@ end;
   $$ language plpgsql volatile;
 
 grant execute on function cif_private.commit_form_change_internal(cif.form_change, int) to cif_internal, cif_external, cif_admin;
-
 comment on function cif_private.commit_form_change_internal(cif.form_change, int) is 'Commits the form change and calls the corresponding commit handler.';
 
 commit;
