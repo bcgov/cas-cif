eb54b2a570aa5da6f683ab9b3817329598ddc6e6
Josh Gamache <joshua@button.is>
2024-01-08 11:26:23 -0700

    chore: add script to run move from TF Cloud state backup to local TF state backup

---
+++ terraform/tf-migration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SOURCE_STATE_PATH="./temp-state/tfcloud.tfstate"
+TARGET_STATE_PATH="./temp-state/local.tfstate"
+
+NAMESPACE="c53ff1-dev"
+declare -a PATHS=("google_storage_bucket.bucket")
+declare -a APPS=("cif-documents" "cif-backups")
+
+for path in "${PATHS[@]}"; do
+  for app in "${APPS[@]}"; do
+    source_resource="${path}[\"${NAMESPACE},${app}\"]"
+    target_resource="${path}[\"${app}\"]"
+
+    terraform state mv -state="${SOURCE_STATE_PATH}" -state-out="${TARGET_STATE_PATH}" "${source_resource}" "${target_resource}"
+  
+  done
+done
