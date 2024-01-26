c95fa160a4aa56dd68e51fc1d8c694f2e0458be3
Josh Gamache <joshua@button.is>
2024-01-26 11:35:22 -0700

    docs: add usage comments to migration script

--- chart/cas-cif/terraform/tf-migration.sh
+++ chart/cas-cif/terraform/tf-migration.sh
@@ -2,9 +2,11 @@
 SOURCE_STATE_PATH="./temp-state/tfcloud.tfstate"
 TARGET_STATE_PATH="./temp-state/local.tfstate"
 
-NAMESPACE="c53ff1-dev"
+NAMESPACE="c53ff1-dev" # Change this value to match the project's namespace-prefix and environment
+declare -a APPS=("cif-documents" "cif-backups") # Change these to match the project's application bucket names
+
+# The PATHS used as Terraform resource paths are all consistent across projects
 declare -a PATHS=("google_storage_bucket.bucket" "google_service_account.account" "google_storage_bucket_iam_member.admin" "google_service_account.viewer_account" "google_storage_bucket_iam_member.viewer" "google_service_account_key.key" "google_service_account_key.viewer_key" "kubernetes_secret.secret_sa")
-declare -a APPS=("cif-documents" "cif-backups")
 
 for path in "${PATHS[@]}"; do
   for app in "${APPS[@]}"; do
