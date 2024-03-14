ef243d849390ada41f4045f2d5eb900013e087be
Mike Vesprini <mike@button.is>
2024-03-13 17:00:32 -0700

    chore: remove redundant limit 1

--- schema/deploy/mutations/commit_form_change.sql
+++ schema/deploy/mutations/commit_form_change.sql
@@ -14,9 +14,10 @@ begin
 
   return (select cif_private.commit_form_change_internal(
     (select row(form_change.*)::cif.form_change from cif.form_change where id = row_id),
+    -- This is guaranteed to be a single row as we have unique inidices on pending general revision and pending amendment
     (select id  from cif.project_revision
       where project_id=(select project_id from cif.project_revision where id = form_change_patch.project_revision_id)
-      and change_status = 'pending' and id != form_change_patch.project_revision_id limit 1)
+      and change_status = 'pending' and id != form_change_patch.project_revision_id)
   ));
 end;
   $$ language plpgsql volatile;
